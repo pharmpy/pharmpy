2c125776c622631d141b0554a422b1f1ecf43f16
Gunnar Yngman <gunnar.yngman@farmbio.uu.se>
2018-07-27 09:35:44 +0200

    NONMEM model.py Input class to separate source file

--- src/pysn/psn/model_apis/api_nonmem/model.py
+++ src/pysn/psn/model_apis/api_nonmem/model.py
@@ -1,6 +1,7 @@
 """(Specific) NONMEM 7.x model API"""
 from .records.factory import create_record
 from ...exceptions import *
+from .input import Input
 
 
 def create_unique_symbol(symbols, prefix):
@@ -12,43 +13,6 @@ def create_unique_symbol(symbols, prefix):
             i += 1
         else:
             return candidate
-
-
-class Input:
-    """A NONMEM 7.x model $INPUT class"""
-    def __init__(self, model):
-        self.model = model
-
-    def column_names(self, problem=0):
-        """Gets a list of the column names of the input dataset
-
-        Limitation: Tries to create unique symbol for anonymous columns, but
-        only uses the INPUT names.
-        """
-        records = self.model.get_records("INPUT", problem=problem)
-        all_symbols = []
-        for record in records:
-            pairs = record.ordered_pairs()
-            for key in pairs:
-                all_symbols.append(key)
-                if pairs[key]:
-                    all_symbols.append(key)
-        names = []
-        for record in records:
-            pairs = record.ordered_pairs()
-            for key, value in pairs.items():
-                if key == "DROP" or key == "SKIP":
-                    names.append(all_symbols, "DROP")
-                else:
-                    names.append(key)
-        return names
-
-    def dataset_filename(self, problem=0):
-        """Get the filename of the dataset"""
-        data_records = self.model.get_records("DATA", problem=problem)
-        pairs = data_records[0].ordered_pairs()
-        first_pair = next(iter(pairs.items()))
-        return first_pair[0]
 
 
 class Model:
