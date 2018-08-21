e6ec9e97b7f7183a840fe052f50c6610c08ba333
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2018-08-21 11:46:21 +0200

    data: First rudimentary data class
    Can read in a data set using pandas, which was added as dependency

--- src/pysn/psn/api_nonmem/data.py
+++ src/pysn/psn/api_nonmem/data.py
@@ -1,19 +1,21 @@
 import re
+import pandas as pd
 
 # This class is an IO class that is a prefilter for pandas.read_table.
 # Only things that cannot be handled by pandas will be taken care of here and the rest will be taken care of by pandas.
 
 class NMTRANDataIO:
     def __init__(self, filename, ignore_character):
-        if ignore_character == '@':
-            comment_regexp = re.compile(r'^[A-Za-z].*\n', re.MULTILINE)
-        else:
-            comment_regexp = re.compile('^[' + ignore_character + '].*\n', re.MULTILINE)
-
         with open(str(filename), 'r') as datafile:
             self.contents = datafile.read()      # All variations of newlines are converted into \n
 
-        self.contents = re.sub(comment_regexp, '', self.contents)
+        if ignore_character:
+            if ignore_character == '@':
+                comment_regexp = re.compile(r'^[A-Za-z].*\n', re.MULTILINE)
+            else:
+                comment_regexp = re.compile('^[' + ignore_character + '].*\n', re.MULTILINE)
+            self.contents = re.sub(comment_regexp, '', self.contents)
+
         self.contents = re.sub(r'\s\.\s', '0', self.contents)        # Replace dot surrounded by space with 0 as explained in the NM-TRAN manual
 
         self.length = len(self.contents)
@@ -23,3 +25,14 @@ class NMTRANDataIO:
         index = self.index
         self.index += numbytes
         return self.contents[index:index + numbytes]
+
+# A data set
+
+class Data:
+    def __init__(self, filename):       # FIXME: Should init directly from model in future
+        self.filename = filename
+        self.ignore_character = '@'
+
+    def read(self):
+        file_io = NMTRANDataIO(self.filename, self.ignore_character) 
+        self.data_frame = pd.read_table(str(self.filename), sep='\s+|,', engine='python')
--- tests/nonmem/test_data.py
+++ tests/nonmem/test_data.py
@@ -7,3 +7,9 @@ def test_data_io(pheno_data):
     assert data_io.read(13) == '      1    0.'
     data_io = pysn.psn.api_nonmem.data.NMTRANDataIO(pheno_data, 'Q')
     assert data_io.read(5) == 'ID TI'
+
+def test_data_read(pheno_data):
+    data = pysn.psn.api_nonmem.data.Data(pheno_data)
+    data.read()
+    df = data.data_frame
+    assert list(df.iloc[1]) == [1.0, 2.0, 0.0, 1.4, 7.0, 17.3, 0.0, 0.0]
