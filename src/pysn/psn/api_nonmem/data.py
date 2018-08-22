c51e9d4358f92f35e8f027fe2748c85509fece48
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2018-08-22 11:45:06 +0200

    data: Simplify StringIO and actually use it in Data

--- src/pysn/psn/api_nonmem/data.py
+++ src/pysn/psn/api_nonmem/data.py
@@ -1,30 +1,24 @@
 import re
+from io import StringIO
 import pandas as pd
 
 # This class is an IO class that is a prefilter for pandas.read_table.
 # Only things that cannot be handled by pandas will be taken care of here and the rest will be taken care of by pandas.
 
-class NMTRANDataIO:
+class NMTRANDataIO(StringIO):
     def __init__(self, filename, ignore_character):
         with open(str(filename), 'r') as datafile:
-            self.contents = datafile.read()      # All variations of newlines are converted into \n
+            contents = datafile.read()      # All variations of newlines are converted into \n
 
         if ignore_character:
             if ignore_character == '@':
                 comment_regexp = re.compile(r'^[A-Za-z].*\n', re.MULTILINE)
             else:
                 comment_regexp = re.compile('^[' + ignore_character + '].*\n', re.MULTILINE)
-            self.contents = re.sub(comment_regexp, '', self.contents)
+            contents = re.sub(comment_regexp, '', contents)
 
-        self.contents = re.sub(r'\s\.\s', '0', self.contents)        # Replace dot surrounded by space with 0 as explained in the NM-TRAN manual
-
-        self.length = len(self.contents)
-        self.index = 0
-
-    def read(self, numbytes):
-        index = self.index
-        self.index += numbytes
-        return self.contents[index:index + numbytes]
+        contents = re.sub(r'\s\.\s', '0', contents)        # Replace dot surrounded by space with 0 as explained in the NM-TRAN manual
+        super().__init__(contents)
 
 # A data set
 
@@ -33,6 +27,10 @@ class Data:
         self.filename = filename
         self.ignore_character = '@'
 
+    #@property
+    #def data_frame(self):
+    #    return self._data_frame
+
     def read(self):
         file_io = NMTRANDataIO(self.filename, self.ignore_character) 
-        self.data_frame = pd.read_table(str(self.filename), sep='\s+|,', engine='python')
+        self.data_frame = pd.read_table(file_io, sep='\s+|,', header=None, engine='python')
