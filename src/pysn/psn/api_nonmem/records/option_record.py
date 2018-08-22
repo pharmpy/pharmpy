d57a6ce9a4a449b8bad5f81cd1a441dbd60a3ce4
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2018-08-22 13:48:34 +0200

    Add property options_list to OptionRecord

--- src/pysn/psn/api_nonmem/records/option_record.py
+++ src/pysn/psn/api_nonmem/records/option_record.py
@@ -15,15 +15,20 @@ class OptionRecord(Record):
     def __init__(self, raw_text):
         self.parser = OptionRecordParser(raw_text)
         self.root = self.parser.root
-        #self.root.treeprint()
 
+    @property
     def option_pairs(self):
         """ Extract the key-value pairs
             If no value exists set it to None
         """
         pairs = OrderedDict()
-        for keyval in self.root.all('keyval'):
-            print(keyval)
+        for keyval in self.root.all('pair'):
+            if hasattr(keyval, 'KEY'):
+                pairs[keyval.KEY] = keyval.VALUE
+            else:
+                pairs[keyval.VALUE] = None
+
+        return pairs
 
     def __str__(self):
         return super().__str__() + str(self.root)
--- tests/nonmem/records/test_option_record.py
+++ tests/nonmem/records/test_option_record.py
@@ -1,18 +1,6 @@
+from collections import OrderedDict
+
 def test_create_record(api):
     record = api.records.create_record("ESTIMATION MAXEVALS=9999 INTER")
-    pairs = record.option_pairs() 
-    #create_record = api.records.create_record
-
-    #obj = create_record("PROBLEM ID TIME")
-    #assert obj.__class__.__name__ == "ProblemRecord"
-    #assert obj.name == "PROBLEM"
-
-    #obj = create_record("PROB ID TIME")
-    #assert obj.__class__.__name__ == "ProblemRecord"
-    #assert obj.name == "PROBLEM"
-
-    #obj = create_record("PROB  MYPROB")
-    #assert obj.__class__.__name__, "ProblemRecord"
-    #assert obj.name, "PROBLEM"
-    #assert obj.raw_name, "PROB"
-    #assert obj.string, "MYPROB"
+    pairs = record.option_pairs 
+    assert pairs == OrderedDict([('MAXEVALS', '9999'), ('INTER', None)])
