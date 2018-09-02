99ed74825be96cbf6ce35af93a9c608ec69f0e65
Gunnar Yngman <gunnar.yngman@farmbio.uu.se>
2018-09-02 20:37:13 +0200

    minor ThetaRecord fix

--- src/pysn/api_nonmem/records/theta_record.py
+++ src/pysn/api_nonmem/records/theta_record.py
@@ -4,8 +4,8 @@ from collections import namedtuple
 
 from pysn.parse_utils import AttrTree
 
-from .parser import ThetaRecordParser
-from .record import Record
+from pysn.api_nonmem.records.parser import ThetaRecordParser
+from pysn.api_nonmem.records.record import Record
 
 ThetaInit = namedtuple('ThetaInit', ('low', 'init', 'up', 'fix', 'node'))
 
@@ -14,12 +14,6 @@ class ThetaRecord(Record):
     def __init__(self, buf):
         self.parser = ThetaRecordParser(buf)
         self.root = self.parser.root
-
-    def _lexical_tokens(self):
-        pass
-
-    def ordered_pairs(self):
-        pass
 
     def __str__(self):
         return super().__str__() + str(self.parser.root)
