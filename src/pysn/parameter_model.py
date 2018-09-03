336a1cb14dd497f6cd5db745a3f4214dc83b2f83
Gunnar Yngman <gunnar.yngman@farmbio.uu.se>
2018-09-03 20:11:24 +0200

    expand PopulationParameter class for better basis in npdarray

--- src/pysn/parameter_model.py
+++ src/pysn/parameter_model.py
@@ -1,10 +1,38 @@
 # -*- encoding: utf-8 -*-
 
 
-from collections import namedtuple
+# import numpy as np
 
 
-PopulationParameter = namedtuple('PopulationParameter', ('lower', 'init', 'upper', 'fixed'))
+class PopulationParameter:
+    __slots__ = ('init', 'fix', 'lower', 'upper')
+
+    def __init__(self, init=None, fix=None, lower=None, upper=None):
+        self.init = init
+        self.fix = fix
+        self.lower = lower
+        self.upper = upper
+
+    def __repr__(self):
+        defined = list()
+        for key in self.__slots__:
+            val = getattr(self, key)
+            if val is not None:
+                defined += [(key, val)]
+        values = ', '.join('%s=%s' % (key, repr(val)) for key, val in defined)
+        return '%s(%s)' % (self.__class__.__name__, values)
+
+    def __str__(self):
+        if self.fix:
+            return 'fix(%s)' % (self.init,)
+        if self.lower is None and self.upper is None:
+            values = str(self.init)
+        else:
+            values = (self.lower, self.init, self.upper)
+            values = ','.join('' if x is None else str(x) for x in values)
+        return 'est(%s)' % (values,)
+
+
 # IndividualParameter = namedtuple('IndividualParameter', ()):
 # RandomVariable = namedtuple('RandomVariable', ()):
 
