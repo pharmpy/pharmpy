0a97c871e50ed76b37425bdd2580bfdfb5e09fca
Gunnar Yngman <gunnar.yngman@farmbio.uu.se>
2018-09-15 15:10:12 +0200

    add clean-up of src/pharmpy.egg_info/ dir

--- clean_cache.sh
+++ clean_cache.sh
@@ -34,8 +34,9 @@ elif [ "$(git cat-file -t 0db96cd4db3aa193715322b61e120087a8b2e922 2>/dev/null)"
     exit 1
 fi
 
-cmd "Removing build dir:       dist/"           rm -rf "$DIR/dist/"
-cmd "Removing coverage dir:    htmlcov/"        rm -rf "$DIR/htmlcov/"
-cmd "Removing autogen doc dir: docs/reference/" rm -rf "$DIR/docs/reference/"
-cmd "Removing Tox cache:       .tox/"           rm -rf "$DIR/.tox/"
-cmd "Removing pytest:          .pytest_cache/"  rm -rf "$DIR/.pytest_cache/"
+cmd "Removing autogen docs (docs/reference/)" rm -rf "$DIR"/docs/reference/
+cmd "Removing build dir (dist/)"              rm -rf "$DIR"/dist/
+cmd "Removing coverage dir (htmlcov/)"        rm -rf "$DIR"/htmlcov/
+cmd "Removing egg info dirs (src/*.egg-info)" rm -rf "$DIR"/src/*.egg-info/
+cmd "Removing pytest cache (.pytest_cache/)"  rm -rf "$DIR"/.pytest_cache/
+cmd "Removing Tox cache (.tox/)"              rm -rf "$DIR"/.tox/
