30ce6514d29262c3dbe9f4007cc67695a58b864f
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2018-11-30 17:44:50 +0100

    Add math function round and keep sum

--- src/pharmpy/math.py
+++ src/pharmpy/math.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 
+# This module could probably be made private.
 
 def triangular_root(x):
     '''Calculate the triangular root of x. I.e. if x is a triangular number T_n what is n?
@@ -13,7 +14,26 @@ def flattened_to_symmetric(x):
     '''
     n = triangular_root(len(x))
     new = np.zeros((n, n))
-    inds = np.triu_indices_from(new)
+    inds = np.tril_indices_from(new)
     new[inds] = x
     new[(inds[1], inds[0])] = x
     return new
+
+
+def round_and_keep_sum(x, s):
+    '''Round values in Series x and their sum must be s
+
+       Algorithm: Floor all elements in series. If sum not correct add one to element with highest fractional part
+                  until sum is reached.
+    '''
+    sorted_fractions = x.apply(lambda x: math.modf(x)[0]).sort_values(ascending=False)
+    rounded_sample_sizes = x.apply(lambda x: math.modf(x)[1])
+    for (group_index, _) in sorted_fractions.iteritems():
+        num_samples = rounded_sample_sizes.sum()
+        diff = s - num_samples
+        if diff == 0:
+            break
+        step = math.copysign(1, diff)
+        rounded_sample_sizes[group_index] += step 
+
+    return rounded_sample_sizes
---
+++ tests/test_math.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from pandas.testing import assert_series_equal
+from numpy.testing import assert_array_equal
+
+import pharmpy.math
+
+
+def test_triangular_root():
+    assert pharmpy.math.triangular_root(1) == 1
+    assert pharmpy.math.triangular_root(3) == 2
+    assert pharmpy.math.triangular_root(6) == 3
+    assert pharmpy.math.triangular_root(10) == 4
+
+
+def test_flattened_to_symmetric():
+    assert_array_equal(pharmpy.math.flattened_to_symmetric([1.0]), np.array([[1.0]]))
+    assert_array_equal(pharmpy.math.flattened_to_symmetric([1.0, 1.5, 2.0]), np.array([[1.0, 1.5], [1.5, 2.0]]))
+    assert_array_equal(pharmpy.math.flattened_to_symmetric([1.0, 1.5, 2.0, -1.0, 3.0, 5.5]), np.array([[1.0, 1.5, -1.0], [1.5, 2.0, 3.0], [-1.0, 3.0, 5.5]]))
+
+
+def test_round_and_keep_sum():
+    ser = pd.Series([1.052632, 0.701754, 0.701754, 1.052632, 2.456140, 2.807018, 4.210526, 4.210526, 3.157895, 0.350877])
+    correct_results = pd.Series([1.0, 1.0, 1.0, 1.0, 2.0, 3.0, 4.0, 4.0, 3.0, 0.0])
+    rounded = pharmpy.math.round_and_keep_sum(ser, 20)
+    assert_series_equal(rounded, correct_results)
