2dbe38b2ac86cc7150048ccc8e7446ea24bd1471
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2020-01-05 20:10:57 +0100

    Remove old parameter module

--- src/pharmpy/parameters/__init__.py
+++
@@ -1,6 +0,0 @@
-from .distributions import CovarianceMatrix
-from .parameter_model import ParameterModel
-from .scalars import Covar, Scalar, Var
-from .vectors import ParameterList
-
-__all__ = ['Covar', 'CovarianceMatrix', 'ParameterList', 'ParameterModel', 'Scalar', 'Var']
