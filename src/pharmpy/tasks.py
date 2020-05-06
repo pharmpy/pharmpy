51e419d44d373049d240d399bf51716ad211e01d
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2020-05-06 12:57:14 +0200

    Start of tasks module

---
+++ src/pharmpy/tasks.py
@@ -0,0 +1,23 @@
+"""Model specific tasks. Can be built into a model
+"""
+
+
+class Estimation:
+    """A generic parameter estimation step
+    """
+    def __init__(self, evaluate=False, tool_options=None):
+        self.evaluate = evaluate
+        if tool_options is None:
+            self.tool_options = []
+        else:
+            self.tool_options = tool_options
+
+
+class FOCE:
+    pass 
+
+
+class Uncertainty:
+    """A generic parameter uncertainty step
+    """
+    pass
