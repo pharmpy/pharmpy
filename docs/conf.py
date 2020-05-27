7d2b14e621c9ddffd9e3022ba8ce1e89a55eed20
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2020-05-27 14:35:34 +0200

    Automatically create .nojekyll via sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.doctest',
     'sphinx.ext.extlinks',
+    'sphinx.ext.githubpages',
     'sphinx.ext.graphviz',
     'sphinx.ext.ifconfig',
     'sphinx.ext.inheritance_diagram',
