f7ad268a5e0d94161fc7203373a0be932103079f
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2020-06-16 17:05:23 +0200

    Add parameter name option to nonmem module

--- src/pharmpy/plugins/nonmem/__init__.py
+++ src/pharmpy/plugins/nonmem/__init__.py
@@ -1,3 +1,32 @@
+import pharmpy.config as config
+
 from .model import Model
+
+
+r"""
+.. list-table:: Options for the nonmem plugin
+   :widths: 25 25 50 150
+   :header-rows: 1
+
+   * - Option name
+     - Default value
+     - Type
+     - Description
+   * - ``parameter_names``
+     - ``'basic'``
+     - Str
+     - Naming scheme of NONMEM parameters. One of 'basic' and 'comment'
+
+"""
+
+
+class NONMEMConfiguration(config.Configuration):
+    parameter_names = config.ConfigItem(
+            'basic',
+            'Naming scheme of NONMEM parameters. One of "basic" and "comment"')
+
+
+conf = NONMEMConfiguration()
+
 
 __all__ = ['Model']
