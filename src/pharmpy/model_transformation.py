a46f8f4e22ff8ae5c5855c3995a770882f071153
stella.belin <stella.belin@farmbio.uu.se>
2020-08-19 10:55:21 +0200

    Method for initial estimates of new theta, calculation of mean/median is done first for each individual then total

--- src/pharmpy/model_transformation.py
+++ src/pharmpy/model_transformation.py
@@ -1,17 +1,18 @@
+import math
+
 from pharmpy.covariate_effect import CovariateEffect
 from pharmpy.parameter import Parameter
 
 
 def add_covariate_effect(model, parameter, covariate, effect, operation='*'):
-    baselines = model.dataset.pharmpy.baselines[str(covariate)]
-
-    mean = baselines.mean()
-    median = baselines.median()
+    mean = calculate_mean(model.dataset, covariate)
+    median = calculate_median(model.dataset, covariate)
 
     theta_name = str(model.create_symbol(stem='COVEFF', force_numbering=True))
+    theta_upper, theta_lower = choose_param_inits(effect, model.dataset, covariate)
 
     pset = model.parameters
-    pset.add(Parameter(theta_name, 0.1))
+    pset.add(Parameter(theta_name, theta_upper, theta_lower))
     model.parameters = pset
 
     sset = model.statements
@@ -30,6 +31,38 @@ def add_covariate_effect(model, parameter, covariate, effect, operation='*'):
     model.statements = sset
 
     return model
+
+
+def calculate_mean(df, covariate, baselines=False):
+    if baselines:
+        return df[str(covariate)].mean()
+    else:
+        return df.groupby('ID')[str(covariate)].mean().mean()
+
+
+def calculate_median(df, covariate, baselines=False):
+    if baselines:
+        return df.pharmpy.baselines[str(covariate)].median()
+    else:
+        return df.groupby('ID')[str(covariate)].median().median()
+
+
+def choose_param_inits(effect, df, covariate):
+    lower_expected = 0.1
+    upper_expected = 100
+    if effect == 'exp':
+        min_diff = df[str(covariate)].min() - calculate_median(df, covariate)
+        max_diff = df[str(covariate)].max() - calculate_median(df, covariate)
+        if min_diff == 0 or max_diff == 0:
+            return upper_expected, lower_expected
+        else:
+            upper = min(math.log(lower_expected)/min_diff,
+                        math.log(upper_expected)/max_diff)
+            lower = max(math.log(lower_expected)/max_diff,
+                        math.log(upper_expected)/min_diff)
+            return upper, lower
+    else:
+        return upper_expected, lower_expected
 
 
 def create_template(effect):
