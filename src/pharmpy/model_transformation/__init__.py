a1bfbaa846f1c47f65937c019b9749fca145547b
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2020-08-21 10:53:18 +0200

    Fix lint error

--- src/pharmpy/model_transformation/__init__.py
+++ src/pharmpy/model_transformation/__init__.py
@@ -1,5 +1,4 @@
 from pharmpy.model_transformation.covariate_effect import add_covariate_effect
 from pharmpy.model_transformation.odes import to_explicit_odes
 
-
 __all__ = ['add_covariate_effect', 'to_explicit_odes']
