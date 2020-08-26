f2accfbda5d83d6bb9539d2c4fd89adb9931a731
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2020-08-26 14:04:14 +0200

    Add first version of absorption in modeling

--- src/pharmpy/modeling/odes.py
+++ src/pharmpy/modeling/odes.py
@@ -13,3 +13,27 @@ def explicit_odes(model):
         statements[model.statements.index(odes)] = new
         model.statements = statements
     return model
+
+
+def absorption(model, order, rate):
+    """Set or change the absorption for a model
+
+        order - 0 or 1
+    """
+    statements = model.statements
+    odes = statements.ode_system
+    if not isinstance(odes, CompartmentalSystem):
+        raise ValueError("Setting absorption is not supported for ExplicitODESystem")
+
+    depot = odes.find_depot()
+    if order == 0:
+        if depot:
+            ka = odes.get_flow(depot, odes.find_central())
+            odes.remove_compartment(depot)
+            symbols = ka.free_symbols
+            for s in symbols:
+                statements.remove_symbol_definition(s, odes)
+            model.statements = statements
+            model.remove_unused_parameters_and_rvs()
+
+    return model
