d0d872191090efb810b44e21991398eafa23f182
Rikard Nordgren <rikard.nordgren@farmbio.uu.se>
2020-09-04 16:57:56 +0200

    Update of docs.sh

--- docs.sh
+++ docs.sh
@@ -1,3 +1,8 @@
 # Update documentation pharmpy.github.io before release
 
 cp -RT dist/docs/ ../pharmpy.github.io/latest/
+cd ../pharmpy.github.io
+git add -A
+git commit -m "Documentation update"
+git push
+cd ../pharmpy
