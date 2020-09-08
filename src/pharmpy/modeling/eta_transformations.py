552e953907cdf94a4f6518e0886f1b9f058267a4
stella.belin <stella.belin@farmbio.uu.se>
2020-09-08 11:25:58 +0200

    Apply function for eta transformations replaces etas with correct eta

--- src/pharmpy/modeling/eta_transformations.py
+++ src/pharmpy/modeling/eta_transformations.py
@@ -1,14 +1,49 @@
+import re
+import warnings
+
 from sympy import exp
 
 from pharmpy.statements import Assignment
-from pharmpy.symbols import real
+from pharmpy.symbols import real, subs
 
 
-def transform_etas(model, transformation, etas):
-    pass
+def transform_etas(model, transformation, list_of_etas):
+    rvs = model.random_variables
+
+    if len(list_of_etas) == 0:
+        etas = rvs.etas
+    else:
+        etas = []
+        for eta in list_of_etas:
+            try:
+                etas.append(rvs[eta])
+            except KeyError:
+                warnings.warn(f'Random variable "{eta}" does not exist')
+
+    eta_transformation = create_eta_transformation(transformation, len(etas))
+
+    etas_assignment, etas_subs = create_etas(etas)
+
+    eta_transformation.apply(etas_assignment)
+    print(eta_transformation.assignments)
+
+    return model
 
 
-def create_equation(transformation, no_of_etas):
+def create_etas(etas_original):
+    etas_subs = dict()
+    etas_assignment = dict()
+
+    for i, eta in enumerate(etas_original):
+        eta_no = int(re.findall(r'\d', eta.name)[0])
+        etas_subs[eta] = S(f'ETAB({eta_no})')
+        etas_assignment[f'etab{i + 1}'] = f'ETAB({eta_no})'
+        etas_assignment[f'eta{i + 1}'] = f'ETA({eta_no})'
+
+    return etas_assignment, etas_subs
+
+
+def create_eta_transformation(transformation, no_of_etas):
     if transformation == 'boxcox':
         return EtaTransformation.boxcox(no_of_etas)
 
@@ -17,15 +52,16 @@ class EtaTransformation:
     def __init__(self, assignments):
         self.assignments = assignments
 
-    def apply(self, eta, theta):
-        self.equation.subs({'etab': eta, 'theta': theta})
+    def apply(self, etas):
+        for assignment in self.assignments:
+            subs(assignment, etas)
 
     @classmethod
     def boxcox(cls, no_of_etas):
         assignments = []
         for i in range(1, no_of_etas + 1):
             symbol = S(f'etab{i}')
-            expression = ((exp(S(f'etab{i}'))**(S(f'theta{i}')-1)) /
+            expression = ((exp(S(f'eta{i}')**(S(f'theta{i}')-1))) /
                           (S(f'theta{i}')))
 
             assignment = Assignment(symbol, expression)
