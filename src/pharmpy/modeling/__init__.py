9df7a676164dec6bbe72ccae2b05bed5129ec085
stella.belin <stella.belin@farmbio.uu.se>
2020-09-08 16:39:07 +0200

    Add transform_etas

--- src/pharmpy/modeling/__init__.py
+++ src/pharmpy/modeling/__init__.py
@@ -1,4 +1,5 @@
 from pharmpy.modeling.covariate_effect import add_covariate_effect
+from pharmpy.modeling.eta_transformations import transform_etas
 from pharmpy.modeling.odes import absorption, explicit_odes
 
-__all__ = ['absorption', 'add_covariate_effect', 'explicit_odes']
+__all__ = ['absorption', 'add_covariate_effect', 'explicit_odes', 'transform_etas']
