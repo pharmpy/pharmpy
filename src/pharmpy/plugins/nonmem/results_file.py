61057c88c24f168a9dd1bba480bb7205f597bd4c
Kajsa Harling <kharling@users.noreply.github.com>
2020-09-14 14:59:46 +0200

    redesign parsing of NONMEM results file, focus on termination status

--- src/pharmpy/plugins/nonmem/results_file.py
+++ src/pharmpy/plugins/nonmem/results_file.py
@@ -1,65 +1,227 @@
 # import dateutil.parser
 import re
 
-# Warning! Bad and experimental code below.
+from numpy import nan
+from packaging import version
+
 # Should ideally be able to parse on demand or at least be able to skip sections
 
 
 class NONMEMResultsFile:
     '''Representing and parsing a NONMEM results file (aka lst-file)
        This is not a generic output object and will be combined by other classes
-       into new structures. For example if ext file exists we would not use the
-       estimates from the lst-file.
+       into new structures.
+       We rely on tags in NONMEM >= 7.2.0
     '''
+
     def __init__(self, path):
-        with open(str(path), 'r') as res_file:
-            content = res_file.readlines()
-        parts = self._split_content(content)
-        self._parse_datestamps(parts['start_timestamp'])
-        self.model_code = parts['model_code']
-        self.nmtran_messages = parts['nmtran_messages']
-        self._parse_version(parts['about_text'])
+        self.table = dict()
+        self.nonmem_version = None
+        for name, content in lst_blocks(path):
+            if name == 'INIT':
+                self.nonmem_version = content.pop('nonmem_version', None)
+            else:
+                self.table[name] = content
 
-    def _split_content(self, content):
-        '''First coarse parser. Splitting the file in parts that will be parsed separately
-        '''
-        parts = {}
-        parts['start_timestamp'] = content.pop(0)
+    def estimation_status(self, table_number):
+        result = unknown_termination()
+        if version.parse(self.nonmem_version) >= version.parse('7.2.0'):
+            if (table_number in self.table.keys()):
+                for key in result.keys():
+                    result[key] = self.table[table_number].get(key)
+            else:
+                result['minimization_successful'] = False
+        return result
 
-        (parts['model_code'], content) = self._split_until_regexp(
-                content, ['NM-TRAN MESSAGES', 'License Registered to:'], remove_lines=1)
+    def covariance_status(self, table_number):
+        result = {'covariance_step_ok': None}
+        if version.parse(self.nonmem_version) >= version.parse('7.2.0'):
+            if (table_number in self.table.keys()):
+                result['covariance_step_ok'] = self.table[table_number].get('covariance_step_ok')
+            else:
+                result['covariance_step_ok'] = False
+        return result
 
-        if content[0].startswith('NM-TRAN MESSAGES'):
-            (parts['nmtran_messages'], content) = \
-                    self._split_until_regexp(content, ['License Registered to:'], remove_lines=1)
-
-        (_, content) = self._split_until_regexp(content, ['1NONLINEAR MIXED EFFECTS MODEL PROGRAM'])
-        (parts['about_text'], content) = self._split_until_regexp(content, [' PROBLEM NO.:'])
-        return parts
-
-    def _split_until_regexp(self, content, regexps, remove_lines=0):
-        '''Split an array of strings into two parts. One before the match of any of the regexps and one
-        after and including the match.
-        remove_lines is the number of lines to remove at the end of the before part
-        '''
-        match = False
-        for i, line in enumerate(content):
-            for regexp in regexps:
-                if re.match(regexp, line):
-                    match = True
-            if match:
-                break
+    def ofv(self, table_number):
+        ofv = None
+        if version.parse(self.nonmem_version) >= version.parse('7.2.0'):
+            if (table_number in self.table.keys()):
+                ofv = self.table[table_number].get('OBJV')
+        if ofv is not None:
+            ofv = float(ofv)
         else:
-            raise EOFError
-        return (content[0:(i - remove_lines)], content[i:])
+            ofv = nan
+        return ofv
 
-    def _parse_version(self, about_text):
-        m = re.match(r'1NONLINEAR MIXED EFFECTS MODEL PROGRAM \(NONMEM\) VERSION (.*)\n',
-                     about_text[0])
-        if m:
-            self.nonmem_version = m.group(1)
 
-    def _parse_datestamps(self, raw_string):
-        pass  # Crap! This dateutil doesn't seem to support Swedish
-        # Had a look at dateparser that could support Swedish, but it needs patching for that
-        # self.start_timestamp = dateutil.parser.parse(raw_string)
+def _parse_datestamps(self, raw_string):
+    pass  # Crap! This dateutil doesn't seem to support Swedish
+    # Had a look at dateparser that could support Swedish, but it needs patching for that
+    # self.start_timestamp = dateutil.parser.parse(raw_string)
+
+
+def cleanup_version(v):
+    if v == 'V':
+        v = '5.0'
+    elif v == 'VI':
+        v = '6.0'
+    return v
+
+
+def parse_covariance(rows):
+    if len(rows) < 1:
+        return {'covariance_step_ok': False}
+    result = {'covariance_step_ok': False}
+
+    ok = re.compile(r' Elapsed covariance\s+time in seconds: ')  # need variable whitespace
+
+    for row in rows:
+        if ok.match(row):
+            result['covariance_step_ok'] = True
+            break
+    return result
+
+
+def unknown_termination():
+    return {'minimization_successful': None,
+            'estimate_near_boundary': None,
+            'rounding_errors': None,
+            'maxevals_exceeded': None,
+            'significant_digits': nan,
+            'function_evaluations': nan,
+            'estimation_warning': None}
+
+
+def parse_termination(rows):
+    result = unknown_termination()
+    if len(rows) < 1:  # will happen if e.g. TERMINATED BY OBJ during estimation
+        result['minimization_successful'] = False
+        return result
+    result['estimate_near_boundary'] = False
+    result['rounding_errors'] = False
+    result['maxevals_exceeded'] = False
+    result['estimation_warning'] = False
+
+    success = [re.compile(r'0MINIMIZATION SUCCESSFUL'),
+               re.compile(r' (REDUCED STOCHASTIC PORTION|OPTIMIZATION|' +
+                          r'BURN-IN|EXPECTATION ONLY PROCESS)' +
+                          r'( STATISTICALLY | WAS | )(COMPLETED|NOT TESTED)'),
+               re.compile(r'1OBJECTIVE FUNCTION IS TO BE EVALUATED')]
+    failure = [re.compile(r'0MINIMIZATION TERMINATED'),
+               re.compile(r'0SEARCH WITH ESTIMATION STEP WILL NOT PROCEED')]
+    maybe = re.compile(r' (REDUCED STOCHASTIC PORTION|OPTIMIZATION|' +
+                       r'BURN-IN|EXPECTATION ONLY PROCESS)' +
+                       r'( WAS | )NOT COMPLETED')  # success only if next line USER INTERRUPT
+    misc = {'estimate_near_boundary': re.compile(r'0(ESTIMATE OF THETA IS NEAR THE BOUNDARY AND|' +
+                                                 r'PARAMETER ESTIMATE IS NEAR ITS BOUNDARY)'),
+            'rounding_errors': re.compile(r' DUE TO ROUNDING ERRORS'),
+            'maxevals_exceeded': re.compile(r' DUE TO MAX. NO. OF FUNCTION EVALUATIONS EXCEEDED'),
+            'estimation_warning': re.compile(r' HOWEVER, PROBLEMS OCCURRED WITH THE MINIMIZATION.')}
+    sig_digits = re.compile(r' NO. OF SIG. DIGITS IN FINAL EST.:\s*(\S+)')  # only classical est
+    feval = re.compile(r' NO. OF FUNCTION EVALUATIONS USED:\s*(\S+)')  # only classical est
+
+    maybe_success = False
+    for row in rows:
+        if maybe_success:
+            result['minimization_successful'] = bool(re.search(r'USER INTERRUPT', row))
+            break
+        for p in success:
+            if p.match(row):
+                result['minimization_successful'] = True
+                break
+        if result['minimization_successful'] is not None:
+            break
+        for p in failure:
+            if p.match(row):
+                result['minimization_successful'] = False
+                break
+        if result['minimization_successful'] is not None:
+            break
+        maybe_success = bool(maybe.match(row))
+    for row in rows:
+        if sig_digits.match(row):
+            result['significant_digits'] = float(sig_digits.match(row).group(1))
+            continue
+        if feval.match(row):
+            result['function_evaluations'] = int(feval.match(row).group(1))
+            continue
+        for name, p in misc.items():
+            if p.match(row):
+                result[name] = True
+                break
+    return result
+
+
+def lst_items(path):
+    nmversion = \
+        re.compile(r'1NONLINEAR MIXED EFFECTS MODEL PROGRAM \(NONMEM\) VERSION\s+(\S+)')
+    tag = re.compile(r'\s*#([A-Z]{4}):\s*(.*)')
+    end_TERE = re.compile(r'(0|1)')  # The part we need after #TERE: will preceed next ^1 or ^0
+    cleanup = re.compile(r'\*+\s*')
+    TERM = list()
+    TERE = list()
+    found_TERM = False
+    found_TERE = False
+
+    with open(path) as file:
+        version_number = 0
+        for row in file:
+            m = nmversion.match(row)
+            if m:
+                version_number = cleanup_version(m.group(1))
+                yield ('nonmem_version', version_number)
+                break  # we will stay at current file position
+        if version.parse(version_number) >= version.parse('7.2.0'):
+            for row in file:
+                m = tag.match(row)
+                if m:
+                    if m.group(1) == 'TERM':
+                        if found_TERM:
+                            raise NotImplementedError('Two TERM tags without TERE in between')
+                        found_TERM = True
+                        TERM = list()
+                    elif m.group(1) == 'TERE':
+                        if not found_TERM:
+                            raise NotImplementedError('TERE tag without TERM tag')
+                        found_TERE = True
+                        yield ('TERM', parse_termination(TERM))
+                        found_TERM = False
+                        TERM = list()
+                    elif found_TERE:
+                        raise NotImplementedError('TERE tag without ^1 or ^0 before next tag')
+                    else:
+                        v = cleanup.sub('', m.group(2))
+                        yield (m.group(1), v.strip())
+                elif found_TERE:
+                    if end_TERE.match(row):
+                        yield('TERE', parse_covariance(TERE))
+                        found_TERE = False
+                        TERE = list()
+                    else:
+                        TERE.append(row)
+                elif found_TERM:
+                    TERM.append(row)
+            if found_TERM:
+                yield ('TERM', parse_termination(TERM))
+            if found_TERE:
+                yield ('TERE', parse_covariance(TERE))
+
+
+def lst_blocks(path):
+    block = dict()
+    table_number = 'INIT'
+    for name, content in lst_items(path):
+        if name == 'TERM' or name == 'TERE':
+            for k, v in content.items():
+                block[k] = v
+        elif name == 'TBLN':
+            if bool(block):
+                yield (table_number, block)
+            block = dict()
+            table_number = int(content)
+        else:
+            # If already set then it means TBLN was missing, probably $SIM, skip
+            if name not in block.keys():
+                block[name] = content
+    if bool(block):
+        yield (table_number, block)
--- tests/nonmem/output/test_nonmem_results_file.py
+++ tests/nonmem/output/test_nonmem_results_file.py
@@ -1,6 +1,203 @@
+from pathlib import Path
+
+import pytest
+from numpy import nan
+
 import pharmpy.plugins.nonmem.results_file as rf
+import pharmpy.plugins.nonmem.table as table
 
 
 def test_data_io(pheno_lst):
     rfile = rf.NONMEMResultsFile(pheno_lst)
     assert rfile.nonmem_version == "7.4.2"
+
+
+@pytest.mark.parametrize(
+    'file, table_number, expected, covariance_step_ok',
+    [('phenocorr.lst', 1, {'minimization_successful': True,
+                           'estimate_near_boundary': False,
+                           'rounding_errors': False,
+                           'maxevals_exceeded': False,
+                           'significant_digits': 4.9,
+                           'function_evaluations': 98,
+                           'estimation_warning': False}, True),
+     ('hessian_error.lst', 1, {'minimization_successful': False,
+                               'estimate_near_boundary': None,
+                               'rounding_errors': None,
+                               'maxevals_exceeded': None,
+                               'significant_digits': nan,
+                               'function_evaluations': nan,
+                               'estimation_warning': None}, False),
+     ('large_s_matrix_cov_fail.lst', 1, {'minimization_successful': True,
+                                         'estimate_near_boundary': False,
+                                         'rounding_errors': False,
+                                         'maxevals_exceeded': False,
+                                         'significant_digits': 3.1,
+                                         'function_evaluations': 62,
+                                         'estimation_warning': True}, False),
+     ('nm710_fail_negV.lst', 1, {'minimization_successful': None,
+                                 'estimate_near_boundary': None,
+                                 'rounding_errors': None,
+                                 'maxevals_exceeded': None,
+                                 'significant_digits': nan,
+                                 'function_evaluations': nan,
+                                 'estimation_warning': None}, None),
+     ('sparse_matrix_with_msfi.lst', 1, {'minimization_successful': True,
+                                         'estimate_near_boundary': False,
+                                         'rounding_errors': False,
+                                         'maxevals_exceeded': False,
+                                         'significant_digits': 3.1,
+                                         'function_evaluations': 112,
+                                         'estimation_warning': True}, True),
+     ('warfarin_ddmore.lst', 1, {'minimization_successful': True,
+                                 'estimate_near_boundary': False,
+                                 'rounding_errors': False,
+                                 'maxevals_exceeded': False,
+                                 'significant_digits': nan,
+                                 'function_evaluations': nan,
+                                 'estimation_warning': False}, False),
+     ('mox_fail_nonp.lst', 1, {'minimization_successful': False,
+                               'estimate_near_boundary': False,
+                               'rounding_errors': False,
+                               'maxevals_exceeded': False,
+                               'significant_digits': nan,
+                               'function_evaluations': 153,
+                               'estimation_warning': False}, False),
+     ('mox_nocov_nonp.lst', 1, {'minimization_successful': False,
+                                'estimate_near_boundary': False,
+                                'rounding_errors': False,
+                                'maxevals_exceeded': False,
+                                'significant_digits': nan,
+                                'function_evaluations': 153,
+                                'estimation_warning': False}, False),
+     ('pheno_nonp.lst', 1, {'minimization_successful': True,
+                            'estimate_near_boundary': False,
+                            'rounding_errors': False,
+                            'maxevals_exceeded': False,
+                            'significant_digits': 3.6,
+                            'function_evaluations': 107,
+                            'estimation_warning': False}, True),
+     ('theo.lst', 1, {'minimization_successful': True,
+                      'estimate_near_boundary': False,
+                      'rounding_errors': False,
+                      'maxevals_exceeded': False,
+                      'significant_digits': 4.2,
+                      'function_evaluations': 208,
+                      'estimation_warning': False}, True),
+     ('theo_nonp.lst', 1, {'minimization_successful': False,
+                           'estimate_near_boundary': True,
+                           'rounding_errors': True,
+                           'maxevals_exceeded': False,
+                           'significant_digits': nan,
+                           'function_evaluations': 735,
+                           'estimation_warning': False}, False),
+     ('theo_withcov.lst', 1, {'minimization_successful': True,
+                              'estimate_near_boundary': False,
+                              'rounding_errors': False,
+                              'maxevals_exceeded': False,
+                              'significant_digits': 4.2,
+                              'function_evaluations': 208,
+                              'estimation_warning': False}, True),
+     ('UseCase7.lst', 1, {'minimization_successful': True,
+                          'estimate_near_boundary': False,
+                          'rounding_errors': False,
+                          'maxevals_exceeded': False,
+                          'significant_digits': nan,
+                          'function_evaluations': nan,
+                          'estimation_warning': False}, False),
+     ('example6b_V7_30_beta.lst', 1, {'minimization_successful': True,
+                                      'estimate_near_boundary': False,
+                                      'rounding_errors': False,
+                                      'maxevals_exceeded': False,
+                                      'significant_digits': nan,
+                                      'function_evaluations': nan,
+                                      'estimation_warning': False}, False)
+     ]
+)
+def test_estimation_status(testdata, file, table_number, expected, covariance_step_ok):
+    p = Path(testdata / 'nonmem' / 'modelfit_results' / 'onePROB' / 'oneEST' / 'noSIM')
+    rfile = rf.NONMEMResultsFile(p / file)
+    assert rfile.estimation_status(table_number) == expected
+    if covariance_step_ok is None:
+        assert rfile.covariance_status(table_number)['covariance_step_ok'] is None
+    else:
+        assert rfile.covariance_status(table_number)['covariance_step_ok'] == covariance_step_ok
+
+
+@pytest.mark.parametrize(
+    'file, table_number, expected, covariance_step_ok',
+    [('anneal2_V7_30_beta.lst', 2, {'minimization_successful': True,
+                                    'estimate_near_boundary': False,
+                                    'rounding_errors': False,
+                                    'maxevals_exceeded': False,
+                                    'significant_digits': nan,
+                                    'function_evaluations': nan,
+                                    'estimation_warning': False}, True),
+     ('superid2_6_V7_30_beta.lst', 2, {'minimization_successful': True,
+                                       'estimate_near_boundary': False,
+                                       'rounding_errors': False,
+                                       'maxevals_exceeded': False,
+                                       'significant_digits': nan,
+                                       'function_evaluations': nan,
+                                       'estimation_warning': False}, True)
+     ]
+)
+def test_estimation_status_multest(testdata, file, table_number, expected, covariance_step_ok):
+    p = Path(testdata / 'nonmem' / 'modelfit_results' / 'onePROB' / 'multEST' / 'noSIM')
+    rfile = rf.NONMEMResultsFile(p / file)
+    assert rfile.estimation_status(table_number) == expected
+    if covariance_step_ok is None:
+        assert rfile.covariance_status(table_number)['covariance_step_ok'] is None
+    else:
+        assert rfile.covariance_status(table_number)['covariance_step_ok'] == covariance_step_ok
+
+
+def test_estimation_status_withsim(testdata):
+    p = Path(testdata / 'nonmem' / 'modelfit_results' / 'onePROB' / 'oneEST' / 'withSIM')
+    rfile = rf.NONMEMResultsFile(p / 'control3boot.res')
+
+    assert rfile.estimation_status(45) == {'minimization_successful': True,
+                                           'estimate_near_boundary': False,
+                                           'rounding_errors': False,
+                                           'maxevals_exceeded': False,
+                                           'significant_digits': 3.3,
+                                           'function_evaluations': 192,
+                                           'estimation_warning': False}
+    assert rfile.covariance_status(45)['covariance_step_ok'] is False
+
+    assert rfile.estimation_status(70) == {'minimization_successful': True,
+                                           'estimate_near_boundary': True,
+                                           'rounding_errors': False,
+                                           'maxevals_exceeded': False,
+                                           'significant_digits': 3.6,
+                                           'function_evaluations': 202,
+                                           'estimation_warning': False}
+    assert rfile.covariance_status(70)['covariance_step_ok'] is False
+
+    assert rfile.estimation_status(100) == {'minimization_successful': True,
+                                            'estimate_near_boundary': False,
+                                            'rounding_errors': False,
+                                            'maxevals_exceeded': False,
+                                            'significant_digits': 5.6,
+                                            'function_evaluations': 100,
+                                            'estimation_warning': False}
+    assert rfile.covariance_status(100)['covariance_step_ok'] is True
+
+
+def test_ofv_table_gap(testdata):
+    p = Path(testdata / 'nonmem' / 'modelfit_results' / 'multPROB' / 'multEST' / 'withSIM')
+    rfile = rf.NONMEMResultsFile(p / 'multprobmix_nm730.lst')
+
+    assert rfile.estimation_status(2) == {'minimization_successful': False,
+                                          'estimate_near_boundary': False,
+                                          'rounding_errors': False,
+                                          'maxevals_exceeded': True,
+                                          'significant_digits': nan,
+                                          'function_evaluations': 16,
+                                          'estimation_warning': False}
+
+    table_numbers = (1, 2, 3, 4, 6, 8, 10, 11, 12, 13)
+    ext_table_file = table.NONMEMTableFile(p / 'multprobmix_nm730.ext')
+
+    for n in table_numbers:
+        assert rfile.ofv(n) == pytest.approx(ext_table_file.table_no(n).final_ofv)
