422cf77e2b6df195be627b05441d66147aca72bf
stella.belin <stella.belin@farmbio.uu.se>
2020-09-15 16:29:35 +0200

    Cleaned up reference model

--- tests/modeling/test_modeling.py
+++ tests/modeling/test_modeling.py
@@ -10,37 +10,37 @@ from pharmpy.modeling import absorption, add_covariate_effect, boxcox, explicit_
 @pytest.mark.parametrize('effect, covariate, operation, buf_new', [
     ('exp', 'WGT', '*', 'WGT_MEDIAN = 1.30000\n'
                         'CLWGT = EXP((WGT - WGT_MEDIAN)*THETA(4))\n'
-                        'CL = CLWGT*TVCL*EXP(ETA(1))\n'),
+                        'CL = CLWGT*TVCL*EXP(ETA(1))'),
     ('exp', 'WGT', '+', 'WGT_MEDIAN = 1.30000\n'
                         'CLWGT = EXP((WGT - WGT_MEDIAN)*THETA(4))\n'
-                        'CL = CLWGT + TVCL*EXP(ETA(1))\n'),
+                        'CL = CLWGT + TVCL*EXP(ETA(1))'),
     ('pow', 'WGT', '*', 'WGT_MEDIAN = 1.30000\n'
                         'CLWGT = (WGT/WGT_MEDIAN)**THETA(4)\n'
-                        'CL = CLWGT*TVCL*EXP(ETA(1))\n'),
+                        'CL = CLWGT*TVCL*EXP(ETA(1))'),
     ('lin', 'WGT', '*', 'WGT_MEDIAN = 1.30000\n'
                         'CLWGT = (WGT - WGT_MEDIAN)*THETA(4) + 1\n'
-                        'CL = CLWGT*TVCL*EXP(ETA(1))\n'),
+                        'CL = CLWGT*TVCL*EXP(ETA(1))'),
     ('cat', 'FA1', '*', 'IF (FA1.EQ.1.0) THEN\n'
                         'CLFA1 = 1\n'
                         'ELSE IF (FA1.EQ.0.0) THEN\n'
                         'CLFA1 = THETA(4) + 1\n'
                         'END IF\n'
-                        'CL = CLFA1*TVCL*EXP(ETA(1))\n'),
+                        'CL = CLFA1*TVCL*EXP(ETA(1))'),
     ('piece_lin', 'WGT', '*', 'WGT_MEDIAN = 1.30000\n'
                               'IF (WGT.LE.WGT_MEDIAN) THEN\n'
                               'CLWGT = (WGT - WGT_MEDIAN)*THETA(4) + 1\n'
                               'ELSE\n'
                               'CLWGT = (WGT - WGT_MEDIAN)*THETA(5) + 1\n'
                               'END IF\n'
-                              'CL = CLWGT*TVCL*EXP(ETA(1))\n'),
+                              'CL = CLWGT*TVCL*EXP(ETA(1))'),
     ('theta - cov + median', 'WGT', '*',
      'WGT_MEDIAN = 1.30000\n'
      'CLWGT = -WGT + WGT_MEDIAN + THETA(4)\n'
-     'CL = CLWGT*TVCL*EXP(ETA(1))\n'),
+     'CL = CLWGT*TVCL*EXP(ETA(1))'),
     ('theta - cov + std', 'WGT', '*',
      'WGT_STD = 0.704565\n'
      'CLWGT = -WGT + WGT_STD + THETA(4)\n'
-     'CL = CLWGT*TVCL*EXP(ETA(1))\n')
+     'CL = CLWGT*TVCL*EXP(ETA(1))')
 ])
 def test_add_covariate_effect(pheno_path, effect, covariate, operation, buf_new):
     model = Model(pheno_path)
@@ -48,16 +48,16 @@ def test_add_covariate_effect(pheno_path, effect, covariate, operation, buf_new)
     add_covariate_effect(model, 'CL', covariate, effect, operation)
     model.update_source()
 
-    rec_ref = f'$PK\n\n\n' \
+    rec_ref = f'$PK\n' \
               f'IF(AMT.GT.0) BTIME=TIME\n' \
               f'TAD=TIME-BTIME\n' \
-              f'      TVCL=THETA(1)*WGT\n' \
-              f'      TVV=THETA(2)*WGT\n' \
+              f'TVCL=THETA(1)*WGT\n' \
+              f'TVV=THETA(2)*WGT\n' \
               f'IF(APGR.LT.5) TVV=TVV*(1+THETA(3))\n' \
-              f'      CL=TVCL*EXP(ETA(1))\n' \
-              f'{buf_new}' \
-              f'      V=TVV*EXP(ETA(2))\n' \
-              f'      S1=V\n'
+              f'CL=TVCL*EXP(ETA(1))\n' \
+              f'{buf_new}\n' \
+              f'V=TVV*EXP(ETA(2))\n' \
+              f'S1=V\n\n'
 
     assert str(model.get_pred_pk_record()) == rec_ref
 
@@ -84,9 +84,10 @@ def test_to_explicit_odes(pheno_path, testdata):
     explicit_odes(model)
     model.update_source()
     lines = str(model).split('\n')
-    assert lines[6] == '$MODEL TOL=3 COMPARTMENT=(CENTRAL DEFDOSE)'
-    assert lines[18] == '$DES'
-    assert lines[19] == 'DADT(1) = -A(1)*CL/V'
+    print(lines)
+    assert lines[5] == '$MODEL TOL=3 COMPARTMENT=(CENTRAL DEFDOSE)'
+    assert lines[16] == '$DES'
+    assert lines[17] == 'DADT(1) = -A(1)*CL/V'
 
     model = Model(testdata / 'nonmem' / 'modeling' / 'pheno_advan1_zero_order.mod')
     explicit_odes(model)
@@ -162,7 +163,7 @@ def test_absorption(testdata):
 
 @pytest.mark.parametrize('etas, etab, buf_new', [
     (['ETA(1)'], 'ETAB1 = (EXP(ETA(1))**THETA(4) - 1)/THETA(4)',
-     'CL = TVCL*EXP(ETAB1)\n      V=TVV*EXP(ETA(2))'),
+     'CL = TVCL*EXP(ETAB1)\nV=TVV*EXP(ETA(2))'),
     (['ETA(1)', 'ETA(2)'], 'ETAB1 = (EXP(ETA(1))**THETA(4) - 1)/THETA(4)\n'
                            'ETAB2 = (EXP(ETA(2))**THETA(5) - 1)/THETA(5)',
      'CL = TVCL*EXP(ETAB1)\nV = TVV*EXP(ETAB2)'),
@@ -170,7 +171,7 @@ def test_absorption(testdata):
            'ETAB2 = (EXP(ETA(2))**THETA(5) - 1)/THETA(5)',
      'CL = TVCL*EXP(ETAB1)\nV = TVV*EXP(ETAB2)'),
     (['eta(1)'], 'ETAB1 = (EXP(ETA(1))**THETA(4) - 1)/THETA(4)',
-     'CL = TVCL*EXP(ETAB1)\n      V=TVV*EXP(ETA(2))')
+     'CL = TVCL*EXP(ETAB1)\nV=TVV*EXP(ETA(2))')
 ])
 def test_boxcox(pheno_path, etas, etab, buf_new):
     model = Model(pheno_path)
@@ -178,15 +179,15 @@ def test_boxcox(pheno_path, etas, etab, buf_new):
     boxcox(model, etas)
     model.update_source()
 
-    rec_ref = f'$PK\n\n\n' \
+    rec_ref = f'$PK\n' \
               f'{etab}\n' \
               f'IF(AMT.GT.0) BTIME=TIME\n' \
               f'TAD=TIME-BTIME\n' \
-              f'      TVCL=THETA(1)*WGT\n' \
-              f'      TVV=THETA(2)*WGT\n' \
+              f'TVCL=THETA(1)*WGT\n' \
+              f'TVV=THETA(2)*WGT\n' \
               f'IF(APGR.LT.5) TVV=TVV*(1+THETA(3))\n' \
               f'{buf_new}\n' \
-              f'      S1=V\n'
+              f'S1=V\n\n'
 
     assert str(model.get_pred_pk_record()) == rec_ref
     assert model.parameters['lambda1'].init == 0.01
@@ -215,16 +216,16 @@ def test_tdist(pheno_path):
     expression = f'(({num_1})/({denom_1}) + ({num_2})/({denom_2}) + ' \
                  f'({num_3})/({denom_3}) + 1)*ETA(1)'
 
-    rec_ref = f'$PK\n\n\n' \
+    rec_ref = f'$PK\n' \
               f'{symbol} = {expression}\n' \
               f'IF(AMT.GT.0) BTIME=TIME\n' \
               f'TAD=TIME-BTIME\n' \
-              f'      TVCL=THETA(1)*WGT\n' \
-              f'      TVV=THETA(2)*WGT\n' \
+              f'TVCL=THETA(1)*WGT\n' \
+              f'TVV=THETA(2)*WGT\n' \
               f'IF(APGR.LT.5) TVV=TVV*(1+THETA(3))\n' \
               f'CL = TVCL*EXP(ETAT1)\n' \
-              f'      V=TVV*EXP(ETA(2))\n' \
-              f'      S1=V\n'
+              f'V=TVV*EXP(ETA(2))\n' \
+              f'S1=V\n\n'
 
     assert str(model.get_pred_pk_record()) == rec_ref
     assert model.parameters['df1'].init == 80
