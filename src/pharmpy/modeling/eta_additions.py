0a1b5774c10883df8dcc50122d13ee563c554dea
Stella <stella.belin@farmaci.uu.se>
2020-09-18 13:22:30 +0200

    File for adding etas

---
+++ src/pharmpy/modeling/eta_additions.py
@@ -0,0 +1,7 @@
+def add_etas(model, parameter, expression, operation):
+    # Create eta
+    # Create omega
+    # Create expression
+    # Substitute parameter expression with new expression
+
+    pass
