7c4d209ee1d6c1034c1958cafcf4fc8cff92883d
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2020-10-08 16:49:18 +0200

    Bump version: 0.7.0 → 0.8.0

--- src/pharmpy/__init__.py
+++ src/pharmpy/__init__.py
@@ -9,7 +9,7 @@ Definitions
 ===========
 """
 
-__version__ = '0.7.0'
+__version__ = '0.8.0'
 
 import logging
 
--- src/pharmpy/cli.py
+++ src/pharmpy/cli.py
@@ -875,7 +875,7 @@ parser = argparse.ArgumentParser(
     formatter_class=formatter,
     allow_abbrev=True,
 )
-parser.add_argument('--version', action='version', version='0.7.0')
+parser.add_argument('--version', action='version', version='0.8.0')
 
 # subcommand parsers
 subparsers = parser.add_subparsers(title='Pharmpy commands', metavar='COMMAND')
