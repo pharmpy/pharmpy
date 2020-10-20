3e88258c4581bdd738923e39c559440c5dce9f76
Stella <stella.belin@farmaci.uu.se>
2020-10-20 14:51:07 +0200

    Adjusted margins for results report (html)

--- src/pharmpy/reporting/conf.py
+++ src/pharmpy/reporting/conf.py
@@ -2,5 +2,8 @@ extensions = ['altair.sphinxext.altairplot']
 html_sidebars = {
    '**': ['localtoc.html'],
 }
+html_theme_options = {
+    'page_width': '1750px'
+}
 html_title = 'Results'
 master_doc = 'results'
