1fd384f1957479506303cad95ee02606d5f8e6ca
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2020-10-22 13:39:21 +0200

    Add reading boxcox and tdist results for qa

--- src/pharmpy/methods/qa/results.py
+++ src/pharmpy/methods/qa/results.py
@@ -1,46 +1,87 @@
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 import pharmpy.random_variables
+import pharmpy.symbols
 from pharmpy import Model
 from pharmpy.random_variables import VariabilityLevel
 from pharmpy.results import Results
 
 
 class QAResults(Results):
-    def __init__(self, dofv=None, fullblock_parameters=None):
+    def __init__(
+        self, dofv=None, fullblock_parameters=None, boxcox_parameters=None, tdist_parameters=None
+    ):
         self.dofv = dofv
         self.fullblock_parameters = fullblock_parameters
+        self.boxcox_parameters = boxcox_parameters
+        self.tdist_parameters = tdist_parameters
 
 
-def calculate_results(original_model, fullblock_model=None):
+def calculate_results(original_model, fullblock_model=None, boxcox_model=None, tdist_model=None):
     fullblock_table, fullblock_dofv = calc_fullblock(original_model, fullblock_model)
-    dofv_table = fullblock_dofv
-    res = QAResults(dofv=dofv_table, fullblock_parameters=fullblock_table)
+    boxcox_table, boxcox_dofv = calc_transformed_etas(
+        original_model, boxcox_model, 'boxcox', 'lambda'
+    )
+    tdist_table, tdist_dofv = calc_transformed_etas(original_model, tdist_model, 'tdist', 'df')
+    dofv_table = pd.concat([fullblock_dofv, boxcox_dofv, tdist_dofv])
+    res = QAResults(
+        dofv=dofv_table,
+        fullblock_parameters=fullblock_table,
+        boxcox_parameters=boxcox_table,
+        tdist_parameters=tdist_table,
+    )
     return res
 
 
-def calc_boxcox(original_model, boxcox_model=None):
+def calc_transformed_etas(original_model, new_model, transform_name, parameter_name):
     """Retrieve new and old parameters of boxcox"""
-    if boxcox_model is None:
-        return None
+    dofv_tab = pd.DataFrame({'dofv': np.nan, 'df': np.nan}, index=[transform_name])
+    if new_model is None:
+        return None, dofv_tab
     origres = original_model.modelfit_results
-    boxcoxres = boxcox_model.modelfit_results
-    if boxcoxres is None:
-        return None
-    boxcoxres.random_variables.etas
-    origres
+    newres = new_model.modelfit_results
+    if newres is None:
+        return None, dofv_tab
+    params = new_model.random_variables.variance_parameters(exclude_level=VariabilityLevel.RUV)
+    params = [pharmpy.symbols.symbol(p.name) for p in params]
+    origres.reparameterize(
+        [
+            pharmpy.random_variables.NormalParametrizationSd,
+            pharmpy.random_variables.MultivariateNormalParametrizationSdCorr,
+        ]
+    )
+    newres.reparameterize(
+        [
+            pharmpy.random_variables.NormalParametrizationSd,
+            pharmpy.random_variables.MultivariateNormalParametrizationSdCorr,
+        ]
+    )
+    boxcox_sds = [newres.parameter_estimates[p.name] for p in params]
+    orig_sds = [origres.parameter_estimates[p.name] for p in params]
+    thetas = newres.parameter_estimates[0 : len(params)]
+    eta_names = [rv.name for rv in new_model.random_variables.etas]
+
+    table = pd.DataFrame(
+        {parameter_name: thetas.values, 'new_sd': boxcox_sds, 'old_sd': orig_sds}, index=eta_names
+    )
+
+    dofv = origres.ofv - newres.ofv
+    dofv_tab = pd.DataFrame({'dofv': dofv, 'df': len(eta_names)}, index=[transform_name])
+    return table, dofv_tab
 
 
 def calc_fullblock(original_model, fullblock_model):
     """Retrieve new and old parameters of full block"""
+    dofv_tab = pd.DataFrame({'dofv': np.nan, 'df': np.nan}, index=['fullblock'])
     if fullblock_model is None:
-        return None
+        return None, dofv_tab
     origres = original_model.modelfit_results
     fullres = fullblock_model.modelfit_results
     if fullres is None:
-        return None
+        return None, dofv_tab
     _, dist = list(fullblock_model.random_variables.distributions(level=VariabilityLevel.IIV))[0]
     fullblock_parameters = [str(symb) for symb in dist.free_symbols]
     origres.reparameterize(
@@ -83,6 +124,21 @@ def psn_qa_results(path):
         fullblock_model = Model(fullblock_path)
     else:
         fullblock_model = None
+    boxcox_path = path / 'modelfit_run' / 'boxcox.mod'
+    if boxcox_path.is_file():
+        boxcox_model = Model(boxcox_path)
+    else:
+        boxcox_model = None
+    tdist_path = path / 'modelfit_run' / 'tdist.mod'
+    if tdist_path.is_file():
+        tdist_model = Model(tdist_path)
+    else:
+        tdist_model = None
 
-    res = calculate_results(original_model, fullblock_model=fullblock_model)
+    res = calculate_results(
+        original_model,
+        fullblock_model=fullblock_model,
+        boxcox_model=boxcox_model,
+        tdist_model=tdist_model,
+    )
     return res
--- tests/methods/test_qa.py
+++ tests/methods/test_qa.py
@@ -21,3 +21,42 @@ def test_fullblock(testdata):
 
     assert res.dofv['dofv']['fullblock'] == pytest.approx(730.89472681373070 - 706.36113798726512)
     assert res.dofv['df']['fullblock'] == 1
+
+    res = calculate_results(orig, fullblock_model=None)
+    assert res.fullblock_parameters is None
+
+
+def test_boxcox(testdata):
+    orig = Model(testdata / 'nonmem' / 'pheno.mod')
+    bc = Model(testdata / 'nonmem' / 'qa' / 'boxcox.mod')
+    res = calculate_results(orig, boxcox_model=bc)
+    correct = """lambda,new_sd,old_sd
+ETA(1),-1.581460,0.296257,0.333246
+ETA(2),0.645817,0.429369,0.448917
+"""
+    correct = pd.read_csv(StringIO(correct), index_col=[0])
+    pd.testing.assert_frame_equal(res.boxcox_parameters, correct)
+
+    assert res.dofv['dofv']['boxcox'] == pytest.approx(730.89472681373070 - 721.78812733817688)
+    assert res.dofv['df']['boxcox'] == 2
+
+    res = calculate_results(orig, boxcox_model=None)
+    assert res.boxcox_parameters is None
+
+
+def test_tdist(testdata):
+    orig = Model(testdata / 'nonmem' / 'pheno.mod')
+    td = Model(testdata / 'nonmem' / 'qa' / 'tdist.mod')
+    res = calculate_results(orig, tdist_model=td)
+    correct = """df,new_sd,old_sd
+ETA(1),3.77,0.344951,0.333246
+ETA(2),3.77,0.400863,0.448917
+"""
+    correct = pd.read_csv(StringIO(correct), index_col=[0])
+    pd.testing.assert_frame_equal(res.tdist_parameters, correct)
+
+    assert res.dofv['dofv']['tdist'] == pytest.approx(730.89472681373070 - 729.45800311609150)
+    assert res.dofv['df']['tdist'] == 2
+
+    res = calculate_results(orig, tdist_model=None)
+    assert res.tdist_parameters is None
