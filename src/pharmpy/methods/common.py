092cbd738492bbc6cbcebff81b54a4c5ac38cbf1
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2020-11-02 11:09:32 +0100

    Support paths for run directories

--- src/pharmpy/methods/common.py
+++ src/pharmpy/methods/common.py
@@ -2,18 +2,23 @@ from pathlib import Path
 
 
 class RunDirectory:
-    def __init__(self, method_name):
+    def __init__(self, method_name, path=None):
         i = 1
         while True:
             name = f'{method_name}_dir{i}'
-            path = Path(name)
+            if path is not None:
+                path = path / name
+            else:
+                path = Path(name)
             if not path.is_file():
                 path.mkdir()
                 self.path = path
+                self.models_path = path / 'models'
+                self.models_path.mkdir()
                 break
             i += 1
 
 
 class Method:
-    def __init__(self):
-        self.rundir = RunDirectory(type(self).__name__.lower())
+    def __init__(self, path=None):
+        self.rundir = RunDirectory(type(self).__name__.lower(), path=path)
