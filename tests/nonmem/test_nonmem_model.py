bee37304c418371e7e05802683d6d36004a9fff2
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2020-12-02 12:24:47 +0100

    Add unit test for thetas having same label

--- tests/nonmem/test_nonmem_model.py
+++ tests/nonmem/test_nonmem_model.py
@@ -457,9 +457,7 @@ def test_clashing_parameter_names(datadir):
     with ConfigurationContext(conf, parameter_names='comment'):
         model = Model(datadir / 'pheno_clashing_symbols.mod')
         with pytest.warns(UserWarning):
-            print("START!")
             model.statements
-            print("END!")
         assert model.parameters.names == ['THETA(1)', 'TVV', 'IVCL', 'OMEGA(2,2)', 'SIGMA(1,1)']
 
         code = """$PROBLEM base model
@@ -477,6 +475,21 @@ $ESTIMATION METHOD=1 INTER MAXEVALS=9990 PRINT=2 POSTHOC
         model = Model(StringIO(code))
         with pytest.warns(UserWarning):
             assert model.parameters.names == ['TV', 'OMEGA(1,1)', 'SIGMA(1,1)']
+
+        code = """$PROBLEM base model
+$INPUT ID DV TIME
+$DATA file.csv IGNORE=@
+
+$PRED
+Y = THETA(1) + THETA(2)
+
+$THETA 0.1  ; TV
+$THETA 0.1  ; TV
+$ESTIMATION METHOD=1 INTER MAXEVALS=9990 PRINT=2 POSTHOC
+"""
+        model = Model(StringIO(code))
+        with pytest.warns(UserWarning):
+            assert model.parameters.names == ['TV', 'THETA(2)']
 
 
 def test_dv_symbol(pheno_path):
