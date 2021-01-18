cd720841008d26adf1b6cf4c92ee28e380fe6958
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-01-18 09:18:35 +0100

    Add more sections to the nlmixr generation

--- src/pharmpy/plugins/nlmixr/model.py
+++ src/pharmpy/plugins/nlmixr/model.py
@@ -1,4 +1,5 @@
 import pharmpy.model
+from pharmpy.statements import Assignment
 
 
 class CodeGenerator:
@@ -35,8 +36,29 @@ def create_ini(cg, model):
     """Create the nlmixr ini section code"""
     cg.add('ini({')
     cg.indent()
+    thetas = [p for p in model.parameters if p.symbol not in model.random_variables.free_symbols]
+    for theta in thetas:
+        theta_name = theta.name.replace('(', '').replace(')', '')
+        cg.add(f'{theta_name} <- {theta.init}')
     cg.dedent()
     cg.add('})')
+
+
+def create_model(cg, model):
+    """Create the nlmixr model section code"""
+    cg.add('model({')
+    cg.indent()
+    for s in model.statements:
+        if isinstance(s, Assignment):
+            cg.add(f'{s.symbol.name} <- {str(s.expression)}')
+    cg.dedent()
+    cg.add('})')
+
+
+def create_fit(cg, model):
+    """Create the call to fit"""
+    cg.add(f'fit <- nlmixr({model.name}, dataset, "saem"')
+    cg.add('print(fit)')
 
 
 class Model(pharmpy.model.Model):
@@ -47,8 +69,11 @@ class Model(pharmpy.model.Model):
         cg.add(f'{self.name} <- function() {{')
         cg.indent()
         create_ini(cg, self)
+        create_model(cg, self)
         cg.dedent()
         cg.add('}')
+        cg.empty_line()
+        create_fit(cg, self)
         self._src = str(cg)
 
     def __str__(self):
