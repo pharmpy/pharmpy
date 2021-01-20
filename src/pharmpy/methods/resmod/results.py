2baf486eb40b451c969e28752f9ef6e774883707
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-01-20 11:08:49 +0100

    Skeleton for resmod results

---
+++ src/pharmpy/methods/resmod/results.py
@@ -0,0 +1,19 @@
+from pharmpy.results import Results
+
+
+class ResmodResults(Results):
+    """Resmod results class"""
+
+    def __init__(self):
+        pass
+
+
+def calculate_results():
+    """Calculate resmod results"""
+    res = ResmodResults()
+    return res
+
+
+def psn_resmod_results():
+    res = calculate_results()
+    return res
