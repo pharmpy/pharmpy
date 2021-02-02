676bf142014d15ea84b7dedf4d8e23a3ca67cb02
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-02-02 09:33:43 +0100

    Remove old documentation build script

--- docs.sh
+++
@@ -1,8 +0,0 @@
-# Update documentation pharmpy.github.io before release
-
-cp -RT dist/docs/ ../pharmpy.github.io/latest/
-cd ../pharmpy.github.io
-git add -A
-git commit -m "Documentation update"
-git push
-cd ../pharmpy
