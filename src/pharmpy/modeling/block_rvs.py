Init for covariance where RV does not have individual estimates will fall back to default init (fixes #94), keep same order in RV-block as in full RVs and corrected test for create_rv_block

src/pharmpy/modeling/block_rvs.py
@@ -29,15 +29,19 @@ def create_rv_block(model, list_of_rvs=None):
         raise ValueError('At least two random variables are needed')
 
     rvs_full = model.random_variables
-    rvs_block = _get_rvs(model, list_of_rvs)
+    rvs = _get_rvs(model, list_of_rvs)
 
     if list_of_rvs is not None:
-        for rv in rvs_block:
+        for rv in rvs:
             if isinstance(rv.pspace.distribution, MultivariateNormalDistribution):
-                if rvs_full.get_rvs_from_same_dist(rv):
-                    rv_extracted = rvs_full.extract_from_block(rv.name)
-                    rvs_block.discard(rv)
-                    rvs_block.add(rv_extracted)
+                rvs_same_dist = rvs_full.get_rvs_from_same_dist(rv)
+                if not all(rv in rvs for rv in rvs_same_dist):
+                    rvs_full.extract_from_block(rv)
+
+    rvs_block = RandomVariables()
+    for rv in rvs_full:
+        if rv.name in [rv.name for rv in rvs]:
+            rvs_block.add(rv)
 
     pset = _merge_rvs(model, rvs_block)
 
@@ -165,13 +169,19 @@ def _choose_param_init(model, rvs, params):
             etas.append(rvs_names[i])
 
     sd = np.array([np.sqrt(params[0].init), np.sqrt(params[1].init)])
+    init_default = round(0.1 * sd[0] * sd[1], 7)
 
     if res is not None:
-        ie = res.individual_estimates
+        try:
+            ie = res.individual_estimates
+            if not all(eta in ie.columns for eta in etas):
+                return init_default
+        except KeyError:
+            return init_default
         eta_corr = ie[etas].corr()
         cov = math.corr2cov(eta_corr.to_numpy(), sd)
         cov[cov == 0] = 0.0001
         cov = math.nearest_posdef(cov)
         return round(cov[1][0], 7)
     else:
-        return round(0.1 * sd[0] * sd[1], 7)
+        return init_default