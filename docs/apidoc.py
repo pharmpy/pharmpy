276a0699a508332fc78e7b8a2e237e2d59e38054
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-03-12 15:04:55 +0100

    Fix diagram warnings when building documentation

--- docs/apidoc.py
+++ docs/apidoc.py
@@ -12,12 +12,12 @@ import pharmpy
 
 pharmpy_path = Path(pharmpy.__file__).parent
 
-# Remove old rst files to make sure no
+# Remove old rst files to make sure no old lingers
 os.system('rm docs/reference/*.rst')
 
 exclude = []
 noclass = []
-for _, modname, _ in pkgutil.walk_packages([Path('src/pharmpy')], 'pharmpy.'):
+for _, modname, is_pkg in pkgutil.walk_packages([Path('src/pharmpy')], 'pharmpy.'):
     module = importlib.import_module(modname)
     if hasattr(module, '__doc__') and module.__doc__ is not None and re.search(':meta private:', module.__doc__):
         path = Path(module.__file__).relative_to(pharmpy_path)
@@ -30,13 +30,14 @@ for _, modname, _ in pkgutil.walk_packages([Path('src/pharmpy')], 'pharmpy.'):
                 if qualname.startswith(module.__name__):
                     no_classes = False
                     break
-        if no_classes:
+        if no_classes or is_pkg:        # No classes in subpackages to get rid of warnings about inheritance diagrams
             noclass.append(module.__name__)
+
+noclass.append('pharmpy')
 
 cmd = f'sphinx-apidoc -e -f -d 2 -o docs/reference src/pharmpy {" ".join(exclude)}'
 
 os.system(cmd)
-
 
 for path in Path('docs/reference').glob('pharmpy*.rst'):
     documented_module = str(path.name)[0:-4]
