67f18b25f075cfcd831996a5e7294a3ed87c2b50
Stella <stella.belin@farmaci.uu.se>
2021-03-29 10:58:59 +0200

    Removed extra space in to_dataframe docstring (caused error)

--- src/pharmpy/parameter.py
+++ src/pharmpy/parameter.py
@@ -86,7 +86,6 @@ class ParameterSet(OrderedSet):
         >>> par2 = Parameter("V", 10, lower=0, upper=100)
         >>> pset = ParameterSet([par1, par2])
         >>> pset.to_dataframe()
-
             value  lower  upper    fix
         CL      1      0     10  False
         V      10      0    100  False
