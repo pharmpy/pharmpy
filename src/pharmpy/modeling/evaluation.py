7908fec7e825f86b9ff0e0838ef23ec946780dcc
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-04-29 16:04:17 +0200

    Add docstring to evaluate_expression

--- src/pharmpy/modeling/evaluation.py
+++ src/pharmpy/modeling/evaluation.py
@@ -3,6 +3,23 @@ import sympy
 
 
 def evaluate_expression(model, expression):
+    """Evaluate expression using model
+
+    Calculate the value of expression for each data record.
+    The expression can contain dataset columns, variables in model and
+    population parameters. If the model has parameter estimates these
+    will be used. Initial estimates will be used for non-estimated parameters.
+
+    Parameters
+    ----------
+    expression : str or sympy expression
+        Expression to evaluate
+
+    Returns
+    -------
+    pd.Series
+        A series of one evaluated value for each data record
+    """
     expression = sympy.sympify(expression)
     full_expr = model.statements.full_expression_from_odes(expression)
     pe = model.modelfit_results.parameter_estimates
