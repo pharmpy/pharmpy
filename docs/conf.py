e125175919054737fc693128030dce229ca43fd5
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-05-03 11:30:00 +0200

    Bump version: 0.23.3 → 0.23.4

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ project = 'Pharmpy'
 year = '2018-2021'
 authors = ['the Pharmpy development team']
 copyright = '{0}; {1}'.format(year, ', '.join(authors))
-version = release = '0.23.3'
+version = release = '0.23.4'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- src/pharmpy/__init__.py
+++ src/pharmpy/__init__.py
@@ -9,7 +9,7 @@ Definitions
 ===========
 """
 
-__version__ = '0.23.3'
+__version__ = '0.23.4'
 
 import logging
 
