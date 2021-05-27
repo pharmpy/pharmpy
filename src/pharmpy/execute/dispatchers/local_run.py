56eb1b257b3b11453145459fe42a986acc670a28
Stella <stella.belin@farmaci.uu.se>
2021-05-27 11:47:12 +0200

    Use multiprocessing instead of threads

--- src/pharmpy/execute/dispatchers/local_run.py
+++ src/pharmpy/execute/dispatchers/local_run.py
@@ -1,8 +1,10 @@
 from dask.diagnostics import ProgressBar
-from dask.threaded import get
+from dask.multiprocessing import get
 
 
 def run(workflow):
     with ProgressBar(dt=1):
+        # FIXME: issue with NONMEM license file, use multiprocessing instead of threads
+        #  as temporary solution
         res = get(workflow, 'results')
     return res
