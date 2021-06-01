6caa795b1f7adb242f795173d8c778da94d2426c
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-06-01 15:30:41 +0200

    Switch to using dask distributed

--- src/pharmpy/execute/dispatchers/local_run.py
+++ src/pharmpy/execute/dispatchers/local_run.py
@@ -1,11 +1,7 @@
-# from dask.diagnostics import ProgressBar
-from dask.multiprocessing import get
-
-# from dask.distributed import Client
+from dask.distributed import Client
 
 
 def run(workflow):
-    # with Client() as client:
-    #    res = client.get(workflow, 'results')
-    res = get(workflow, 'results')
+    with Client() as client:
+        res = client.get(workflow, 'results')
     return res
