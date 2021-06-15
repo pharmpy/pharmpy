28545674f8b33cf5ddadecf9e7acb9deb4d7633c
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-06-15 12:44:04 +0200

    Docstrings for ninds and nobs

--- src/pharmpy/modeling/data.py
+++ src/pharmpy/modeling/data.py
@@ -2,8 +2,12 @@
 
 
 def ninds(model):
+    """Retrieve the number of individuals in the model dataset
+    """
     return len(model.dataset.pharmpy.ids)
 
 
 def nobs(model):
+    """Retrieve the total number of observations in the model dataset
+    """
     return len(model.dataset.pharmpy.observations)
