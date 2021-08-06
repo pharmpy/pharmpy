fcb544f2c43e3c26295aaf9f56f3cbc4ac72f4b3
Stella <stella.belin@farmaci.uu.se>
2021-08-06 14:58:02 +0200

    First test of exhaustive stepwise algorithm

---
+++ tests/integration/test_modelsearch.py
@@ -0,0 +1,21 @@
+import shutil
+
+from pharmpy import Model
+from pharmpy.modeling import first_order_absorption, first_order_elimination
+from pharmpy.tools.modelsearch import ModelSearch
+from pharmpy.utils import TemporaryDirectoryChanger
+
+
+def test_exhaustive_stepwise(tmp_path, testdata):
+    with TemporaryDirectoryChanger(tmp_path):
+        shutil.copy2(testdata / 'nonmem' / 'models' / 'mox2.mod', tmp_path)
+        shutil.copy2(testdata / 'nonmem' / 'models' / 'mx19B_ZO.csv', tmp_path)
+        model = Model('mox2.mod')
+        model.dataset_path = tmp_path / 'mx19B_ZO.csv'
+
+        first_order_absorption(model)
+        first_order_elimination(model)
+        model.update_source(force=True)
+
+        res = ModelSearch(model, 'exhaustive_stepwise', 'ABSORPTION(ZO)\nPERIPHERALS(1)').run()
+        assert res
