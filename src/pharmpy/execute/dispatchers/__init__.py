638f09e8c208b2502e64c4772d803987062f5108
Stella <stella.belin@farmaci.uu.se>
2021-08-09 11:13:46 +0200

    Use dask threaded for Windows, dask distributed for unix-based, allow config of dispatcher

--- src/pharmpy/execute/dispatchers/__init__.py
+++ src/pharmpy/execute/dispatchers/__init__.py
@@ -1,3 +1,18 @@
+import pharmpy.config as config
+
 from .local import LocalDispatcher
+
+
+class DispatcherConfiguration(config.Configuration):
+    module = 'pharmpy.execute.dispatchers'
+    dask_dispatcher = config.ConfigItem(
+        None,
+        'Which type of dask scheduler to use (supports threaded and distributed).',
+        str,
+    )
+
+
+conf = DispatcherConfiguration()
+
 
 __all__ = ['LocalDispatcher']
--- src/pharmpy/execute/dispatchers/local_run.py
+++ src/pharmpy/execute/dispatchers/local_run.py
@@ -1,7 +1,22 @@
+import os
+
 from dask.distributed import Client
+from dask.threaded import get
+
+import pharmpy.execute.dispatchers
 
 
 def run(workflow):
-    with Client(threads_per_worker=1) as client:
-        res = client.get(workflow, 'results')
+    if pharmpy.execute.dispatchers.conf.dask_dispatcher:
+        dask_dispatcher = pharmpy.execute.dispatchers.conf.dask_dispatcher
+    elif os.name == 'nt':
+        dask_dispatcher = 'threaded'
+    else:
+        dask_dispatcher = 'distributed'
+
+    if dask_dispatcher == 'threaded':
+        res = get(workflow, 'results')
+    else:
+        with Client(threads_per_worker=1) as client:
+            res = client.get(workflow, 'results')
     return res
