298b8a96447a05e5da060ddfdc17cba795112c94
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-08-24 15:35:46 +0200

    Change name of argument in remove_iiv
    From list_to_remove to to_remove

--- src/pharmpy/modeling/remove_iiv.py
+++ src/pharmpy/modeling/remove_iiv.py
@@ -6,7 +6,7 @@ from pharmpy.modeling.help_functions import _format_input_list, _get_etas
 from pharmpy.symbols import symbol as S
 
 
-def remove_iiv(model, list_to_remove=None):
+def remove_iiv(model, to_remove=None):
     """
     Removes all IIV omegas given a list with eta names and/or parameter names.
 
@@ -14,13 +14,13 @@ def remove_iiv(model, list_to_remove=None):
     ----------
     model : Model
         Pharmpy model to create block effect on.
-    list_to_remove : str, list
+    to_remove : str, list
         Name/names of etas and/or name/names of individual parameters to remove.
         If None, all etas that are IIVs will be removed. None is default.
     """
     rvs, sset = model.random_variables, model.statements
-    list_to_remove = _format_input_list(list_to_remove)
-    etas = _get_etas(model, list_to_remove, include_symbols=True)
+    to_remove = _format_input_list(to_remove)
+    etas = _get_etas(model, to_remove, include_symbols=True)
 
     for eta in etas:
         statement = sset.find_assignment(eta.name, is_symbol=False)
