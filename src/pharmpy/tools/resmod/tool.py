d6ee23d625fb05d8f4be99cfae4c6bcb7674608f
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-09-01 15:16:48 +0200

    Run models in resmod

--- src/pharmpy/tools/resmod/tool.py
+++ src/pharmpy/tools/resmod/tool.py
@@ -22,15 +22,28 @@ class Resmod(pharmpy.tools.Tool):
         return res
 
     def create_workflow(self):
+        self.model.database = (
+            self.database.model_database
+        )  # FIXME: Not right! Changes the user model object
         wf = Workflow()
-        task_create_models = Task('create_models', create_models, self.model, final_task=True)
-        wf.add_tasks(task_create_models)
-        # wf_fit = self.workflow_creator()
-        # wf.add_tasks(wf_fit, connect=True)
+        task_base_model = Task('create_base_model', _create_base_model, self.model)
+        task_iiv = Task('create_iiv_on_ruv_model', _create_iiv_on_ruv_model)
+        wf.add_tasks(task_base_model)
+        wf.add_tasks(task_iiv, connect=True)
+        # task_create_models = Task('create_models', create_models, self.model, final_task=False)
+        # wf.add_tasks(task_create_models)
+        wf_fit = self.workflow_creator()
+        wf.add_tasks(wf_fit, connect=True)
+        task_results = Task('results', post_process, final_task=True)
+        wf.add_tasks(task_results, connect=True)
         return wf
 
 
-def _create_base_model(dataset):
+def post_process(models):
+    return models
+
+
+def _create_base_model(input_model):
     base_model = pharmpy.model.Model()
     theta = Parameter('theta', 0.1)
     omega = Parameter('omega', 0.01, lower=0)
@@ -49,13 +62,16 @@ def _create_base_model(dataset):
 
     base_model.dependent_variable = y.symbol
     base_model.name = "base"
-    base_model.dataset = dataset
+    base_model.dataset = _create_dataset(input_model)
+    base_model.database = input_model.database
     return base_model
 
 
 def _create_iiv_on_ruv_model(base_model):
     model = base_model.copy()
+    model.database = base_model.database  # FIXME: Should be unnecessary
     set_iiv_on_ruv(model)
+    model.name = 'iiv_on_ruv'
     return model
 
 
@@ -68,13 +84,13 @@ def _create_power_model(base_model):
 
 def _create_dataset(input_model):
     residuals = input_model.modelfit_results.residuals
-    df = residuals['CWRES'].reset_index()
+    df = residuals['CWRES'].reset_index().rename(columns={'CWRES': 'DV'})
     return df
 
 
-def create_models(input_model):
-    dataset = _create_dataset(input_model)
-    base_model = _create_base_model(dataset)
-    iiv_on_ruv = _create_iiv_on_ruv_model(base_model)
-    power = _create_power_model(base_model)
-    return (base_model, iiv_on_ruv, power)
+# def create_models(input_model):
+#    dataset = _create_dataset(input_model)
+#    base_model = _create_base_model(dataset)
+#    iiv_on_ruv = _create_iiv_on_ruv_model(base_model)
+#    power = _create_power_model(base_model)
+#    return (base_model, iiv_on_ruv, power)
