5aa6339f822e53c2302b434e5dc7cff7a9b0a3f2
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-09-10 13:15:52 +0200

    Fix bug in statement dependencies for one statment models

--- src/pharmpy/statements.py
+++ src/pharmpy/statements.py
@@ -860,6 +860,9 @@ class ModelStatements(MutableSequence):
             raise KeyError(f"Could not find symbol {symbol}")
         g = self._create_dependency_graph()
         symbs = self[i].rhs_symbols
+        if i == 0:
+            # Special case for models with only one statement
+            return symbs
         for j, _ in nx.bfs_predecessors(g, i, sort_neighbors=lambda x: reversed(sorted(x))):
             if isinstance(self[j], Assignment):
                 symbs -= {self[j].symbol}
---
+++ tests/test_utils.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+import pharmpy.utils as utils
+
+
+def test_tempdir(fs):
+    with utils.TemporaryDirectory() as td:
+        path = Path(td)
+        assert path.is_dir()
+    assert not path.is_dir()
