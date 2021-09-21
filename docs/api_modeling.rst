a10eb1af7bb94ec382b92c5f3a276e9e5b612182
Stella <stella.belin@farmaci.uu.se>
2021-09-21 13:24:33 +0200

    Set pandas option in conf

--- docs/api_modeling.rst
+++ docs/api_modeling.rst
@@ -1,6 +1,1 @@
-.. testsetup:: *
-    import pandas as pd
-    pd.set_option('display.width', 1000)
-    pd.set_option('display.max_columns', 1000)
-
 .. automodapi:: pharmpy.modeling
--- docs/conf.py
+++ docs/conf.py
@@ -66,3 +66,8 @@ graphviz_output_format = 'svg'
 
 import doctest
 doctest_default_flags = doctest.NORMALIZE_WHITESPACE
+doctest_global_setup = '''
+import pandas as pd
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_columns', 1000)
+'''
