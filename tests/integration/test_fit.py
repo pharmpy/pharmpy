c85e06f1fce56cb7316f368a3d78141db2c81858
Stella <stella.belin@farmaci.uu.se>
2021-10-21 07:41:22 +0200

    Add test for copied models (check that correct modelfit results are chosen)

--- tests/integration/test_fit.py
+++ tests/integration/test_fit.py
@@ -45,3 +45,19 @@ def test_fit_multiple(tmp_path, testdata):
         assert model_1.modelfit_results.ofv == pytest.approx(730.8947268137308)
         assert model_2.modelfit_results.ofv == pytest.approx(730.8947268137308)
         assert rundir.is_dir()
+
+
+def test_fit_copy(tmp_path, testdata):
+    with TemporaryDirectoryChanger(tmp_path):
+        shutil.copy2(testdata / 'nonmem' / 'pheno.mod', tmp_path / 'pheno.mod')
+        shutil.copy2(testdata / 'nonmem' / 'pheno.dta', tmp_path / 'pheno.dta')
+
+        model_1 = Model('pheno.mod')
+        model_1.dataset_path = tmp_path / 'pheno.dta'
+        modeling.fit(model_1)
+
+        model_2 = modeling.copy_model(model_1, 'pheno_copy')
+        modeling.update_inits(model_2)
+        modeling.fit(model_2)
+
+        assert model_1.modelfit_results.ofv != model_2.modelfit_results.ofv
