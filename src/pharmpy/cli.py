d7349fba53effb9dec9ac88ce4b536b98adea2b0
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-11-11 13:29:48 +0100

    Bump version: 0.35.1 → 0.36.0

--- src/pharmpy/cli.py
+++ src/pharmpy/cli.py
@@ -1618,7 +1618,7 @@ parser = argparse.ArgumentParser(
     formatter_class=formatter,
     allow_abbrev=True,
 )
-parser.add_argument('--version', action='version', version='0.35.1')
+parser.add_argument('--version', action='version', version='0.36.0')
 
 # subcommand parsers
 subparsers = parser.add_subparsers(title='Pharmpy commands', metavar='COMMAND')
