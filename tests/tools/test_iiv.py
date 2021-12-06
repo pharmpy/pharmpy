3f664fcca09242545c1ccdc065d222b5c273c748
Stella <stella.belin@farmaci.uu.se>
2021-12-06 15:18:53 +0100

    Allow all combinations of multiple blocks (e.g. BLOCK(2) + BLOCK(2)), test with five etas

--- tests/tools/test_iiv.py
+++ tests/tools/test_iiv.py
@@ -5,7 +5,7 @@ from pharmpy.tools.iiv.algorithms import _get_iiv_combinations
 from pharmpy.tools.iiv.tool import _get_iiv_block
 
 
-def test_get_iiv_combinations(testdata, pheno_path):
+def test_get_iiv_combinations_4_etas(testdata, pheno_path):
     model = Model(
         StringIO(
             '''
@@ -38,7 +38,9 @@ $ESTIMATION METHOD=1 INTERACTION
 
     model.path = testdata / 'nonmem' / 'pheno.mod'  # To be able to find dataset
 
-    assert _get_iiv_combinations(model) == [
+    iiv_single_block, iiv_multi_block = _get_iiv_combinations(model)
+
+    assert iiv_single_block == [
         ['ETA(1)', 'ETA(2)'],
         ['ETA(1)', 'ETA(3)'],
         ['ETA(1)', 'ETA(4)'],
@@ -50,6 +52,91 @@ $ESTIMATION METHOD=1 INTERACTION
         ['ETA(1)', 'ETA(3)', 'ETA(4)'],
         ['ETA(2)', 'ETA(3)', 'ETA(4)'],
         ['ETA(1)', 'ETA(2)', 'ETA(3)', 'ETA(4)'],
+    ]
+
+    assert iiv_multi_block == [
+        [['ETA(1)', 'ETA(2)'], ['ETA(3)', 'ETA(4)']],
+        [['ETA(1)', 'ETA(3)'], ['ETA(2)', 'ETA(4)']],
+        [['ETA(1)', 'ETA(4)'], ['ETA(2)', 'ETA(3)']],
+    ]
+
+
+def test_get_iiv_combinations_5_etas(testdata, pheno_path):
+    model = Model(
+        StringIO(
+            '''
+$PROBLEM PHENOBARB SIMPLE MODEL
+$DATA pheno.dta IGNORE=@
+$INPUT ID TIME AMT WGT APGR DV
+$SUBROUTINE ADVAN1 TRANS2
+
+$PK
+CL=THETA(1)*EXP(ETA(1))
+V=THETA(2)*EXP(ETA(2))
+S1=V*EXP(ETA(3))*EXP(ETA(4))*EXP(ETA(5))
+
+$ERROR
+Y=F+F*EPS(1)
+
+$THETA (0,0.00469307) ; TVCL
+$THETA (0,1.00916) ; TVV
+$OMEGA DIAGONAL(5)
+ 0.0309626  ;       IVCL
+ 0.031128  ;        IVV
+ 0.031128
+ 0.031128
+ 0.031128
+$SIGMA 0.013241
+
+$ESTIMATION METHOD=1 INTERACTION
+'''
+        )
+    )
+
+    model.path = testdata / 'nonmem' / 'pheno.mod'  # To be able to find dataset
+
+    iiv_single_block, iiv_multi_block = _get_iiv_combinations(model)
+
+    assert iiv_single_block == [
+        ['ETA(1)', 'ETA(2)'],
+        ['ETA(1)', 'ETA(3)'],
+        ['ETA(1)', 'ETA(4)'],
+        ['ETA(1)', 'ETA(5)'],
+        ['ETA(2)', 'ETA(3)'],
+        ['ETA(2)', 'ETA(4)'],
+        ['ETA(2)', 'ETA(5)'],
+        ['ETA(3)', 'ETA(4)'],
+        ['ETA(3)', 'ETA(5)'],
+        ['ETA(4)', 'ETA(5)'],
+        ['ETA(1)', 'ETA(2)', 'ETA(3)'],
+        ['ETA(1)', 'ETA(2)', 'ETA(4)'],
+        ['ETA(1)', 'ETA(2)', 'ETA(5)'],
+        ['ETA(1)', 'ETA(3)', 'ETA(4)'],
+        ['ETA(1)', 'ETA(3)', 'ETA(5)'],
+        ['ETA(1)', 'ETA(4)', 'ETA(5)'],
+        ['ETA(2)', 'ETA(3)', 'ETA(4)'],
+        ['ETA(2)', 'ETA(3)', 'ETA(5)'],
+        ['ETA(2)', 'ETA(4)', 'ETA(5)'],
+        ['ETA(3)', 'ETA(4)', 'ETA(5)'],
+        ['ETA(1)', 'ETA(2)', 'ETA(3)', 'ETA(4)'],
+        ['ETA(1)', 'ETA(2)', 'ETA(3)', 'ETA(5)'],
+        ['ETA(1)', 'ETA(2)', 'ETA(4)', 'ETA(5)'],
+        ['ETA(1)', 'ETA(3)', 'ETA(4)', 'ETA(5)'],
+        ['ETA(2)', 'ETA(3)', 'ETA(4)', 'ETA(5)'],
+        ['ETA(1)', 'ETA(2)', 'ETA(3)', 'ETA(4)', 'ETA(5)'],
+    ]
+
+    assert iiv_multi_block == [
+        [['ETA(1)', 'ETA(2)'], ['ETA(3)', 'ETA(4)', 'ETA(5)']],
+        [['ETA(1)', 'ETA(3)'], ['ETA(2)', 'ETA(4)', 'ETA(5)']],
+        [['ETA(1)', 'ETA(4)'], ['ETA(2)', 'ETA(3)', 'ETA(5)']],
+        [['ETA(1)', 'ETA(5)'], ['ETA(2)', 'ETA(3)', 'ETA(4)']],
+        [['ETA(2)', 'ETA(3)'], ['ETA(1)', 'ETA(4)', 'ETA(5)']],
+        [['ETA(2)', 'ETA(4)'], ['ETA(1)', 'ETA(3)', 'ETA(5)']],
+        [['ETA(2)', 'ETA(5)'], ['ETA(1)', 'ETA(3)', 'ETA(4)']],
+        [['ETA(3)', 'ETA(4)'], ['ETA(1)', 'ETA(2)', 'ETA(5)']],
+        [['ETA(3)', 'ETA(5)'], ['ETA(1)', 'ETA(2)', 'ETA(4)']],
+        [['ETA(4)', 'ETA(5)'], ['ETA(1)', 'ETA(2)', 'ETA(3)']],
     ]
 
 
