c6da8328cc434dffd01057098e7dd7eca7b8a64e
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2022-01-18 08:53:21 +0100

    Document Log class

--- src/pharmpy/workflows/log.py
+++ src/pharmpy/workflows/log.py
@@ -17,18 +17,41 @@ class LogEntry:
 
 
 class Log:
+    """Timestamped error and warning log"""
+
     def __init__(self):
         self._log = []  # list of LogEntry
 
     def log_error(self, message):
+        """Log an error
+
+        Parameters
+        ----------
+        message : str
+            Error message
+        """
         entry = LogEntry(category='ERROR', message=message)
         self._log.append(entry)
 
     def log_warning(self, message):
+        """Log a warning
+
+        Parameters
+        ----------
+        message : str
+            Warning message
+        """
         entry = LogEntry(category='WARNING', message=message)
         self._log.append(entry)
 
     def to_dataframe(self):
+        """Create an overview dataframe from log
+
+        Returns
+        -------
+        pd.DataFrame
+            Dataframe with overview of log entries
+        """
         categories = [entry.category for entry in self._log]
         times = [entry.time for entry in self._log]
         models = [entry.model for entry in self._log]
