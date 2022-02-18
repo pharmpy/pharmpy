f3117245d262676ae4ed494739982c1b1175bf1a
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2022-02-18 14:34:39 +0100

    Add option for free ordering of steps in run_amd

--- src/pharmpy/modeling/amd.py
+++ src/pharmpy/modeling/amd.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 from pharmpy.modeling import copy_model, remove_iiv
 from pharmpy.results import Results
 from pharmpy.workflows import default_tool_database
@@ -11,7 +13,7 @@ class AMDResults(Results):
         self.final_model = final_model
 
 
-def run_amd(model, mfl=None, lloq=None):
+def run_amd(model, mfl=None, lloq=None, order=None):
     """Run Automatic Model Development (AMD) tool
 
     Runs structural modelsearch, IIV building, and resmod
@@ -24,6 +26,8 @@ def run_amd(model, mfl=None, lloq=None):
         MFL for search space for structural model
     lloq : float
         Lower limit of quantification. LOQ data will be removed.
+    order : list
+        Runorder of components
 
     Returns
     -------
@@ -45,8 +49,9 @@ def run_amd(model, mfl=None, lloq=None):
     if lloq is not None:
         remove_loq_data(model, lloq=lloq)
 
-    db = default_tool_database(toolname='amd')
-    run_tool('modelfit', model, path=db.path / 'modelfit')
+    default_order = ['structural', 'iiv', 'residual']
+    if order is None:
+        order = default_order
 
     if mfl is None:
         mfl = (
@@ -57,18 +62,47 @@ def run_amd(model, mfl=None, lloq=None):
             'PERIPHERALS([1,2])'
         )
 
+    run_funcs = []
+    for section in order:
+        if section == 'structural':
+            func = partial(_run_modelsearch, mfl=mfl)
+        elif section == 'iiv':
+            func = _run_iiv
+        elif section == 'residual':
+            func = _run_resmod
+        else:
+            raise ValueError(
+                f"Unrecognized section {section} in order. Must be one of {default_order}"
+            )
+        run_funcs.append(func)
+
+    db = default_tool_database(toolname='amd')
+    run_tool('modelfit', model, path=db.path / 'modelfit')
+
+    next_model = model
+    for func in run_funcs:
+        next_model = func(next_model)
+
+    res = AMDResults(final_model=next_model)
+    return res
+
+
+def _run_modelsearch(model, mfl):
     res_modelsearch = run_tool('modelsearch', 'exhaustive_stepwise', mfl=mfl, model=model)
     selected_model = res_modelsearch.best_model
+    return selected_model
 
-    res_iiv = run_iiv(selected_model)
+
+def _run_iiv(model):
+    res_iiv = run_iiv(model)
     selected_iiv_model = res_iiv.best_model
+    return selected_iiv_model
 
-    res_resmod = run_tool('resmod', selected_iiv_model)
-    final_model = res_resmod.best_model
 
-    res = AMDResults(final_model=final_model)
-
-    return res
+def _run_resmod(model):
+    res_resmod = run_tool('resmod', model)
+    selected_model = res_resmod.best_model
+    return selected_model
 
 
 def run_iiv(model):
