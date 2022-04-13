997a9f4306e83ce00707a659cc14ee512138fefb
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2022-04-13 11:16:58 +0200

    Do approximate tests of ml model results

--- tests/modeling/test_ml.py
+++ tests/modeling/test_ml.py
@@ -12,7 +12,7 @@ def test_predict_outliers():
     model = load_example_model('pheno')
     res = predict_outliers(model)
     assert len(res) == 59
-    assert res['residual'][1] == -0.28144291043281555
+    assert res['residual'][1] == pytest.approx(-0.28144291043281555)
 
 
 @pytest.mark.skipif(
@@ -22,4 +22,4 @@ def test_predict_influential_individuals():
     model = load_example_model('pheno')
     res = predict_influential_individuals(model)
     assert len(res) == 59
-    assert res['dofv'][59] == 0.08806940913200378
+    assert res['dofv'][59] == pytest.approx(0.08806940913200378)
