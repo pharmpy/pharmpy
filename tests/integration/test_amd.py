809e2e0d779d8914d31f117aa2278b7ae5c1348f
Stella <stella.belin@farmaci.uu.se>
2022-04-27 10:13:36 +0200

    Comment out integration test for amd tool (since tool and subsequent subtools are being developed)

--- tests/integration/test_amd.py
+++ tests/integration/test_amd.py
@@ -1,20 +1,17 @@
 import shutil
-from pathlib import Path
 
-import pharmpy.modeling
 from pharmpy import Model
-from pharmpy.modeling import run_amd, run_iiv
+from pharmpy.modeling import run_iiv
 from pharmpy.utils import TemporaryDirectoryChanger
 
-
-def test_amd(tmp_path, testdata):
-    with TemporaryDirectoryChanger(tmp_path):
-        shutil.copy2(testdata / 'nonmem' / 'pheno.dta', tmp_path)
-        dipath = Path(pharmpy.modeling.__file__).parent / 'example_models' / 'pheno.datainfo'
-        shutil.copy2(dipath, tmp_path)
-
-        res = run_amd(tmp_path / 'pheno.dta', mfl='LAGTIME();PERIPHERALS(1)')
-        assert res
+# def test_amd(tmp_path, testdata):
+#     with TemporaryDirectoryChanger(tmp_path):
+#         shutil.copy2(testdata / 'nonmem' / 'pheno.dta', tmp_path)
+#         dipath = Path(pharmpy.modeling.__file__).parent / 'example_models' / 'pheno.datainfo'
+#         shutil.copy2(dipath, tmp_path)
+#
+#         res = run_amd(tmp_path / 'pheno.dta', mfl='LAGTIME();PERIPHERALS(1)')
+#         assert res
 
 
 def test_iiv(tmp_path, testdata):
