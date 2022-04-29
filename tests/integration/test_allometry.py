8faccee4286763b4efa8b614ea6b839126772704
Zhe Huang <zhe.huang@farmaci.uu.se>
2022-04-29 15:20:11 +0200

    Add integration test for allometry tool

---
+++ tests/integration/test_allometry.py
@@ -0,0 +1,18 @@
+import shutil
+
+from pharmpy import Model
+from pharmpy.modeling import run_tool
+from pharmpy.utils import TemporaryDirectoryChanger
+
+
+def test_allometry(tmp_path, testdata):
+    with TemporaryDirectoryChanger(tmp_path):
+        for path in (testdata / 'nonmem').glob('pheno_real.*'):
+            shutil.copy2(path, tmp_path)
+        shutil.copy2(testdata / 'nonmem' / 'pheno.dta', tmp_path)
+        shutil.copy2(testdata / 'nonmem' / 'sdtab1', tmp_path)
+
+        model = Model.create_model('pheno_real.mod')
+        model.datainfo.path = tmp_path / 'pheno.dta'
+        res = run_tool('allometry', model)
+        assert res
