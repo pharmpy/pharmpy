5190427b1d165437754954968ea35c7d5b047781
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2022-04-29 11:40:09 +0200

    Add deptree script in new scripts/ directory
    Deptree is used to get a dependency tree for Pharmpy

---
+++ scripts/deptree.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Print the dependency tree of Pharmpy 
+# Actual dependencies will be used (i.e. not requirements.txt)
+
+rm -rf /tmp/updatevenv
+python3 -m venv /tmp/updatevenv
+activate () {
+    . /tmp/updatevenv/bin/activate
+}
+activate
+pip install setuptools --upgrade
+python3 setup.py install
+pip install pipdeptree
+pipdeptree -fl
+
+exit
