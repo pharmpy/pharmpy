61a72a8cca577a4a44718578e689fb05d03145a8
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2022-05-04 17:04:57 +0200

    Only parse first line as description in $PROBLEM
    Remove limitation of 72 characters

--- src/pharmpy/plugins/nonmem/records/problem_record.py
+++ src/pharmpy/plugins/nonmem/records/problem_record.py
@@ -6,22 +6,9 @@ from .record import Record
 class ProblemRecord(Record):
     @property
     def title(self):
-        first_line = str(self.root.raw_title).split('\n')[0].strip()
-        max_number_of_characters_in_title = 72
-        return first_line[:max_number_of_characters_in_title]
+        return str(self.root.raw_title).lstrip()
 
     @title.setter
     def title(self, new_title):
-        if len(new_title.strip()) > 72:
-            raise ValueError(
-                f'The provided title is {len(new_title.strip())} characters long but '
-                f'can be maximum 72 characters long (not counting initial and trailing'
-                f'whitespace)'
-            )
-        if new_title == new_title.lstrip():
-            new_title = ' ' + new_title
-        lines = str(self.root.raw_title).split('\n')
-        lines[0] = new_title
-        s = '\n'.join(lines)
-        node = AttrTree.create('raw_title', dict(ANYTHING=s))
+        node = AttrTree.create('raw_title', dict(ANYTHING=" " + new_title))
         self.root.set('raw_title', node)
--- tests/nonmem/records/test_problem.py
+++ tests/nonmem/records/test_problem.py
@@ -1,16 +1,17 @@
 import pytest
 
 
+# FIXME: Change parsing of $PROBLEM. First row is free form. Rest can be comments or empty lines
 @pytest.mark.parametrize(
     'buf,title,new_title,new_str',
     [
         ('$PROB\n', '', 'PHENO  MODEL', '$PROB PHENO  MODEL\n'),
         ('$PROBLEM ABC\n', 'ABC', None, None),
-        ('$PROBLEM   A;BC \n\n', 'A;BC', "New", "$PROBLEM New\n\n"),
+        ('$PROBLEM   A;BC \n\n', 'A;BC ', "New", "$PROBLEM New\n\n"),
         ('$PROBLEM A\n', 'A', None, None),
-        ('$PROBLEM ABC \n', 'ABC', None, None),
-        ('$PROBL A ; B ; C \n', 'A ; B ; C', None, None),
-        ('$PROBL A ; B \n', 'A ; B', None, None),
+        ('$PROBLEM ABC \n', 'ABC ', None, None),
+        ('$PROBL A ; B ; C \n', 'A ; B ; C ', None, None),
+        ('$PROBL A ; B \n', 'A ; B ', None, None),
         ('$PROBLEM \n ', '', None, None),
         ('$PROBLEM \n \n', '', None, None),
     ],
@@ -24,15 +25,3 @@ def test_modify_string(parser, buf, title, new_title, new_str):
         rec.title = new_title
         assert rec.title == new_title
         assert str(rec) == new_str
-
-
-def test_title_limits(parser):
-    rec = parser.parse("$PROBLEM " + "k" * 72).records[0]
-    assert rec.title == "k" * 72
-    rec = parser.parse("$PROBLEM " + "k" * 73).records[0]
-    assert rec.title == "k" * 72
-    with pytest.raises(ValueError):
-        rec.title = "k" * 73
-    rec.title = "k" * 72
-    rec.title = " mytitle"
-    assert rec.title == "mytitle"
