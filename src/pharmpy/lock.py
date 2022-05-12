a9ff4f5fcdb5f3a0a060c7f7bd690545a7221c83
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-05-12 15:47:08 +0200

    Fix implementation of process-level locks when shared
    Previous implementation released process-level locks too early when
    sharing these locks among threads of the same process on Linux. This had
    no consequence however since the current approach to parallelism does
    not involve spanning multiple processes.

--- src/pharmpy/lock.py
+++ src/pharmpy/lock.py
@@ -38,10 +38,33 @@ Caveats:
     happen.
 """
 import os
+from collections import Counter
 from contextlib import contextmanager
-from threading import Condition, Lock
+from threading import Condition, Lock, RLock, get_ident
+from typing import Generic, TypeVar
 
-if os.name == 'nt':
+T = TypeVar('T')
+
+
+class AcquiringLockWouldBlockError(Exception):
+    """Raised when acquiring lock would block and blocking is False"""
+
+
+class AcquiringProcessLevelLockWouldBlockError(AcquiringLockWouldBlockError):
+    """Raised when acquiring a process-level lock would block and blocking is False"""
+
+
+class AcquiringThreadLevelLockWouldBlockError(AcquiringLockWouldBlockError):
+    """Raised when acquiring a thread-level lock would block and blocking is False"""
+
+
+class RecursiveDeadlockError(Exception):
+    """Raised when recursive dead-lock is detected."""
+
+
+is_windows = os.name == 'nt'
+
+if is_windows:
     # Windows file locking
     import msvcrt
     import sys
@@ -87,43 +110,110 @@ else:
         fcntl.lockf(fd, fcntl.LOCK_UN)
 
 
-@contextmanager
-def process_level_lock(fd: int, shared: bool = False, blocking: bool = True):
-    _process_level_lock(fd, shared, blocking)
+class ShareableProcessLock:
+    """Creates a process lock for a FD shared by all threads of a process"""
 
-    try:
-        yield
+    def __init__(self, fd: int):
+        self._fd = fd
+        self._lock = Lock()
+        self._shared_by: Counter[int] = Counter()
+        self._exclusively_held_by: Counter[int] = Counter()
 
-    finally:
-        _process_level_unlock(fd)
+    @contextmanager
+    def lock(self, shared: bool = False, blocking: bool = True):
+        """Locks the scoped FD
 
+        shared : bool
+            Whether the lock should be shared. If not shared, the lock is
+            exclusive. Exclusively locking a file only means that we want to
+            exclude other processes from locking the same file: any number of
+            threads of the same process can lock the same file both shared and
+            exclusively simultaneously. We keep track of which threads are
+            holding what kind of locks to upgrade the process lock from shared
+            to exclusive and vice versa.
+        blocking : bool
+            Whether lock acquisition should be blocking. If True, will raise an
+            error if a lock cannot be acquired immediately. Otherwise, will block
+            until the lock can be acquired.
+        """
+        thread_id = get_ident()
+        if self._lock.acquire(blocking=blocking):
+            try:
+                if self._shared_by[thread_id] or self._exclusively_held_by[thread_id]:
+                    raise RecursiveDeadlockError()
 
-class AcquiringTheadLevelLockWouldBlockError(Exception):
-    """Raised when acquiring a thread-level lock would block and blocking is False"""
+                is_held_shared = bool(self._shared_by)
+                is_held_exclusively = bool(self._exclusively_held_by)
+                is_held = is_held_shared or is_held_exclusively
+
+                if not is_held or (is_held_shared and not shared and not is_windows):
+                    # NOTE We only lock when the first locking attempt is made, or
+                    # if we want to upgrade the lock to an exclusive lock, but only on
+                    # Linux since current implementation always uses exclusive locks on
+                    # Windows
+                    _process_level_lock(self._fd, shared, blocking)
+
+                if shared:
+                    self._shared_by[thread_id] += 1
+                else:
+                    self._exclusively_held_by[thread_id] += 1
+            finally:
+                self._lock.release()
+        else:
+            raise AcquiringProcessLevelLockWouldBlockError()
+
+        try:
+            yield
+
+        finally:
+            with self._lock:
+                if shared:
+                    self._shared_by[thread_id] -= 1
+                    if not self._shared_by[thread_id]:
+                        del self._shared_by[thread_id]
+                else:
+                    self._exclusively_held_by[thread_id] -= 1
+                    if not self._exclusively_held_by[thread_id]:
+                        del self._exclusively_held_by[thread_id]
+
+                is_held_shared = bool(self._shared_by)
+                is_held_exclusively = bool(self._exclusively_held_by)
+                is_held = is_held_shared or is_held_exclusively
+
+                if not is_held:
+                    # NOTE We only release the lock once we have exhausted all
+                    # locking attempts
+                    _process_level_unlock(self._fd)
+                elif not is_held_exclusively and not shared and not is_windows:
+                    # NOTE We downgrade the lock from exclusive to shared if we
+                    # are not on Windows and we just released an exclusive
+                    # lock, and no exclusive lock is left.
+                    _process_level_lock(self._fd, shared=True, blocking=True)
 
 
 class ShareableThreadLock:
     def __init__(self):
-        self._condition = Condition(Lock())
-        self._shared_count = 0
+        self._condition = Condition(RLock())
+        self._acquired_by: Counter[int] = Counter()
 
     def lock(self, shared: bool = False, blocking: bool = True):
         if shared:
             return self._lock_sh(blocking=blocking)
-        elif blocking:
-            return self._lock_ex()
         else:
-            return self._lock_ex_nb()
+            return self._lock_ex(blocking=blocking)
 
     @contextmanager
     def _lock_sh(self, blocking: bool = True):
+        thread_id = get_ident()
         if self._condition.acquire(blocking=blocking):
             try:
-                self._shared_count += 1
+                if self._acquired_by[thread_id]:
+                    raise RecursiveDeadlockError()
+                self._acquired_by[thread_id] += 1
             finally:
                 self._condition.release()
         else:
-            raise AcquiringTheadLevelLockWouldBlockError()
+            raise AcquiringThreadLevelLockWouldBlockError()
 
         try:
             yield
@@ -132,52 +222,70 @@ class ShareableThreadLock:
 
             self._condition.acquire(blocking=True)
             try:
-                self._shared_count -= 1
-                if self._shared_count == 0:
-                    self._condition.notifyAll()
+                self._acquired_by[thread_id] -= 1
+                if not self._acquired_by[thread_id]:
+                    del self._acquired_by[thread_id]  # NOTE GC
+                    if not self._acquired_by:
+                        self._condition.notifyAll()
             finally:
                 self._condition.release()
 
     @contextmanager
-    def _lock_ex(self):
-        self._condition.acquire(blocking=True)
-        try:
-            while self._shared_count != 0:
-                self._condition.wait()
-
-            yield
-
-        finally:
-            self._condition.release()
-
-    @contextmanager
-    def _lock_ex_nb(self):
-        if self._condition.acquire(blocking=False):
+    def _lock_ex(self, blocking: bool = True):
+        thread_id = get_ident()
+        if self._condition.acquire(blocking=blocking):
+            acquired = False
             try:
-                if self._shared_count != 0:
-                    raise AcquiringTheadLevelLockWouldBlockError()
+                this_thread_count = Counter({thread_id: self._acquired_by[thread_id]})
+                if blocking:
+                    # NOTE We could use self._acquired_by != this_thread_count in
+                    # Python >= 3.10
+                    while self._acquired_by - this_thread_count:
+                        self._condition.wait()
+                else:
+                    # NOTE We could use self._acquired_by != this_thread_count in
+                    # Python >= 3.10
+                    if self._acquired_by - this_thread_count:
+                        raise AcquiringThreadLevelLockWouldBlockError()
+
+                # NOTE The following check also works for Python < 3.10 because we
+                # systematically get rid of zero-count entries.
+                if self._acquired_by:
+                    # NOTE The following two checks can be replaced by
+                    # assert self._acquired_by == this_thread_count
+                    # in Python >= 3.10
+                    assert len(self._acquired_by) == 1
+                    assert self._acquired_by[thread_id] == this_thread_count[thread_id]
+                    raise RecursiveDeadlockError()
+
+                acquired = True
+                self._acquired_by[thread_id] += 1
 
                 yield
 
             finally:
+                if acquired:
+                    self._acquired_by[thread_id] -= 1
+                    if not self._acquired_by[thread_id]:
+                        del self._acquired_by[thread_id]  # NOTE GC
                 self._condition.release()
         else:
-            raise AcquiringTheadLevelLockWouldBlockError()
+            raise AcquiringThreadLevelLockWouldBlockError()
 
 
-class ThreadSafeKeyedRefPool:
+class ThreadSafeKeyedRefPool(Generic[T]):
     def __init__(self, lock, refs: dict, factory):
         self._lock = lock
         self._refs = refs
         self._factory = factory
 
     @contextmanager
-    def __call__(self, key: str):
+    def __call__(self, key: T):
         with self._lock:
             entry = self._refs.get(key)
             if entry is None:
                 # NOTE We create a new object if none exists
-                obj = self._factory()
+                obj = self._factory(key)
                 self._refs[key] = (obj, 1)
             else:
                 # NOTE Otherwise we count one ref more
@@ -199,15 +307,27 @@ class ThreadSafeKeyedRefPool:
                     self._refs[key] = (obj, refcount - 1)
 
 
-_thread_level_lock_ref = ThreadSafeKeyedRefPool(Lock(), {}, ShareableThreadLock)
+_thread_level_lock_ref = ThreadSafeKeyedRefPool(Lock(), {}, lambda _key: ShareableThreadLock())
+_process_level_lock_ref = ThreadSafeKeyedRefPool(Lock(), {}, ShareableProcessLock)
 
 
 @contextmanager
-def thread_level_path_lock(path: str, shared: bool = False, blocking: bool = True):
-    key = os.path.normpath(path)
+def process_level_lock(fd: int, shared: bool = False, blocking: bool = True):
+    with _process_level_lock_ref(fd) as ref:
+        with ref.lock(shared, blocking):
+            yield
+
+
+@contextmanager
+def thread_level_lock(key: str, shared: bool = False, blocking: bool = True):
     with _thread_level_lock_ref(key) as ref:
         with ref.lock(shared, blocking):
             yield
+
+
+def thread_level_path_lock(path: str, shared: bool = False, blocking: bool = True):
+    key = os.path.normpath(path)
+    return thread_level_lock(key, shared, blocking)
 
 
 @contextmanager
