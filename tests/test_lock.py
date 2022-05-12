e6e98376c400fe1820d27ed57562d530ac7a1488
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-05-12 16:38:52 +0200

    Test concurrent shared/exlusive path_lock use

--- tests/test_lock.py
+++ tests/test_lock.py
@@ -1,7 +1,8 @@
+import os
 import time
 from concurrent.futures import ThreadPoolExecutor
 from contextlib import contextmanager
-from multiprocessing import Pipe, Process
+from multiprocessing import Manager, Pipe, Pool, Process
 from pathlib import Path
 from queue import Queue
 
@@ -86,3 +87,85 @@ def test_exclusive_processes_non_blocking(tmp_path):
         assert isinstance(e, AcquiringProcessLevelLockWouldBlockError)
 
         parent_first_conn.send('')
+
+
+def test_many_shared_one_exclusive_threads_blocking(tmp_path):
+    with lock(tmp_path) as path:
+
+        n = 10
+
+        q = Queue()
+
+        def thread_lock_shared(results, i):
+            with path_lock(path, shared=True):
+                q.put(0)
+                time.sleep(1)
+                results.append(i)
+
+        def thread_lock_exclusive(results, i):
+            for j in range(n - 1):
+                q.get()
+            with path_lock(path, shared=False):
+                results.append(i)
+
+        with ThreadPoolExecutor(max_workers=n) as executor:
+            for j in range(2):
+                results = []
+
+                for i in range(1, n):
+                    executor.submit(thread_lock_shared, results, i)
+                last = executor.submit(thread_lock_exclusive, results, 0)
+
+                last.result()
+
+                assert sorted(results) == sorted(range(n))
+                assert results[-1] == 0
+
+
+def process_lock_shared(conn, q, path, i):
+    with path_lock(path, shared=True):
+        conn.put(0)
+        time.sleep(2)
+        q.put(i)
+
+
+def process_lock_exclusive(conn, q, path, i):
+    conn.recv()
+    with path_lock(path, shared=False):
+        q.put(i)
+
+
+@pytest.mark.skipif(os.name == 'nt', reason="Windows shared process locks are not implemented.")
+def test_many_shared_one_exclusive_processes_blocking(tmp_path):
+    with lock(tmp_path) as path:
+
+        n = 10
+
+        with Pool(processes=n) as pool:
+
+            m = Manager()
+            results_queue = m.Queue()
+
+            for j in range(2):
+
+                locked_queue = m.Queue()
+                parent_last_conn, last_conn = Pipe()
+
+                for i in range(1, n):
+                    pool.apply_async(process_lock_shared, [locked_queue, results_queue, path, i])
+                last = pool.apply_async(process_lock_exclusive, [last_conn, results_queue, path, 0])
+
+                # Waiting for at least one process to acquire a shared lock
+                locked_queue.get()
+
+                # Allow last process to attempt acquiring an exlusive lock
+                parent_last_conn.send('')
+
+                last.wait()
+
+                results = []
+                for i in range(n):
+                    results.append(results_queue.get())
+
+                assert sorted(results) == sorted(range(n))
+                assert results[-1] == 0
