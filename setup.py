9e0cc30de8374846051ebd4aa2906941386fc040
Stella <stella.belin@farmaci.uu.se>
2022-05-13 16:53:10 +0200

    Bump version: 0.69.0 → 0.70.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def longdesc(text_str):
 
 setup(
     name='pharmpy-core',
-    version='0.69.0',
+    version='0.70.0',
     license='GNU Lesser General Public License v3 (LGPLv3)',
     description='Pharmacometric modelling',
     long_description='%s\n\n%s'
