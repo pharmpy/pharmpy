0ac40c78f725e36e8b1d3e6ae4f69f89aed6ac62
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2022-06-02 17:41:22 +0200

    Add Allometry example to update_tool_results.sh

--- scripts/update_tool_results.sh
+++ scripts/update_tool_results.sh
@@ -3,25 +3,36 @@
 # This script will create a temporary directory and run tools there, then results.json will overwrite
 # each example tool results respectively
 
-rm -rf /tmp/tool_results
+TESTPATH=/tmp/tool_results
+TESTDATA=tests/testdata/nonmem
+DEST=tests/testdata/results
+
+rm -rf $TESTPATH
 
 cd ..
-mkdir /tmp/tool_results
+mkdir $TESTPATH
 
-cp tests/testdata/nonmem/models/mox2.mod /tmp/tool_results/
-cp tests/testdata/nonmem/models/mox2.lst /tmp/tool_results/
-cp tests/testdata/nonmem/models/mox2.ext /tmp/tool_results/
-cp tests/testdata/nonmem/models/mox2.phi /tmp/tool_results/
-cp tests/testdata/nonmem/models/mox_simulated_normal.csv /tmp/tool_results/
 
-tox -e run -- pharmpy run modelsearch /tmp/tool_results/mox2.mod 'ABSORPTION(ZO);PERIPHERALS(1)' 'exhaustive_stepwise' --path /tmp/tool_results/modelsearch/
-cp /tmp/tool_results/modelsearch/results.json tests/testdata/results/modelsearch_results.json
+cp $TESTDATA/pheno.mod $TESTPATH
+cp $TESTDATA/pheno.dta $TESTPATH
 
-tox -e run -- pharmpy run iivsearch /tmp/tool_results/mox2.mod 'brute_force' --path /tmp/tool_results/iivsearch/
-cp /tmp/tool_results/iivsearch/results.json tests/testdata/results/iivsearch_results.json
+tox -e run -- pharmpy run allometry $TESTPATH/pheno.mod --allometric_variable=WGT --path $TESTPATH/allometry/
+cp $TESTPATH/allometry/results.json $DEST/allometry_results.json
 
-cp tests/testdata/nonmem/resmod/mox3.* /tmp/tool_results/
-cp tests/testdata/nonmem/resmod/moxo_simulated_resmod.csv /tmp/tool_results/
-cp tests/testdata/nonmem/resmod/mytab /tmp/tool_results/
-tox -e run -- pharmpy run resmod /tmp/tool_results/mox3.mod --path /tmp/tool_results/resmod/
-cp /tmp/tool_results/resmod/results.json tests/testdata/results/resmod_results.json
+cp $TESTDATA/models/mox2.mod $TESTPATH
+cp $TESTDATA/models/mox2.lst $TESTPATH
+cp $TESTDATA/models/mox2.ext $TESTPATH
+cp $TESTDATA/models/mox2.phi $TESTPATH
+cp $TESTDATA/models/mox_simulated_normal.csv $TESTPATH
+
+tox -e run -- pharmpy run modelsearch $TESTPATH/mox2.mod 'ABSORPTION(ZO);PERIPHERALS(1)' 'exhaustive_stepwise' --path $TESTPATH/modelsearch/
+cp $TESTPATH/modelsearch/results.json $DEST/modelsearch_results.json
+
+tox -e run -- pharmpy run iivsearch $TESTPATH/mox2.mod 'brute_force' --path $TESTPATH/iivsearch/
+cp $TESTPATH/iivsearch/results.json $DEST/iivsearch_results.json
+
+cp $TESTDATA/resmod/mox3.* $TESTPATH
+cp $TESTDATA/resmod/moxo_simulated_resmod.csv $TESTPATH
+cp $TESTDATA/resmod/mytab $TESTPATH
+tox -e run -- pharmpy run resmod $TESTPATH/mox3.mod --path $TESTPATH/resmod/
+cp $TESTPATH/resmod/results.json $DEST/resmod_results.json
