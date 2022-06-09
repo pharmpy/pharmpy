e6bead5530169af616bf86a22d0aa63a92bd9fe1
Stella <stella.belin@farmaci.uu.se>
2022-06-09 12:00:04 +0200

    Use run_tool instead of wrapper in iovsearch

--- tests/integration/test_iovsearch.py
+++ tests/integration/test_iovsearch.py
@@ -2,7 +2,7 @@ import shutil
 from pathlib import Path
 
 from pharmpy import Model
-from pharmpy.modeling import run_iovsearch
+from pharmpy.modeling import run_tool
 from pharmpy.utils import TemporaryDirectoryChanger
 
 
@@ -17,7 +17,7 @@ def _model_count(rundir: Path):
 
 def test_default_mox2(tmp_path, start_model):
     with TemporaryDirectoryChanger(tmp_path):
-        res = run_iovsearch('VISI', model=start_model)
+        res = run_tool('iovsearch', 'VISI', model=start_model)
         rundir = tmp_path / 'iovsearch_dir1'
         assert _model_count(rundir) == 8
 
@@ -26,7 +26,7 @@ def test_default_mox2(tmp_path, start_model):
 
 def test_rank_type_ofv_mox2(tmp_path, start_model):
     with TemporaryDirectoryChanger(tmp_path):
-        res = run_iovsearch('VISI', model=start_model, rank_type='ofv')
+        res = run_tool('iovsearch', 'VISI', model=start_model, rank_type='ofv')
         rundir = tmp_path / 'iovsearch_dir1'
         assert _model_count(rundir) == 8
 
@@ -38,7 +38,7 @@ def test_default_mox1(tmp_path, testdata):
     shutil.copy2(testdata / 'nonmem' / 'models' / 'mox_simulated_log.csv', tmp_path)
     with TemporaryDirectoryChanger(tmp_path):
         start_model = Model.create_model('mox1.mod')
-        res = run_iovsearch('VISI', model=start_model)
+        res = run_tool('iovsearch', 'VISI', model=start_model)
         rundir = tmp_path / 'iovsearch_dir1'
         assert _model_count(rundir) == 8
 
