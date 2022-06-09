0fb59006a15f8080571044472f576e87bcd7432b
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-06-09 16:47:48 +0200

    Create pharmpy.tools.run_* functions on demand
    Related to #816.

--- src/pharmpy/tools/__init__.py
+++ src/pharmpy/tools/__init__.py
@@ -1,25 +1,45 @@
+from typing import Any, List
+
 from .run import create_results, fit, read_results, retrieve_models, run_tool
 from .run_amd import run_amd
-from .wrap import wrap
-
-run_allometry = wrap('allometry')
-run_covsearch = wrap('covsearch')
-run_iivsearch = wrap('iivsearch')
-run_iovsearch = wrap('iovsearch')
-run_modelsearch = wrap('modelsearch')
-run_resmod = wrap('resmod')
 
 __all__ = [
     'create_results',
     'fit',
     'read_results',
     'retrieve_models',
-    'run_allometry',
     'run_amd',
+    'run_tool',
+]
+
+_dynamic__all__ = {
+    'run_allometry',
     'run_covsearch',
     'run_iivsearch',
     'run_iovsearch',
     'run_modelsearch',
     'run_resmod',
-    'run_tool',
-]
+}
+
+
+def __getattr__(name: str) -> Any:
+    try:
+        if not name.startswith('run_') or name not in _dynamic__all__:
+            raise AttributeError()
+
+        tool_name = name[4:]
+        from .wrap import wrap
+
+        func = wrap(tool_name)
+        globals()[name] = func  # NOTE This is only to avoid searching next time
+        return func
+
+    except AttributeError:
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+
+_dir = sorted(set(dir()) | _dynamic__all__ | {'__dir__'})
+
+
+def __dir__() -> List[str]:
+    return _dir
