0a170ecd8c68530bffcc3239c43d64961821a0a3
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-06-09 18:29:04 +0200

    First draft for implementation of covariates search tool

---
+++ src/pharmpy/tools/covsearch/__init__.py
@@ -0,0 +1,6 @@
+from .results import CovariatesResults
+from .tool import create_workflow
+
+results_object = CovariatesResults
+
+__all__ = ['create_workflow', 'CovariatesResults']
---
+++ src/pharmpy/tools/covsearch/results.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+import pandas as pd
+
+import pharmpy.results
+
+
+class CovariatesResults(pharmpy.results.Results):
+    def __init__(
+        self,
+        summary_tool=None,
+        summary_models=None,
+        summary_individuals=None,
+        summary_individuals_count=None,
+        best_model=None,
+        input_model=None,
+        models=None,
+        steps: Union[None, pd.DataFrame] = None,
+        ofv_summary: Union[None, pd.DataFrame] = None,
+        candidate_summary: Union[None, pd.DataFrame] = None,
+    ):
+        self.summary_tool = summary_tool
+        self.summary_models = summary_models
+        self.summary_individuals = summary_individuals
+        self.summary_individuals_count = summary_individuals_count
+        self.best_model = best_model
+        self.input_model = input_model
+        self.models = models
+        self.steps = steps
+        self.candidate_summary = candidate_summary
+        self.ofv_summary = ofv_summary
