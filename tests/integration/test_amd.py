0366e0f2efcc71a581c9fb4613cb7ced8687c4dc
Zhe Huang <zhe.huang@farmaci.uu.se>
2022-06-17 16:37:03 +0200

    Add amd tool test

--- tests/integration/test_amd.py
+++ tests/integration/test_amd.py
@@ -1,7 +1,24 @@
-# def test_amd(tmp_path, testdata):
-#     with TemporaryDirectoryChanger(tmp_path):
-#         shutil.copy2(testdata / 'nonmem' / 'pheno.dta', tmp_path)
-#         dipath = Path(pharmpy.modeling.__file__).parent / 'example_models' / 'pheno.datainfo'
-#         shutil.copy2(dipath, tmp_path)
-#         res = run_amd(tmp_path / 'pheno.dta', mfl='LAGTIME();PERIPHERALS(1)')
-#         assert res
+import shutil
+
+import pytest
+
+from pharmpy.tools import run_amd
+from pharmpy.utils import TemporaryDirectoryChanger
+
+
+@pytest.mark.filterwarnings("ignore::UserWarning")
+def test_amd(tmp_path, testdata):
+    with TemporaryDirectoryChanger(tmp_path):
+        shutil.copy2(testdata / 'nonmem' / 'models' / 'mox_simulated_normal.csv', tmp_path)
+        shutil.copy2(testdata / 'nonmem' / 'models' / 'mox_simulated_normal.datainfo', tmp_path)
+        input = (tmp_path / 'mox_simulated_normal.csv').as_posix()
+        res = run_amd(
+            input,
+            modeltype='pk_oral',
+            search_space='PERIPHERALS(1)',
+            occasion='VISI',
+        )
+        assert len(res.summary_tool) == 58
+        assert len(res.summary_models) == 60
+        assert len(res.summary_individuals_count) == 60
+        assert res.final_model.name == 'scaled_model'
