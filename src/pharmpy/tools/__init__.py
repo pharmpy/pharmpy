69af0ee23b5ed1e3eb38fc2b4105dfa0a6c093f3
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2022-06-20 17:42:27 +0200

    Export run_covsearch from tools module

--- src/pharmpy/tools/__init__.py
+++ src/pharmpy/tools/__init__.py
@@ -3,6 +3,7 @@ from .run_amd import run_amd
 from .wrap import wrap
 
 run_allometry = wrap('allometry')
+run_covsearch = wrap('covsearch')
 run_iivsearch = wrap('iivsearch')
 run_iovsearch = wrap('iovsearch')
 run_modelsearch = wrap('modelsearch')
@@ -15,6 +16,7 @@ __all__ = [
     'retrieve_models',
     'run_allometry',
     'run_amd',
+    'run_covsearch',
     'run_iivsearch',
     'run_iovsearch',
     'run_modelsearch',
