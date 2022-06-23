4e92b3584a1fe79803c07ec1bf903278265c4f84
Stella <stella.belin@farmaci.uu.se>
2022-06-23 16:32:49 +0200

    Bump version: 0.73.1 → 0.73.2

--- src/pharmpy/cli.py
+++ src/pharmpy/cli.py
@@ -1993,7 +1993,7 @@ parser = argparse.ArgumentParser(
     formatter_class=formatter,
     allow_abbrev=True,
 )
-parser.add_argument('--version', action='version', version='0.73.1')
+parser.add_argument('--version', action='version', version='0.73.2')
 
 # subcommand parsers
 subparsers = parser.add_subparsers(title='Pharmpy commands', metavar='COMMAND')
