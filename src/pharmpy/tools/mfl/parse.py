da0f0f3ea7334e0c17cc632e67e9439e20c81929
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-07-07 17:56:01 +0200

    Make MFL parser as fast as possible

--- src/pharmpy/tools/mfl/parse.py
+++ src/pharmpy/tools/mfl/parse.py
@@ -10,11 +10,12 @@ from .statement.statement import Statement
 def parse(code: str) -> List[Statement]:
     parser = Lark(
         grammar,
-        # parser='lalr',
-        # lexer='standard',
-        # propagate_positions=False,
-        # maybe_placeholders=False,
-        # cache=True,
+        parser='lalr',
+        # lexer='standard',  # NOTE This does not work because lexing for the
+        #      MFL grammar is context-dependent
+        propagate_positions=False,
+        maybe_placeholders=False,
+        cache=True,
     )
 
     tree = parser.parse(code)
