205e7c7910cb6b8da38363aceeeb62e1be597c3c
Stella <stella.belin@farmaci.uu.se>
2022-08-17 16:02:43 +0200

    Return only dataframe in rank_models

--- src/pharmpy/tools/common.py
+++ src/pharmpy/tools/common.py
@@ -69,7 +69,7 @@ def summarize_tool(
 ) -> pd.DataFrame:
     models_all = [start_model] + models
 
-    df_rank, _ = rank_models(
+    df_rank = rank_models(
         start_model, models, strictness=[], rank_type=rank_type, cutoff=cutoff, bic_type=bic_type
     )
 
