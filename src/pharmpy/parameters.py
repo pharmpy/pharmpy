8dbc4e3a93f5afd2718e13ddf58c2e07453f2add
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2022-08-26 14:40:42 +0200

    Move Parameter to the top of parameters.py

--- src/pharmpy/parameters.py
+++ src/pharmpy/parameters.py
@@ -7,6 +7,120 @@ import sympy
 import pharmpy.symbols as symbols
 
 
+class Parameter:
+    """A single parameter
+
+    Example
+    -------
+
+    >>> from pharmpy import Parameter
+    >>> param = Parameter("TVCL", 0.005, lower=0)
+    >>> param.init
+    0.005
+
+    Parameters
+    ----------
+    name : str
+        Name of the parameter
+    init : number
+        Initial estimate or simply the value of parameter.
+    fix : bool
+        A boolean to indicate whether the parameter is fixed or not. Note that fixing a parameter
+        will keep its bounds even if a fixed parameter is actually constrained to one single
+        value. This is so that unfixing will take back the previous bounds.
+    lower : number
+        The lower bound of the parameter. Default no bound. Must be less than the init.
+    upper : number
+        The upper bound of the parameter. Default no bound. Must be greater than the init.
+    """
+
+    def __init__(self, name, init, lower=None, upper=None, fix=False):
+        if init is sympy.nan or np.isnan(init):
+            raise ValueError('Initial estimate cannot be NaN')
+        self._init = init
+
+        if not isinstance(name, str):
+            raise ValueError("Name of parameter must be of type string")
+        self._name = name
+        self._fix = bool(fix)
+
+        if lower is None:
+            self._lower = -sympy.oo
+        elif lower > self.init:
+            raise ValueError(f'Lower bound {lower} cannot be greater than init {init}')
+        else:
+            self._lower = lower
+
+        if upper is None:
+            self._upper = sympy.oo
+        elif upper < init:
+            raise ValueError(f'Upper bound {upper} cannot be less than init {init}')
+        else:
+            self._upper = upper
+
+    @property
+    def name(self):
+        """Parameter name"""
+        return self._name
+
+    @property
+    def fix(self):
+        """Should parameter be fixed or not"""
+        return self._fix
+
+    def derive(self, init=None, lower=None, upper=None, fix=None):
+        """Derive a new parameter with new properties"""
+        if init is None:
+            init = self.init
+        if lower is None:
+            lower = self.lower
+        if upper is None:
+            upper = self.upper
+        if fix is None:
+            fix = self.fix
+        new = Parameter(self.name, init, lower=lower, upper=upper, fix=fix)
+        return new
+
+    @property
+    def symbol(self):
+        """Symbol representing the parameter"""
+        return symbols.symbol(self._name)
+
+    @property
+    def lower(self):
+        """Lower bound of the parameter"""
+        return self._lower
+
+    @property
+    def upper(self):
+        """Upper bound of the parameter"""
+        return self._upper
+
+    @property
+    def init(self):
+        """Initial parameter estimate or value"""
+        return self._init
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        """Two parameters are equal if they have the same name, init and constraints"""
+        return (
+            self._init == other._init
+            and self._lower == other._lower
+            and self._upper == other._upper
+            and self._name == other._name
+            and self._fix == other._fix
+        )
+
+    def __repr__(self):
+        return (
+            f'Parameter("{self._name}", {self._init}, lower={self._lower}, upper={self._upper}, '
+            f'fix={self._fix})'
+        )
+
+
 class Parameters(Sequence):
     """An immutable collection of parameters
 
@@ -237,117 +351,3 @@ class Parameters(Sequence):
             return "Parameters()"
         else:
             return self.to_dataframe().to_html()
-
-
-class Parameter:
-    """A single parameter
-
-    Example
-    -------
-
-    >>> from pharmpy import Parameter
-    >>> param = Parameter("TVCL", 0.005, lower=0)
-    >>> param.init
-    0.005
-
-    Parameters
-    ----------
-    name : str
-        Name of the parameter
-    init : number
-        Initial estimate or simply the value of parameter.
-    fix : bool
-        A boolean to indicate whether the parameter is fixed or not. Note that fixing a parameter
-        will keep its bounds even if a fixed parameter is actually constrained to one single
-        value. This is so that unfixing will take back the previous bounds.
-    lower : number
-        The lower bound of the parameter. Default no bound. Must be less than the init.
-    upper : number
-        The upper bound of the parameter. Default no bound. Must be greater than the init.
-    """
-
-    def __init__(self, name, init, lower=None, upper=None, fix=False):
-        if init is sympy.nan or np.isnan(init):
-            raise ValueError('Initial estimate cannot be NaN')
-        self._init = init
-
-        if not isinstance(name, str):
-            raise ValueError("Name of parameter must be of type string")
-        self._name = name
-        self._fix = bool(fix)
-
-        if lower is None:
-            self._lower = -sympy.oo
-        elif lower > self.init:
-            raise ValueError(f'Lower bound {lower} cannot be greater than init {init}')
-        else:
-            self._lower = lower
-
-        if upper is None:
-            self._upper = sympy.oo
-        elif upper < init:
-            raise ValueError(f'Upper bound {upper} cannot be less than init {init}')
-        else:
-            self._upper = upper
-
-    @property
-    def name(self):
-        """Parameter name"""
-        return self._name
-
-    @property
-    def fix(self):
-        """Should parameter be fixed or not"""
-        return self._fix
-
-    def derive(self, init=None, lower=None, upper=None, fix=None):
-        """Derive a new parameter with new properties"""
-        if init is None:
-            init = self.init
-        if lower is None:
-            lower = self.lower
-        if upper is None:
-            upper = self.upper
-        if fix is None:
-            fix = self.fix
-        new = Parameter(self.name, init, lower=lower, upper=upper, fix=fix)
-        return new
-
-    @property
-    def symbol(self):
-        """Symbol representing the parameter"""
-        return symbols.symbol(self._name)
-
-    @property
-    def lower(self):
-        """Lower bound of the parameter"""
-        return self._lower
-
-    @property
-    def upper(self):
-        """Upper bound of the parameter"""
-        return self._upper
-
-    @property
-    def init(self):
-        """Initial parameter estimate or value"""
-        return self._init
-
-    def __hash__(self):
-        return hash(self.name)
-
-    def __eq__(self, other):
-        """Two parameters are equal if they have the same name, init and constraints"""
-        return (
-            self._init == other._init
-            and self._lower == other._lower
-            and self._upper == other._upper
-            and self._name == other._name
-            and self._fix == other._fix
-        )
-
-    def __repr__(self):
-        return (
-            f'Parameter("{self._name}", {self._init}, lower={self._lower}, upper={self._upper}, '
-            f'fix={self._fix})'
-        )
