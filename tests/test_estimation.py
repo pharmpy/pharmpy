866b694d5f0baa35cd12a9c7872a7606641834ae
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2022-08-31 16:33:51 +0200

    Increase test coverage of estimation.py

--- tests/test_estimation.py
+++ tests/test_estimation.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pharmpy.estimation import EstimationStep
+from pharmpy.estimation import EstimationStep, EstimationSteps
 
 
 def test_estimation_method():
@@ -34,3 +34,81 @@ def test_append_options():
 
     a.tool_options.update({'PRINT': 1})
     assert len(a.tool_options) == 2
+
+
+def test_repr():
+    steps = EstimationSteps()
+    assert steps._repr_html_() == 'EstimationSteps()'
+    assert repr(steps) == 'EstimationSteps()'
+
+    a = EstimationStep('foce')
+    steps = EstimationSteps([a])
+    assert type(steps._repr_html_()) == str
+    assert type(repr(steps)) == str
+
+
+def test_eq():
+    s1 = EstimationSteps()
+    s2 = EstimationSteps()
+    assert s1 == s2
+    a = EstimationStep('foce')
+    s3 = EstimationSteps([a, a])
+    assert s1 != s3
+    b = EstimationStep('fo')
+    s4 = EstimationSteps([a, b])
+    assert s3 != s4
+
+
+def test_len():
+    s1 = EstimationSteps()
+    assert len(s1) == 0
+    a = EstimationStep('foce')
+    s2 = EstimationSteps([a, a])
+    assert len(s2) == 2
+
+
+def test_radd():
+    a = EstimationStep('foce')
+    s2 = EstimationSteps([a, a])
+    b = EstimationStep('fo')
+    conc = b + s2
+    assert len(conc) == 3
+    conc = (b,) + s2
+    assert len(conc) == 3
+
+
+def test_add():
+    a = EstimationStep('foce')
+    s2 = EstimationSteps([a, a])
+    b = EstimationStep('fo')
+    conc = s2 + b
+    assert len(conc) == 3
+    conc = s2 + (b,)
+    assert len(conc) == 3
+
+
+def test_getitem():
+    a = EstimationStep('foce')
+    b = EstimationStep('fo')
+    s = EstimationSteps([a, b])
+    assert s[0].method == 'FOCE'
+    assert s[1].method == 'FO'
+
+    assert len(s[1:]) == 1
+
+
+def test_properties():
+    a = EstimationStep('foce', epsilon_derivatives=['EPS(1)'])
+    assert a.epsilon_derivatives == ['EPS(1)']
+    a = EstimationStep('foce', eta_derivatives=['ETA(1)'])
+    assert a.eta_derivatives == ['ETA(1)']
+    a = EstimationStep('foce', predictions=['PRED'])
+    assert a.predictions == ['PRED']
+    a = EstimationStep('foce', residuals=['CWRES'])
+    assert a.residuals == ['CWRES']
+
+
+def test_derive():
+    a = EstimationStep('foce')
+    b = a.derive(method='fo')
+    assert b.method == 'FO'
