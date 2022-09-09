ae928c2c8ab34c7c65a8a2624c97d678461a44ec
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-09-09 20:16:25 +0200

    Increase coverage of pharmpy.lazyload

--- tests/deps/test_deps.py
+++ tests/deps/test_deps.py
@@ -7,6 +7,8 @@ def test_sympy_is_sympy():
 
 
 def test_sympy_dir():
-    from pharmpy.deps import sympy
+    import sympy
 
-    assert 'Symbol' in dir(sympy)
+    from pharmpy.deps import sympy as sympy2
+
+    assert dir(sympy2) == dir(sympy)
