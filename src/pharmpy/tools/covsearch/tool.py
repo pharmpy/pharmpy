aec4be9fcbecdec412778f269526457ebe4e3235
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-09-15 11:48:16 +0200

    Refactor and test input validation for covsearch

--- src/pharmpy/tools/covsearch/tool.py
+++ src/pharmpy/tools/covsearch/tool.py
@@ -6,15 +6,22 @@ from typing import Any, Callable, Iterable, List, Tuple, Union
 from pharmpy.deps import numpy as np
 from pharmpy.deps import pandas as pd
 from pharmpy.model import Model
-from pharmpy.modeling import add_covariate_effect, copy_model
+from pharmpy.modeling import add_covariate_effect, copy_model, get_pk_parameters
 from pharmpy.modeling.lrt import best_of_many as lrt_best_of_many
 from pharmpy.modeling.lrt import p_value as lrt_p_value
 from pharmpy.modeling.lrt import test as lrt_test
 from pharmpy.tools.common import create_results, update_initial_estimates
-from pharmpy.tools.mfl.feature.covariate import EffectLiteral, Spec, parse_spec, spec
+from pharmpy.tools.mfl.feature.covariate import (
+    EffectLiteral,
+    Spec,
+    all_covariate_effects,
+    parse_spec,
+    spec,
+)
 from pharmpy.tools.mfl.parse import parse
 from pharmpy.tools.modelfit import create_fit_workflow
 from pharmpy.tools.scm.results import candidate_summary_dataframe, ofv_summary_dataframe
+from pharmpy.utils import same_signature_as
 from pharmpy.workflows import Task, Workflow, call_workflow
 
 from .results import COVSearchResults
@@ -132,14 +139,7 @@ def create_workflow(
 
     """
 
-    if algorithm not in ALGORITHMS:
-        raise ValueError(f'covsearch only supports algorithm in {sorted(ALGORITHMS)}')
-
-    if p_forward <= 0:
-        raise ValueError(f'p_forward must be positive (got {p_forward})')
-
-    if p_backward <= 0:
-        raise ValueError(f'p_backward must be positive (got {p_backward})')
+    validate_input(effects, p_forward, p_backward, max_steps, algorithm)
 
     wf = Workflow()
     wf.name = NAME_WF
@@ -441,3 +441,85 @@ def _make_df_steps_row(
         'model': candidate.model.name,
         'covariate_effects': np.nan,
     }
+
+
+@same_signature_as(create_workflow)
+def validate_input(effects, p_forward, p_backward, max_steps, algorithm, model):
+    if algorithm not in ALGORITHMS:
+        raise ValueError(
+            f'Invalid algorithm: got "{algorithm}" of type {type(algorithm)},'
+            f' must be one of {sorted(ALGORITHMS)}.'
+        )
+
+    if not isinstance(p_forward, (float, int)) or not 0 < p_forward <= 1:
+        raise ValueError(
+            f'Invalid p_forward: got "{p_forward}" of type {type(p_forward)},'
+            f' must be a float in range (0, 1].'
+        )
+
+    if not isinstance(p_backward, (float, int)) or not 0 < p_backward <= 1:
+        raise ValueError(
+            f'Invalid p_backward: got "{p_backward}" of type {type(p_backward)},'
+            f' must be a float in range (0, 1].'
+        )
+
+    if not isinstance(max_steps, int):
+        raise TypeError(
+            f'Invalid max_steps: got "{max_steps}" of type {type(max_steps)},' f' must be an int.'
+        )
+
+    if model is not None:
+        if not isinstance(model, Model):
+            raise TypeError(
+                f'Invalid model: got "{model}" of type {type(model)}, must be a {Model}.'
+            )
+
+        if isinstance(effects, str):
+            try:
+                parsed = parse(effects)
+            except:  # noqa E722
+                raise ValueError(f'Invalid effects, could not be parsed: "{effects}"')
+            effect_spec = spec(model, parsed)
+        else:
+            effect_spec = effects
+
+        candidate_effects = map(lambda x: Effect(*x), sorted(set(parse_spec(effect_spec))))
+
+        try:
+            di_covariate = model.datainfo.typeix['covariate'].names
+        except IndexError:
+            di_covariate = []
+
+        try:
+            di_unknown = model.datainfo.typeix['unknown'].names
+        except IndexError:
+            di_unknown = []
+
+        allowed_covariates = set(di_covariate).union(di_unknown)
+        allowed_parameters = set(get_pk_parameters(model)).union(
+            str(statement.symbol) for statement in model.statements.before_odes
+        )
+        allowed_covariate_effects = set(all_covariate_effects)
+        allowed_ops = set(['*', '+'])
+
+        for effect in candidate_effects:
+            if effect.covariate not in allowed_covariates:
+                raise ValueError(
+                    f'Invalid covariate found in effects: got "{effect.covariate}",'
+                    f' must be in {sorted(allowed_covariates)}.'
+                )
+            if effect.parameter not in allowed_parameters:
+                raise ValueError(
+                    f'Invalid parameter found in effects: got "{effect.parameter}",'
+                    f' must be in {sorted(allowed_parameters)}.'
+                )
+            if effect.fp not in allowed_covariate_effects:
+                raise ValueError(
+                    f'Invalid effect found in effects: got "{effect.fp}",'
+                    f' must be in {sorted(allowed_covariate_effects)}.'
+                )
+            if effect.operation not in allowed_ops:
+                raise ValueError(
+                    f'Invalid effect found in effects: got "{effect.operation}",'
+                    f' must be in {sorted(allowed_ops)}.'
+                )
---
+++ tests/tools/test_covsearch.py
@@ -0,0 +1,69 @@
+import pytest
+
+from pharmpy.tools.covsearch.tool import create_workflow
+from pharmpy.workflows import Workflow
+
+
+def test_create_workflow():
+    assert isinstance(create_workflow(''), Workflow)
+
+
+@pytest.mark.parametrize(
+    ('model_path', 'effects', 'p_forward', 'p_backward', 'max_steps', 'algorithm'),
+    [
+        (
+            None,
+            '',
+            1.05,
+            0.01,
+            -1,
+            'scm-forward-then-backward',
+        ),
+        (
+            None,
+            '',
+            0.05,
+            1.01,
+            -1,
+            'scm-forward-then-backward',
+        ),
+        (
+            None,
+            '',
+            0.05,
+            0.01,
+            1.2,
+            'scm-forward-then-backward',
+        ),
+        (
+            None,
+            '',
+            0.05,
+            0.01,
+            -1,
+            'scm-backward',
+        ),
+    ],
+)
+def test_create_workflow_raises(
+    load_model_for_test,
+    testdata,
+    model_path,
+    effects,
+    p_forward,
+    p_backward,
+    max_steps,
+    algorithm,
+):
+
+    model = load_model_for_test(testdata.joinpath(*model_path)) if model_path else None
+
+    with pytest.raises((ValueError, TypeError)):
+        create_workflow(
+            effects,
+            p_forward=p_forward,
+            p_backward=p_backward,
+            max_steps=max_steps,
+            algorithm=algorithm,
+            model=model,
+        )
