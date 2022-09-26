5477611b5047a71daa46ac91e2e350ef5afff9c3
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-09-26 15:28:07 +0200

    Increase coverage to 100% for covsearch.validate_input

--- tests/tools/test_covsearch.py
+++ tests/tools/test_covsearch.py
@@ -3,9 +3,30 @@ import pytest
 from pharmpy.tools.covsearch.tool import create_workflow, validate_input
 from pharmpy.workflows import Workflow
 
+MINIMAL_INVALID_MFL_STRING = ''
+MINIMAL_VALID_MFL_STRING = 'LET(x, 0)'
+LARGE_VALID_MFL_STRING = 'COVARIATE(@IIV, @CONTINUOUS, *);COVARIATE(@IIV, @CATEGORICAL, CAT)'
+
 
 def test_create_workflow():
-    assert isinstance(create_workflow(''), Workflow)
+    assert isinstance(create_workflow(MINIMAL_VALID_MFL_STRING), Workflow)
+
+
+def test_create_workflow_with_model(load_model_for_test, testdata):
+    model = load_model_for_test(testdata / 'nonmem' / 'ruvsearch' / 'mox3.mod')
+    assert isinstance(create_workflow(MINIMAL_VALID_MFL_STRING, model=model), Workflow)
+
+
+def test_validate_input():
+    validate_input(MINIMAL_VALID_MFL_STRING)
+
+
+@pytest.mark.parametrize(
+    ('model_path',), ((('nonmem', 'pheno.mod'),), (('nonmem', 'ruvsearch', 'mox3.mod'),))
+)
+def test_validate_input_with_model(load_model_for_test, testdata, model_path):
+    model = load_model_for_test(testdata.joinpath(*model_path))
+    validate_input(LARGE_VALID_MFL_STRING, model=model)
 
 
 @pytest.mark.parametrize(
@@ -13,7 +34,7 @@ def test_create_workflow():
     [
         (
             None,
-            '',
+            MINIMAL_VALID_MFL_STRING,
             1.05,
             0.01,
             -1,
@@ -21,7 +42,7 @@ def test_create_workflow():
         ),
         (
             None,
-            '',
+            MINIMAL_VALID_MFL_STRING,
             0.05,
             1.01,
             -1,
@@ -29,7 +50,7 @@ def test_create_workflow():
         ),
         (
             None,
-            '',
+            MINIMAL_VALID_MFL_STRING,
             0.05,
             0.01,
             1.2,
@@ -37,11 +58,63 @@ def test_create_workflow():
         ),
         (
             None,
-            '',
+            MINIMAL_VALID_MFL_STRING,
             0.05,
             0.01,
             -1,
             'scm-backward',
+        ),
+        (
+            ('nonmem', 'pheno.mod'),
+            MINIMAL_INVALID_MFL_STRING,
+            0.05,
+            0.01,
+            -1,
+            'scm-forward',
+        ),
+        (
+            ('nonmem', 'pheno.mod'),
+            (
+                ('CL', 'WGT', 'exp', '*'),
+                ('VC', 'WGT', 'exp', '*'),
+            ),
+            0.05,
+            0.01,
+            -1,
+            'scm-forward',
+        ),
+        (
+            ('nonmem', 'pheno.mod'),
+            (
+                ('CL', 'WGT', 'exp', '*'),
+                ('V', 'SEX', 'exp', '*'),
+            ),
+            0.05,
+            0.01,
+            -1,
+            'scm-forward',
+        ),
+        (
+            ('nonmem', 'pheno.mod'),
+            (
+                ('CL', 'WGT', 'exp', '*'),
+                ('V', 'WGT', 'abc', '*'),
+            ),
+            0.05,
+            0.01,
+            -1,
+            'scm-forward',
+        ),
+        (
+            ('nonmem', 'pheno.mod'),
+            (
+                ('CL', 'WGT', 'exp', '*'),
+                ('V', 'WGT', 'exp', '-'),
+            ),
+            0.05,
+            0.01,
+            -1,
+            'scm-forward',
         ),
     ],
 )
@@ -67,3 +140,8 @@ def test_validate_input_raises(
             algorithm=algorithm,
             model=model,
         )
+
+
+def test_validate_input_raises_on_wrong_model_type():
+    with pytest.raises(TypeError, match='Invalid model'):
+        validate_input(MINIMAL_VALID_MFL_STRING, model=1)
