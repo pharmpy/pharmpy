3ce84ca2d4bab9b7342d36308c8ba16a91daea47
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-09-28 15:11:39 +0200

    Add regression-covering test for relative $DATA generation

--- tests/integration/test_allometry.py
+++ tests/integration/test_allometry.py
@@ -1,8 +1,10 @@
+import os.path
 import shutil
 
 from pharmpy.model import Model
 from pharmpy.tools import run_tool
 from pharmpy.utils import TemporaryDirectoryChanger
+from pharmpy.workflows import ModelDatabase
 
 
 def test_allometry(tmp_path, testdata):
@@ -16,3 +18,16 @@ def test_allometry(tmp_path, testdata):
         model.datainfo = model.datainfo.derive(path=tmp_path / 'pheno.dta')
         res = run_tool('allometry', model, allometric_variable='WGT')
         assert len(res.summary_models) == 2
+
+        db: ModelDatabase = res.tool_database.model_database
+        sep = os.path.sep
+        model_name = 'scaled_model'
+        assert str(db.retrieve_model(model_name).datainfo.path).endswith(
+            f'{sep}allometry_dir1{sep}models{sep}.datasets{sep}input_model.csv'
+        )
+        path = db.retrieve_file(model_name, f'{model_name}.lst')
+        with open(path, 'r') as fh:
+            fh.readline()  # NOTE skip datetime
+            fh.readline()  # NOTE skip description
+            line = fh.readline()
+            assert line == f'$DATA ..{sep}.datasets{sep}input_model.csv IGNORE=@\n'
