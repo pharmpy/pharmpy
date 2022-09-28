241799dde8b777931061bb6963beff14adbb5493
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-09-28 15:13:23 +0200

    Fix output filename selection bug in write_csv

--- src/pharmpy/modeling/write_csv.py
+++ src/pharmpy/modeling/write_csv.py
@@ -34,7 +34,7 @@ def write_csv(model, path=None, force=False):
         path = Path(path)
     if not path or path.is_dir():
         try:
-            filename = f"{model.datainfo.path.name.with_suffix('.csv')}"
+            filename = f"{model.datainfo.path.with_suffix('.csv').name}"
         except AttributeError:
             filename = f"{model.name + '.csv'}"
         path /= filename
