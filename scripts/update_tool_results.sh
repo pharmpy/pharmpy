b608f01c5ad07277e35c46beed45b46e57829376
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-10-01 18:32:39 +0200

    Make results generated on MacOS compatible with docs building on Linux

--- scripts/update_tool_results.sh
+++ scripts/update_tool_results.sh
@@ -33,6 +33,11 @@ activate
 pip install .
 pip install --index-url https://google-coral.github.io/py-repo/ tflite_runtime
 
+cp_results () {
+  # This converts MacOS-style temp paths to Linux-style ones
+  sed -E 's:"/private/tmp/:"/tmp/:g' "$1" > "$2"
+}
+
 cp $TESTDATA/pheno.mod $TESTPATH
 cp $TESTDATA/pheno.dta $TESTPATH
 cp $TESTDATA/pheno.lst $TESTPATH
@@ -41,7 +46,7 @@ cp $TESTDATA/pheno.phi $TESTPATH
 
 if [ "$TOOL" == 'allometry' ] || [ "$TOOL" == 'all' ]; then
   pharmpy run allometry $TESTPATH/pheno.mod --allometric_variable=WGT --path $TESTPATH/allometry/
-  cp $TESTPATH/allometry/results.json $DEST/allometry_results.json
+  cp_results $TESTPATH/allometry/results.json $DEST/allometry_results.json
 fi
 
 cp $TESTDATA/models/mox2.mod $TESTPATH
@@ -53,26 +58,26 @@ cp $TESTDATA/models/mox_simulated_normal.csv $TESTPATH
 
 if [ "$TOOL" == 'modelsearch' ] || [ "$TOOL" == 'all' ]; then
   pharmpy run modelsearch $TESTPATH/mox2.mod 'PERIPHERALS(1);LAGTIME()' 'reduced_stepwise' --path $TESTPATH/modelsearch/
-  cp $TESTPATH/modelsearch/results.json $DEST/modelsearch_results.json
+  cp_results $TESTPATH/modelsearch/results.json $DEST/modelsearch_results.json
   cp $TESTPATH/modelsearch/metadata.json $DEST/metadata.json
   cp -r $TESTPATH/modelsearch $DEST/tool_databases
 fi
 
 if [ "$TOOL" == 'iivsearch' ] || [ "$TOOL" == 'all' ]; then
   pharmpy run iivsearch $TESTPATH/mox2.mod 'brute_force' --path $TESTPATH/iivsearch/
-  cp $TESTPATH/iivsearch/results.json $DEST/iivsearch_results.json
+  cp_results $TESTPATH/iivsearch/results.json $DEST/iivsearch_results.json
 fi
 
 if [ "$TOOL" == 'iovsearch' ] || [ "$TOOL" == 'all' ]; then
   pharmpy run iovsearch $TESTPATH/mox2.mod --column 'VISI' --path $TESTPATH/iovsearch/
-  cp $TESTPATH/iovsearch/results.json $DEST/iovsearch_results.json
+  cp_results $TESTPATH/iovsearch/results.json $DEST/iovsearch_results.json
 fi
 
 if [ "$TOOL" == 'covsearch' ] || [ "$TOOL" == 'all' ]; then
   pharmpy run covsearch $TESTPATH/mox2.mod \
         --effects 'COVARIATE([CL, MAT, VC], [AGE, WT], EXP);COVARIATE([CL, MAT, VC], [SEX], CAT)' \
         --path $TESTPATH/covsearch/
-  cp $TESTPATH/covsearch/results.json $DEST/covsearch_results.json
+  cp_results $TESTPATH/covsearch/results.json > $DEST/covsearch_results.json
 fi
 
 if [ "$TOOL" == 'resmod' ] || [ "$TOOL" == 'all' ]; then
@@ -80,5 +85,5 @@ if [ "$TOOL" == 'resmod' ] || [ "$TOOL" == 'all' ]; then
   cp $TESTDATA/resmod/moxo_simulated_resmod.csv $TESTPATH
   cp $TESTDATA/resmod/mytab $TESTPATH
   pharmpy run resmod $TESTPATH/mox3.mod --path $TESTPATH/resmod/
-  cp $TESTPATH/resmod/results.json $DEST/resmod_results.json
+  cp_results $TESTPATH/resmod/results.json $DEST/resmod_results.json
 fi
