261be69e8bcf8f45bf69c8db8c832f108262dcfd
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-10-06 20:26:06 +0200

    Fix indentation in ruvsearch.results's psn_resmod_results

--- src/pharmpy/tools/ruvsearch/results.py
+++ src/pharmpy/tools/ruvsearch/results.py
@@ -117,22 +117,22 @@ def psn_resmod_results(path):
     respath = path / 'resmod_results.csv'
     if respath.is_file():
         df = pd.read_csv(respath, names=range(40), skiprows=[0], engine='python')
-    df[0].fillna(1, inplace=True)
-    df[1].fillna(1, inplace=True)
-    df.dropna(how='all', axis=1, inplace=True)
-    df2 = df[[0, 1, 2, 3]].copy()
-    df2 = df2.astype({0: int})
-    df2.columns = ['iteration', 'DVID', 'model', 'dOFV']
-    df2.set_index(['iteration', 'DVID', 'model'], inplace=True)
-    parameters = pd.Series(name='parameters', index=df.index, dtype=object)
-    for rowind, row in df.iterrows():
-        d = dict()
-        for i in range(4, len(row)):
-            if row[i] is not None:
-                a = row[i].split('=')
-                d[a[0]] = float(a[1])
-        parameters[rowind] = d
-    parameters.index = df2.index
-    df2['parameters'] = parameters
-    res.cwres_models = df2
+        df[0].fillna(1, inplace=True)
+        df[1].fillna(1, inplace=True)
+        df.dropna(how='all', axis=1, inplace=True)
+        df2 = df[[0, 1, 2, 3]].copy()
+        df2 = df2.astype({0: int})
+        df2.columns = ['iteration', 'DVID', 'model', 'dOFV']
+        df2.set_index(['iteration', 'DVID', 'model'], inplace=True)
+        parameters = pd.Series(name='parameters', index=df.index, dtype=object)
+        for rowind, row in df.iterrows():
+            d = dict()
+            for i in range(4, len(row)):
+                if row[i] is not None:
+                    a = row[i].split('=')
+                    d[a[0]] = float(a[1])
+            parameters[rowind] = d
+        parameters.index = df2.index
+        df2['parameters'] = parameters
+        res.cwres_models = df2
     return res
