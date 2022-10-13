pharmpy/pharmpy

commit 463b17a6d7d72b483949c0fd615c03c416084ce6

Move rank_models from modeling to tools

src/pharmpy/tools/run.py
@@ -10,7 +10,15 @@ import pharmpy.tools.modelfit
 from pharmpy.deps import numpy as np
 from pharmpy.deps import pandas as pd
 from pharmpy.model import Model, Results
-from pharmpy.modeling import check_high_correlations, copy_model, read_model_from_database
+from pharmpy.modeling import (
+    calculate_aic,
+    calculate_bic,
+    check_high_correlations,
+    copy_model,
+    read_model_from_database,
+)
+from pharmpy.modeling.lrt import degrees_of_freedom as lrt_df
+from pharmpy.modeling.lrt import test as lrt_test
 from pharmpy.tools.psn_helpers import create_results as psn_create_results
 from pharmpy.utils import normalize_user_given_path
 from pharmpy.workflows import execute_workflow, split_common_options
@@ -504,3 +512,147 @@ def summarize_errors(models):
         df = pd.DataFrame(columns=col_names, index=index)
 
     return df.sort_index()
+
+
+def rank_models(
+    base_model, models, errors_allowed=None, rank_type='ofv', cutoff=None, bic_type='mixed'
+) -> pd.DataFrame:
+    """Ranks a list of models
+
+    Ranks a list of models with a given ranking function
+
+    Parameters
+    ----------
+    base_model : Model
+        Base model to compare to
+    models : list
+        List of models
+    errors_allowed : list or None
+        List of errors that are allowed for ranking. Currently available is: rounding_errors and
+        maxevals_exceeded. Default is None
+    rank_type : str
+        Name of ranking type. Available options are 'ofv', 'aic', 'bic', 'lrt' (OFV with LRT)
+    cutoff : float or None
+        Value to use as cutoff. If using LRT, cutoff denotes p-value. Default is None
+    bic_type : str
+        Type of BIC to calculate. Default is the mixed effects.
+
+    Return
+    ------
+    pd.DataFrame
+        DataFrame of the ranked models
+
+    Examples
+    --------
+    >>> from pharmpy.modeling import load_example_model, summarize_modelfit_results
+    >>> from pharmpy.tools import rank_models
+    >>> model_1 = load_example_model("pheno")
+    >>> model_2 = load_example_model("pheno_linear")
+    >>> rank_models(model_1, [model_2],
+    ...             errors_allowed=['rounding_errors'],
+    ...             rank_type='lrt') # doctest: +SKIP
+    """
+    models_all = [base_model] + models
+
+    rank_values, delta_values = {}, {}
+    models_to_rank = []
+
+    ref_value = _get_rankval(base_model, rank_type, bic_type)
+    model_dict = {model.name: model for model in models_all}
+
+    # Filter on strictness
+    for model in models_all:
+        # Exclude OFV etc. if model was not successful
+        if not model.modelfit_results or np.isnan(model.modelfit_results.ofv):
+            continue
+        if not model.modelfit_results.minimization_successful:
+            if errors_allowed:
+                if model.modelfit_results.termination_cause not in errors_allowed:
+                    continue
+                if np.isnan(model.modelfit_results.significant_digits):
+                    continue
+            else:
+                continue
+
+        rank_value = _get_rankval(model, rank_type, bic_type)
+        if rank_type == 'lrt':
+            parent = model_dict[model.parent_model]
+            if cutoff is None:
+                co = 0.05 if lrt_df(parent, model) >= 0 else 0.01
+            elif isinstance(cutoff, tuple):
+                co = cutoff[0] if lrt_df(parent, model) >= 0 else cutoff[1]
+            else:
+                assert isinstance(cutoff, (float, int))
+                co = cutoff
+            if not lrt_test(
+                parent, model, parent.modelfit_results.ofv, model.modelfit_results.ofv, co
+            ):
+                continue
+        elif cutoff is not None:
+            if ref_value - rank_value <= cutoff:
+                continue
+
+        # Add ranking value and model
+        rank_values[model.name] = rank_value
+        delta_values[model.name] = ref_value - rank_value
+        models_to_rank.append(model)
+
+    # Sort
+    def _get_delta(model):
+        if np.isnan(ref_value):
+            return -rank_values[model.name]
+        return delta_values[model.name]
+
+    models_sorted = sorted(models_to_rank, key=_get_delta, reverse=True)
+
+    # Create rank for models, if two have the same value they will have the same rank
+    ranking = dict()
+    rank, count, prev = 0, 0, None
+    for model in models_sorted:
+        count += 1
+        value = _get_delta(model)
+        if value != prev:
+            rank += count
+            prev = value
+            count = 0
+        ranking[model.name] = rank
+
+    rows = dict()
+    for model in models_all:
+        delta, rank_value, rank = np.nan, np.nan, np.nan
+        if model.name in ranking.keys():
+            rank = ranking[model.name]
+        if model.name in rank_values.keys():
+            rank_value = rank_values[model.name]
+        if model.name in delta_values.keys():
+            delta = delta_values[model.name]
+
+        rows[model.name] = (delta, rank_value, rank)
+
+    if rank_type == 'lrt':
+        rank_type_name = 'ofv'
+    else:
+        rank_type_name = rank_type
+
+    index = pd.Index(rows.keys(), name='model')
+    df = pd.DataFrame(
+        rows.values(), index=index, columns=[f'd{rank_type_name}', f'{rank_type_name}', 'rank']
+    )
+
+    if np.isnan(ref_value):
+        return df.sort_values(by=[f'{rank_type_name}'])
+    else:
+        return df.sort_values(by=[f'd{rank_type_name}'], ascending=False)
+
+
+def _get_rankval(model, rank_type, bic_type):
+    if not model.modelfit_results:
+        return np.nan
+    if rank_type in ['ofv', 'lrt']:
+        return model.modelfit_results.ofv
+    elif rank_type == 'aic':
+        return calculate_aic(model, model.modelfit_results.ofv)
+    elif rank_type == 'bic':
+        return calculate_bic(model, model.modelfit_results.ofv, bic_type)
+    else:
+        raise ValueError('Unknown rank_type: must be ofv, lrt, aic, or bic')

src/pharmpy/modeling/__init__.py
@@ -179,7 +179,6 @@ from .results import (
     calculate_pk_parameters_statistics,
     check_high_correlations,
     check_parameters_near_bounds,
-    rank_models,
     summarize_modelfit_results,
 )
 from .units import get_unit_of
@@ -291,7 +290,6 @@ __all__ = [
     'plot_iofv_vs_iofv',
     'print_model_code',
     'print_model_symbols',
-    'rank_models',
     'read_dataset_from_datainfo',
     'read_model',
     'read_model_from_database',

src/pharmpy/modeling/results.py
@@ -18,8 +18,6 @@ from pharmpy.model.random_variables import (
 )
 
 from .data import get_ids, get_observations
-from .lrt import degrees_of_freedom as lrt_df
-from .lrt import test as lrt_test
 from .parameter_sampling import create_rng, sample_parameters_from_covariance_matrix
 
 RANK_TYPES = frozenset(('ofv', 'lrt', 'aic', 'bic'))
@@ -643,149 +641,6 @@ def summarize_modelfit_results(models, include_all_estimation_steps=False):
     df = pd.concat(summaries)
 
     return df
-
-
-def rank_models(
-    base_model, models, errors_allowed=None, rank_type='ofv', cutoff=None, bic_type='mixed'
-) -> pd.DataFrame:
-    """Ranks a list of models
-
-    Ranks a list of models with a given ranking function
-
-    Parameters
-    ----------
-    base_model : Model
-        Base model to compare to
-    models : list
-        List of models
-    errors_allowed : list or None
-        List of errors that are allowed for ranking. Currently available is: rounding_errors and
-        maxevals_exceeded. Default is None
-    rank_type : str
-        Name of ranking type. Available options are 'ofv', 'aic', 'bic', 'lrt' (OFV with LRT)
-    cutoff : float or None
-        Value to use as cutoff. If using LRT, cutoff denotes p-value. Default is None
-    bic_type : str
-        Type of BIC to calculate. Default is the mixed effects.
-
-    Return
-    ------
-    pd.DataFrame
-        DataFrame of the ranked models
-
-    Examples
-    --------
-    >>> from pharmpy.modeling import load_example_model, summarize_modelfit_results
-    >>> model_1 = load_example_model("pheno")
-    >>> model_2 = load_example_model("pheno_linear")
-    >>> rank_models(model_1, [model_2],
-    ...             errors_allowed=['rounding_errors'],
-    ...             rank_type='lrt') # doctest: +SKIP
-    """
-    models_all = [base_model] + models
-
-    rank_values, delta_values = {}, {}
-    models_to_rank = []
-
-    ref_value = _get_rankval(base_model, rank_type, bic_type)
-    model_dict = {model.name: model for model in models_all}
-
-    # Filter on strictness
-    for model in models_all:
-        # Exclude OFV etc. if model was not successful
-        if not model.modelfit_results or np.isnan(model.modelfit_results.ofv):
-            continue
-        if not model.modelfit_results.minimization_successful:
-            if errors_allowed:
-                if model.modelfit_results.termination_cause not in errors_allowed:
-                    continue
-                if np.isnan(model.modelfit_results.significant_digits):
-                    continue
-            else:
-                continue
-
-        rank_value = _get_rankval(model, rank_type, bic_type)
-        if rank_type == 'lrt':
-            parent = model_dict[model.parent_model]
-            if cutoff is None:
-                co = 0.05 if lrt_df(parent, model) >= 0 else 0.01
-            elif isinstance(cutoff, tuple):
-                co = cutoff[0] if lrt_df(parent, model) >= 0 else cutoff[1]
-            else:
-                assert isinstance(cutoff, (float, int))
-                co = cutoff
-            if not lrt_test(
-                parent, model, parent.modelfit_results.ofv, model.modelfit_results.ofv, co
-            ):
-                continue
-        elif cutoff is not None:
-            if ref_value - rank_value <= cutoff:
-                continue
-
-        # Add ranking value and model
-        rank_values[model.name] = rank_value
-        delta_values[model.name] = ref_value - rank_value
-        models_to_rank.append(model)
-
-    # Sort
-    def _get_delta(model):
-        if np.isnan(ref_value):
-            return -rank_values[model.name]
-        return delta_values[model.name]
-
-    models_sorted = sorted(models_to_rank, key=_get_delta, reverse=True)
-
-    # Create rank for models, if two have the same value they will have the same rank
-    ranking = dict()
-    rank, count, prev = 0, 0, None
-    for model in models_sorted:
-        count += 1
-        value = _get_delta(model)
-        if value != prev:
-            rank += count
-            prev = value
-            count = 0
-        ranking[model.name] = rank
-
-    rows = dict()
-    for model in models_all:
-        delta, rank_value, rank = np.nan, np.nan, np.nan
-        if model.name in ranking.keys():
-            rank = ranking[model.name]
-        if model.name in rank_values.keys():
-            rank_value = rank_values[model.name]
-        if model.name in delta_values.keys():
-            delta = delta_values[model.name]
-
-        rows[model.name] = (delta, rank_value, rank)
-
-    if rank_type == 'lrt':
-        rank_type_name = 'ofv'
-    else:
-        rank_type_name = rank_type
-
-    index = pd.Index(rows.keys(), name='model')
-    df = pd.DataFrame(
-        rows.values(), index=index, columns=[f'd{rank_type_name}', f'{rank_type_name}', 'rank']
-    )
-
-    if np.isnan(ref_value):
-        return df.sort_values(by=[f'{rank_type_name}'])
-    else:
-        return df.sort_values(by=[f'd{rank_type_name}'], ascending=False)
-
-
-def _get_rankval(model, rank_type, bic_type):
-    if not model.modelfit_results:
-        return np.nan
-    if rank_type in ['ofv', 'lrt']:
-        return model.modelfit_results.ofv
-    elif rank_type == 'aic':
-        return calculate_aic(model, model.modelfit_results.ofv)
-    elif rank_type == 'bic':
-        return calculate_bic(model, model.modelfit_results.ofv, bic_type)
-    else:
-        raise ValueError('Unknown rank_type: must be ofv, lrt, aic, or bic')
 
 
 def calculate_aic(model, likelihood):

src/pharmpy/tools/__init__.py
@@ -7,6 +7,7 @@ __all__ = (
     'predict_influential_outliers',  # pyright: ignore [reportUnsupportedDunderAll]
     'predict_outliers',  # pyright: ignore [reportUnsupportedDunderAll]
     'print_fit_summary',  # pyright: ignore [reportUnsupportedDunderAll]
+    'rank_models',  # pyright: ignore [reportUnsupportedDunderAll]
     'read_results',  # pyright: ignore [reportUnsupportedDunderAll]
     'retrieve_final_model',  # pyright: ignore [reportUnsupportedDunderAll]
     'retrieve_models',  # pyright: ignore [reportUnsupportedDunderAll]
@@ -31,6 +32,7 @@ _run_keys = {
     'create_results',
     'fit',
     'print_fit_summary',
+    'rank_models',
     'read_results',
     'retrieve_final_model',
     'retrieve_models',

src/pharmpy/tools/common.py
@@ -3,8 +3,8 @@ from typing import Any
 
 from pharmpy.deps import numpy as np
 from pharmpy.deps import pandas as pd
-from pharmpy.modeling import rank_models, summarize_modelfit_results, update_inits
-from pharmpy.tools import summarize_errors
+from pharmpy.modeling import summarize_modelfit_results, update_inits
+from pharmpy.tools import rank_models, summarize_errors
 
 from .funcs import summarize_individuals, summarize_individuals_count_table
 

src/pharmpy/tools/iovsearch/tool.py
@@ -3,16 +3,10 @@ from typing import Callable, Iterable, List, Optional, Tuple, TypeVar, Union
 
 from pharmpy.deps import sympy
 from pharmpy.model import Assignment, Model, Results
-from pharmpy.modeling import (
-    add_iov,
-    copy_model,
-    get_pk_parameters,
-    rank_models,
-    remove_iiv,
-    remove_iov,
-)
+from pharmpy.modeling import add_iov, copy_model, get_pk_parameters, remove_iiv, remove_iov
 from pharmpy.modeling.eta_additions import ADD_IOV_DISTRIBUTION
 from pharmpy.modeling.results import RANK_TYPES
+from pharmpy.tools import rank_models
 from pharmpy.tools.common import create_results, update_initial_estimates
 from pharmpy.tools.modelfit import create_fit_workflow
 from pharmpy.utils import runtime_type_check, same_arguments_as

tests/modeling/test_results.py
@@ -13,7 +13,6 @@ from pharmpy.modeling import (
     calculate_individual_shrinkage,
     calculate_pk_parameters_statistics,
     check_parameters_near_bounds,
-    rank_models,
     set_iiv_on_ruv,
     summarize_modelfit_results,
 )
@@ -239,92 +238,6 @@ def test_summarize_modelfit_results_errors(load_model_for_test, testdata, tmp_pa
         assert summary.loc['pheno_no_header']['warnings_found'] == 1
         assert summary.loc['pheno_rounding_error']['errors_found'] == 2
         assert summary.loc['pheno_rounding_error']['warnings_found'] == 0
-
-
-class DummyModel:
-    def __init__(self, name, parent, parameter_names, **kwargs):
-        self.name = name
-        self.parameters = parameter_names
-        self.parent_model = parent
-        self.modelfit_results = DummyResults(**kwargs)
-
-
-class DummyResults:
-    def __init__(
-        self, ofv, minimization_successful=True, termination_cause=None, significant_digits=5
-    ):
-        self.ofv = ofv
-        self.minimization_successful = minimization_successful
-        self.termination_cause = termination_cause
-        # 5 is an arbitrary number, this is relevant in test if sig. digits is unreportable (NaN)
-        self.significant_digits = significant_digits
-
-
-def test_rank_models():
-    base = DummyModel('base', parent='base', parameter_names=['p1'], ofv=0)
-    m1 = DummyModel(
-        'm1',
-        parent='base',
-        parameter_names=['p1', 'p2'],
-        ofv=-5,
-        minimization_successful=False,
-        termination_cause='rounding_errors',
-    )
-    m2 = DummyModel('m2', parent='base', parameter_names=['p1', 'p2'], ofv=-4)
-    m3 = DummyModel('m3', parent='base', parameter_names=['p1', 'p2', 'p3'], ofv=-4)
-    m4 = DummyModel('m4', parent='base', parameter_names=['p1'], ofv=1)
-
-    models = [m1, m2, m3, m4]
-
-    df = rank_models(base, models, rank_type='ofv')
-    assert len(df) == 5
-    best_model = df.loc[df['rank'] == 1].index.values
-    assert list(best_model) == ['m2', 'm3']
-
-    # Test if rounding errors are allowed
-    df = rank_models(base, models, errors_allowed=['rounding_errors'], rank_type='ofv')
-    best_model = df.loc[df['rank'] == 1].index.values
-    assert list(best_model) == ['m1']
-    ranked_models = df.dropna().index.values
-    assert len(ranked_models) == 5
-
-    # Test with a cutoff of dOFV=1
-    df = rank_models(base, models, rank_type='ofv', cutoff=1)
-    ranked_models = df.dropna().index.values
-    assert len(ranked_models) == 2
-
-    # Test with LRT
-    df = rank_models(base, models, rank_type='lrt', cutoff=0.05)
-    ranked_models = list(df.dropna().index.values)
-    assert sorted(ranked_models) == ['base', 'm2']
-
-    # Test if candidate model does not have an OFV
-    m5 = DummyModel('m5', parent='base', parameter_names=['p1'], ofv=np.nan)
-    df = rank_models(base, models + [m5], rank_type='ofv')
-    ranked_models = list(df.dropna().index.values)
-    assert 'm5' not in ranked_models
-    assert np.isnan(df.loc['m5']['rank'])
-
-    # Test if model has minimized but has unreportable number of significant digits while still allowing rounding
-    # errors
-    m6 = DummyModel(
-        'm6',
-        parent='base',
-        parameter_names=['p1'],
-        ofv=-5,
-        minimization_successful=False,
-        termination_cause='rounding_errors',
-        significant_digits=np.nan,
-    )
-    df = rank_models(base, models + [m6], errors_allowed=['rounding_errors'], rank_type='ofv')
-    ranked_models = list(df.dropna().index.values)
-    assert 'm6' not in ranked_models
-    assert np.isnan(df.loc['m6']['rank'])
-
-    # Test if base model failed, fall back to rank value
-    base_nan = DummyModel('base_nan', parent='base_nan', parameter_names=['p1'], ofv=np.nan)
-    df = rank_models(base_nan, models, errors_allowed=['rounding_errors'], rank_type='ofv')
-    assert df.iloc[0].name == 'm1'
 
 
 def test_aic(load_model_for_test, testdata):

tests/tools/test_run.py
@@ -7,11 +7,13 @@ from pathlib import Path
 import pytest
 
 import pharmpy
+from pharmpy.deps import numpy as np
 from pharmpy.results import read_results
 from pharmpy.tools.run import (
     _create_metadata_common,
     _create_metadata_tool,
     _get_run_setup,
+    rank_models,
     retrieve_final_model,
     retrieve_models,
     summarize_errors,
@@ -206,3 +208,89 @@ def test_summarize_errors(load_model_for_test, testdata, tmp_path, pheno_path):
         assert len(summary.loc[('pheno_no_header', 'WARNING')]) == 1
         assert len(summary.loc[('pheno_no_header', 'ERROR')]) == 2
         assert len(summary.loc[('pheno_rounding_error', 'ERROR')]) == 2
+
+
+class DummyModel:
+    def __init__(self, name, parent, parameter_names, **kwargs):
+        self.name = name
+        self.parameters = parameter_names
+        self.parent_model = parent
+        self.modelfit_results = DummyResults(**kwargs)
+
+
+class DummyResults:
+    def __init__(
+        self, ofv, minimization_successful=True, termination_cause=None, significant_digits=5
+    ):
+        self.ofv = ofv
+        self.minimization_successful = minimization_successful
+        self.termination_cause = termination_cause
+        # 5 is an arbitrary number, this is relevant in test if sig. digits is unreportable (NaN)
+        self.significant_digits = significant_digits
+
+
+def test_rank_models():
+    base = DummyModel('base', parent='base', parameter_names=['p1'], ofv=0)
+    m1 = DummyModel(
+        'm1',
+        parent='base',
+        parameter_names=['p1', 'p2'],
+        ofv=-5,
+        minimization_successful=False,
+        termination_cause='rounding_errors',
+    )
+    m2 = DummyModel('m2', parent='base', parameter_names=['p1', 'p2'], ofv=-4)
+    m3 = DummyModel('m3', parent='base', parameter_names=['p1', 'p2', 'p3'], ofv=-4)
+    m4 = DummyModel('m4', parent='base', parameter_names=['p1'], ofv=1)
+
+    models = [m1, m2, m3, m4]
+
+    df = rank_models(base, models, rank_type='ofv')
+    assert len(df) == 5
+    best_model = df.loc[df['rank'] == 1].index.values
+    assert list(best_model) == ['m2', 'm3']
+
+    # Test if rounding errors are allowed
+    df = rank_models(base, models, errors_allowed=['rounding_errors'], rank_type='ofv')
+    best_model = df.loc[df['rank'] == 1].index.values
+    assert list(best_model) == ['m1']
+    ranked_models = df.dropna().index.values
+    assert len(ranked_models) == 5
+
+    # Test with a cutoff of dOFV=1
+    df = rank_models(base, models, rank_type='ofv', cutoff=1)
+    ranked_models = df.dropna().index.values
+    assert len(ranked_models) == 2
+
+    # Test with LRT
+    df = rank_models(base, models, rank_type='lrt', cutoff=0.05)
+    ranked_models = list(df.dropna().index.values)
+    assert sorted(ranked_models) == ['base', 'm2']
+
+    # Test if candidate model does not have an OFV
+    m5 = DummyModel('m5', parent='base', parameter_names=['p1'], ofv=np.nan)
+    df = rank_models(base, models + [m5], rank_type='ofv')
+    ranked_models = list(df.dropna().index.values)
+    assert 'm5' not in ranked_models
+    assert np.isnan(df.loc['m5']['rank'])
+
+    # Test if model has minimized but has unreportable number of significant digits while still allowing rounding
+    # errors
+    m6 = DummyModel(
+        'm6',
+        parent='base',
+        parameter_names=['p1'],
+        ofv=-5,
+        minimization_successful=False,
+        termination_cause='rounding_errors',
+        significant_digits=np.nan,
+    )
+    df = rank_models(base, models + [m6], errors_allowed=['rounding_errors'], rank_type='ofv')
+    ranked_models = list(df.dropna().index.values)
+    assert 'm6' not in ranked_models
+    assert np.isnan(df.loc['m6']['rank'])
+
+    # Test if base model failed, fall back to rank value
+    base_nan = DummyModel('base_nan', parent='base_nan', parameter_names=['p1'], ofv=np.nan)
+    df = rank_models(base_nan, models, errors_allowed=['rounding_errors'], rank_type='ofv')
+    assert df.iloc[0].name == 'm1'

commit 764cecdef2d1d1c9fb944f9cbc9ab5a659b1c066

Fix importtime regression introduced in 463b17a6

src/pharmpy/tools/run.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import importlib
 import inspect
 from datetime import datetime