pharmpy/pharmpy

commit 6776c31f3b484938ec579f7aa2dd852e97740f31

Update modeling.lrt to not use modelfit_results

src/pharmpy/tools/covsearch/tool.py
@@ -276,7 +276,13 @@ def _greedy_search(
         new_candidate_models = map(lambda candidate: candidate.model, new_candidates)
 
         parent = best_candidate_so_far.model
-        best_model_so_far = lrt_best_of_many(parent, new_candidate_models, alpha)
+        ofvs = [
+            model.modelfit_results.ofv if model.modelfit_results is not None else np.nan
+            for model in new_candidate_models
+        ]
+        best_model_so_far = lrt_best_of_many(
+            parent, new_candidate_models, parent.modelfit_results.ofv, ofvs, alpha
+        )
 
         if best_model_so_far is parent:
             break
@@ -422,7 +428,9 @@ def _make_df_steps_row(
     last_step = candidate.steps[-1]
     last_effect = last_step.effect
     is_backward = isinstance(last_step, BackwardStep)
-    p_value = lrt_p_value(parent_model, model)
+    p_value = lrt_p_value(
+        parent_model, model, parent_model.modelfit_results.ofv, model.modelfit_results.ofv
+    )
     alpha = last_step.alpha
     selected = children_count[candidate.model.name] >= 1 or candidate.model is best_model
     extended_significant = lrt_test(parent_model, candidate.model, alpha)

src/pharmpy/modeling/lrt.py
@@ -1,5 +1,4 @@
-from itertools import chain
-from typing import Iterable, List, Union
+from typing import Iterable
 
 from pharmpy.deps import numpy as np
 from pharmpy.deps.scipy import stats
@@ -12,13 +11,6 @@ def _ofv(model: Model) -> float:
 
 def _dofv(parent: Model, model: Model) -> float:
     return _ofv(parent) - _ofv(model)
-
-
-def _best_by_ofv(models: Iterable[Model], default: Union[None, Model] = None) -> Model:
-    if default is None:
-        return min(models, key=_ofv)
-    else:
-        return min(models, key=_ofv, default=default)
 
 
 def degrees_of_freedom(parent: Model, child: Model) -> int:
@@ -36,32 +28,23 @@ def cutoff(parent: Model, child: Model, alpha: float) -> float:
     )
 
 
-def p_value(parent: Model, child: Model) -> float:
-    x = _dofv(parent, child)
+def p_value(parent: Model, child: Model, parent_ofv, child_ofv) -> float:
+    dofv = parent_ofv - child_ofv
     df = degrees_of_freedom(parent, child)
-    return float(stats.chi2.sf(x=x, df=df))
+    return float(stats.chi2.sf(x=dofv, df=df))
 
 
-def test(parent: Model, child: Model, alpha: float) -> bool:
-    return _dofv(parent, child) >= cutoff(parent, child, alpha)
+def test(parent: Model, child: Model, parent_ofv, child_ofv, alpha: float) -> bool:
+    dofv = parent_ofv - child_ofv
+    return dofv >= cutoff(parent, child, alpha)
 
 
 def best_of_two(parent: Model, child: Model, alpha: float) -> Model:
     return child if test(parent, child, alpha) else parent
 
 
-def best_of_many(parent: Model, models: Iterable[Model], alpha: float) -> Model:
-    best_candidate = _best_by_ofv(models)
-    return best_of_two(parent, best_candidate, alpha)
-
-
-def best_of_subtree(root: Model, nodes: List[Model], alpha: float) -> Model:
-    models_dict = {model.name: model for model in chain((root,), nodes)}
-
-    def parent(model: Model):
-        return models_dict[model.parent_model]
-
-    return _best_by_ofv(
-        filter(lambda model: test(parent(model), model, alpha), nodes),
-        default=root,
-    )
+def best_of_many(
+    parent: Model, models: Iterable[Model], parent_ofv, model_ofvs, alpha: float
+) -> Model:
+    best_index = np.argmax(model_ofvs)
+    return best_of_two(parent, models[best_index], alpha)

src/pharmpy/modeling/results.py
@@ -742,7 +742,9 @@ def rank_models(
             else:
                 assert isinstance(cutoff, (float, int))
                 co = cutoff
-            if not lrt_test(parent, model, co):
+            if not lrt_test(
+                parent, model, parent.modelfit_results.ofv, model.modelfit_results.ofv, co
+            ):
                 continue
         elif cutoff is not None:
             if ref_value - rank_value <= cutoff:

commit 951e1e831e2505764a1ba01914e2a9be10e717f1

Guard against accessing ofv when Model#modelfit_results is None
Regression introduced in 6776c31f

src/pharmpy/tools/covsearch/tool.py
@@ -277,9 +277,11 @@ def _greedy_search(
 
         parent = best_candidate_so_far.model
         ofvs = [
-            model.modelfit_results.ofv if model.modelfit_results is not None else np.nan
+            np.nan if (mfr := model.modelfit_results) is None else mfr.ofv
             for model in new_candidate_models
         ]
+        # NOTE We assume parent.modelfit_results is not None
+        assert parent.modelfit_results is not None
         best_model_so_far = lrt_best_of_many(
             parent, new_candidate_models, parent.modelfit_results.ofv, ofvs, alpha
         )
@@ -420,27 +422,23 @@ def _make_df_steps_row(
 ):
     model = candidate.model
     parent_model = models_dict[model.parent_model]
-    reduced_ofv = (
-        np.nan if parent_model.modelfit_results is None else parent_model.modelfit_results.ofv
-    )
-    extended_ofv = np.nan if model.modelfit_results is None else model.modelfit_results.ofv
+    reduced_ofv = np.nan if (mfr := parent_model.modelfit_results) is None else mfr.ofv
+    extended_ofv = np.nan if (mfr := model.modelfit_results) is None else mfr.ofv
     ofv_drop = reduced_ofv - extended_ofv
     last_step = candidate.steps[-1]
     last_effect = last_step.effect
     is_backward = isinstance(last_step, BackwardStep)
-    p_value = lrt_p_value(
-        parent_model, model, parent_model.modelfit_results.ofv, model.modelfit_results.ofv
-    )
+    p_value = lrt_p_value(parent_model, model, reduced_ofv, extended_ofv)
     alpha = last_step.alpha
-    selected = children_count[candidate.model.name] >= 1 or candidate.model is best_model
+    selected = children_count[model.name] >= 1 or model is best_model
     extended_significant = lrt_test(
         parent_model,
-        candidate.model,
-        parent_model.modelfit_results.ofv,
-        candidate.model.modelfit_results.ofv,
+        model,
+        reduced_ofv,
+        extended_ofv,
         alpha,
     )
-    assert not selected or (candidate.model is parent_model) or extended_significant
+    assert not selected or (model is parent_model) or extended_significant
     return {
         'step': len(candidate.steps),
         'parameter': last_effect.parameter,
@@ -455,8 +453,8 @@ def _make_df_steps_row(
         'is_backward': is_backward,
         'extended_significant': extended_significant,
         'selected': selected,
-        'directory': str(candidate.model.database.path),
-        'model': candidate.model.name,
+        'directory': str(model.database.path),
+        'model': model.name,
         'covariate_effects': np.nan,
     }
 

src/pharmpy/tools/run.py
@@ -585,9 +585,9 @@ def rank_models(
             else:
                 assert isinstance(cutoff, (float, int))
                 co = cutoff
-            if not lrt_test(
-                parent, model, parent.modelfit_results.ofv, model.modelfit_results.ofv, co
-            ):
+            parent_ofv = np.nan if (mfr := parent.modelfit_results) is None else mfr.ofv
+            model_ofv = np.nan if (mfr := model.modelfit_results) is None else mfr.ofv
+            if not lrt_test(parent, model, parent_ofv, model_ofv, co):
                 continue
         elif cutoff is not None:
             if ref_value - rank_value <= cutoff: