2a777491cfa94852523fbd78d65672ed856776a8
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-10-15 15:11:55 +0200

    Refactor tests/nonmem/test_nonmem_table.py

--- tests/nonmem/test_nonmem_table.py
+++ tests/nonmem/test_nonmem_table.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import pytest
 
-import pharmpy.plugins.nonmem.table as table
+from pharmpy.plugins.nonmem.table import NONMEMTableFile, PhiTable
 from pharmpy.utils import TemporaryDirectoryChanger
 
 
 def test_nonmem_table(pheno_ext):
-    ext_table_file = table.NONMEMTableFile(pheno_ext)
+    ext_table_file = NONMEMTableFile(pheno_ext)
     assert list(ext_table_file.table.data_frame.columns) == [
         'ITERATION',
         'THETA(1)',
@@ -21,7 +21,7 @@ def test_nonmem_table(pheno_ext):
 
 
 def test_ext_table(pheno_ext):
-    ext_table_file = table.NONMEMTableFile(pheno_ext)
+    ext_table_file = NONMEMTableFile(pheno_ext)
     ext_table = ext_table_file.table
     assert ext_table.number == 1
     assert ext_table.is_evaluation is False
@@ -37,7 +37,7 @@ def test_ext_table(pheno_ext):
 
 
 def test_phi_table(pheno_phi):
-    phi_table_file = table.NONMEMTableFile(pheno_phi)
+    phi_table_file = NONMEMTableFile(pheno_phi)
     phi_table = phi_table_file.table
     assert phi_table.problem == 1
     assert phi_table.subproblem == 0
@@ -47,7 +47,7 @@ def test_phi_table(pheno_phi):
 
 
 def test_cov_table(pheno_cov):
-    cov_table_file = table.NONMEMTableFile(pheno_cov)
+    cov_table_file = NONMEMTableFile(pheno_cov)
     cov_table = cov_table_file.table
     df = cov_table.data_frame
     paramnames = ['THETA(1)', 'THETA(2)', 'THETA(3)', 'OMEGA(1,1)', 'OMEGA(2,2)', 'SIGMA(1,1)']
@@ -61,7 +61,7 @@ def test_create_phi_table(tmp_path):
     with TemporaryDirectoryChanger(tmp_path):
         df = pd.DataFrame({'ETA(1)': [1, 2], 'ETA(2)': [5, 6]}, index=[1, 2])
         df.index.name = 'ID'
-        phi = table.PhiTable(df=df)
+        phi = PhiTable(df=df)
         phi.create_content()
         correct = ''' SUBJECT_NO   ID           ETA(1)       ETA(2)      
             1            1  1.00000E+00  5.00000E+00
@@ -70,7 +70,7 @@ def test_create_phi_table(tmp_path):
         print(phi.content)
         assert phi.content == correct
 
-        table_file = table.NONMEMTableFile(tables=[phi])
+        table_file = NONMEMTableFile(tables=[phi])
         table_file.write('my.phi')
         with open('my.phi', 'r') as fp:
             cont = fp.read()
@@ -79,6 +79,6 @@ def test_create_phi_table(tmp_path):
 
 
 def test_errors(testdata):
-    ext_file = table.NONMEMTableFile(testdata / 'nonmem' / 'errors' / 'no_header_error.ext')
+    ext_file = NONMEMTableFile(testdata / 'nonmem' / 'errors' / 'no_header_error.ext')
     with pytest.raises(ValueError):
         ext_file.table_no(2).data_frame
