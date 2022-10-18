27e877b89ec79e52185a3e68eca3589ff4bb63db
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-10-18 16:19:42 +0200

    Extract sympy.Expr evaluation subroutines from model/random_variables

--- src/pharmpy/model/random_variables.py
+++ src/pharmpy/model/random_variables.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from collections.abc import Container as CollectionsContainer
 from collections.abc import Sequence as CollectionsSequence
-from functools import lru_cache
 from itertools import chain, product
 from typing import Container, Dict, Iterable, Sequence, Set, Tuple, Union, overload
 
@@ -10,6 +9,7 @@ import pharmpy.math
 from pharmpy.deps import numpy as np
 from pharmpy.deps import symengine, sympy
 from pharmpy.expressions import subs, sympify, xreplace_dict
+from pharmpy.internals.expressions import eval_expr
 
 from .distributions.numeric import NumericDistribution
 from .distributions.symbolic import Distribution, JointNormalDistribution, NormalDistribution
@@ -799,22 +799,6 @@ def sample_expr_from_rvs(
     return eval_expr(expr, nsamples, samples)
 
 
-def eval_expr(
-    expr: sympy.Expr,
-    nsamples: int,
-    samples: Dict[sympy.Symbol, np.ndarray],
-) -> np.ndarray:
-    # NOTE We avoid querying for free_symbols if we know none are expected
-    fs = _free_symbols(expr) if samples else set()
-
-    if fs:
-        ordered_symbols, fn = _lambdify_canonical(expr)
-        data = [samples[rv] for rv in ordered_symbols]
-        return fn(*data)
-
-    return np.full(nsamples, float(expr.evalf()))
-
-
 def sample_rvs(
     sampling_rvs: Iterable[Tuple[Tuple[sympy.Symbol, ...], NumericDistribution]],
     nsamples: int,
@@ -832,24 +816,3 @@ def sample_rvs(
             data[symbols[0]] = cursample
 
     return data
-
-
-@lru_cache(maxsize=256)
-def _free_symbols(expr: sympy.Expr) -> Set[sympy.Symbol]:
-    return expr.free_symbols
-
-
-@lru_cache(maxsize=256)
-def _lambdify_canonical(expr: sympy.Expr):
-    fs = _free_symbols(expr)
-    ordered_symbols = sorted(fs, key=str)
-    # NOTE Substitution allows to use cse. Otherwise weird things happen with
-    # symbols that look like function eval (e.g. ETA(1), THETA(3), OMEGA(1,1)).
-    ordered_substitutes = [sympy.Symbol(f'__tmp{i}') for i in range(len(ordered_symbols))]
-    substituted_expr = subs(
-        expr,
-        {key: value for key, value in zip(ordered_symbols, ordered_substitutes)},
-        simultaneous=True,
-    )
-    fn = sympy.lambdify(ordered_substitutes, substituted_expr, modules='numpy', cse=True)
-    return ordered_symbols, fn
