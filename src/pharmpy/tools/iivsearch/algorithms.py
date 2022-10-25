3fd1dfb30365e0e78a8139c2f6bf6094753d395b
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-10-25 14:27:20 +0200

    Refactor IIVsearch by generalizing and extracting block structure computations
    The two algorithms are best explained so:
    
      - brute_force_no_of_etas: tries all ETAs subsets (except the one we already have)
        - NOTE i.e removes non-empty subsets of ETAs
      - brute_force_block_structure: tries all ETAs partitions (except the one we already have)
        - NOTE a partition = a block structure
    
    The previous implementation would blow up pretty quickly: partitions
    where generated by enumerating and filtering subsets of maximum size n
    of non-empty subsets of [n]. We only tested n=5, it is certainly slow
    for n=6, and impractical for n=7.
    
    Got rid of the vague "combo" wording.
    
    Would be cool to generate partitions directly in shortlex order (without
    sorting at the end). Also shortlex order is only required to keep the
    original ordering.

--- src/pharmpy/tools/iivsearch/algorithms.py
+++ src/pharmpy/tools/iivsearch/algorithms.py
@@ -1,9 +1,9 @@
-from itertools import chain
-from typing import Dict
+from typing import Dict, Tuple
 
 import pharmpy.tools.modelfit as modelfit
 from pharmpy.internals.expr.subs import subs
-from pharmpy.internals.set.subsets import non_empty_subsets, subsets
+from pharmpy.internals.set.partitions import partitions
+from pharmpy.internals.set.subsets import non_empty_subsets
 from pharmpy.model import Model, RandomVariables
 from pharmpy.modeling import copy_model, remove_iiv
 from pharmpy.modeling.block_rvs import create_joint_distribution, split_joint_distribution
@@ -18,9 +18,8 @@ def brute_force_no_of_etas(base_model, index_offset=0):
     base_model.description = create_description(base_model)
 
     iivs = base_model.random_variables.iiv
-    eta_combos = _get_eta_combinations(iivs)
 
-    for i, combo in enumerate(eta_combos, 1):
+    for i, to_remove in enumerate(non_empty_subsets(iivs), 1):
         model_name = f'iivsearch_run{i + index_offset}'
         task_copy = Task('copy', copy, model_name)
         wf.add_task(task_copy)
@@ -28,7 +27,7 @@ def brute_force_no_of_etas(base_model, index_offset=0):
         task_update_inits = Task('update_inits', update_initial_estimates)
         wf.add_task(task_update_inits, predecessors=task_copy)
 
-        task_remove_eta = Task('remove_eta', remove_eta, combo)
+        task_remove_eta = Task('remove_eta', remove_eta, to_remove)
         wf.add_task(task_remove_eta, predecessors=task_update_inits)
 
         task_update_description = Task('update_description', update_description)
@@ -45,11 +44,10 @@ def brute_force_block_structure(base_model, index_offset=0):
     base_model.description = create_description(base_model)
 
     iivs = base_model.random_variables.iiv
-    eta_combos = _get_eta_combinations(iivs, as_blocks=True)
     model_no = 1 + index_offset
 
-    for combo in eta_combos:
-        if _is_current_block_structure(iivs, combo):
+    for block_structure in _rv_block_structures(iivs):
+        if _is_rv_block_structure(iivs, block_structure):
             continue
 
         model_name = f'iivsearch_run{model_no}'
@@ -59,7 +57,7 @@ def brute_force_block_structure(base_model, index_offset=0):
         task_update_inits = Task('update_inits', update_initial_estimates)
         wf.add_task(task_update_inits, predecessors=task_copy)
 
-        task_joint_dist = Task('create_eta_blocks', create_eta_blocks, combo)
+        task_joint_dist = Task('create_eta_blocks', create_eta_blocks, block_structure)
         wf.add_task(task_joint_dist, predecessors=task_update_inits)
 
         task_update_description = Task('update_description', update_description)
@@ -72,30 +70,14 @@ def brute_force_block_structure(base_model, index_offset=0):
     return wf
 
 
-def _get_eta_combinations(etas, as_blocks=False):
-    # All possible combinations of etas
-    eta_combos = list(map(list, non_empty_subsets(etas.names)))
-    if not as_blocks:
-        return eta_combos
-
-    # All possible combinations of blocks
-    block_combos = []
-    for combo in subsets(eta_combos, min_size=1, max_size=len(etas.names)):
-        combo = list(combo)
-        etas_in_combo = list(chain.from_iterable(combo))
-        etas_unique = set(etas_in_combo)
-        # FIXME Use proper partitioning algorithm
-        if len(etas_in_combo) == len(etas.names) and len(etas_unique) == len(etas.names):
-            block_combos.append(combo)
-    return block_combos
+def _rv_block_structures(etas: RandomVariables):
+    # NOTE All possible partitions of etas into block structures
+    return partitions(etas.names)
 
 
-def _is_current_block_structure(etas, combos):
-    for dist in etas:
-        names = list(dist.names)
-        if names not in combos:
-            return False
-    return True
+def _is_rv_block_structure(etas: RandomVariables, partition: Tuple[Tuple[str, ...], ...]):
+    parts = set(partition)
+    return all(map(lambda dist: dist.names in parts, etas))
 
 
 def _create_param_dict(model: Model, dists: RandomVariables) -> Dict[str, str]:
@@ -147,13 +129,13 @@ def remove_eta(etas, model):
     return model
 
 
-def create_eta_blocks(combos, model):
-    for combo in combos:
-        if len(combo) == 1:
-            split_joint_distribution(model, combo)
+def create_eta_blocks(partition: Tuple[Tuple[str, ...], ...], model: Model):
+    for part in partition:
+        if len(part) == 1:
+            split_joint_distribution(model, part)
         else:
             create_joint_distribution(
-                model, combo, individual_estimates=model.modelfit_results.individual_estimates
+                model, list(part), individual_estimates=model.modelfit_results.individual_estimates
             )
     return model
 
