3c944d52839920111c265d472ed5107a5712efb3
pharmpy-dev-123 <101794402+pharmpy-dev-123@users.noreply.github.com>
2022-11-12 11:00:38 +0100

    Let name of MFL definitions be str instances and not Lark Token instances

--- src/pharmpy/tools/mfl/statement/definition.py
+++ src/pharmpy/tools/mfl/statement/definition.py
@@ -14,7 +14,7 @@ class DefinitionInterpreter(Interpreter):
     def interpret(self, tree):
         children = self.visit_children(tree)
         assert len(children) == 2
-        return Definition(children[0], tuple(children[1]))
+        return Definition(children[0].value, tuple(children[1]))
 
     def value(self, tree):
         children = self.visit_children(tree)
