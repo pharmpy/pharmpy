392d9406e23e8e01fd9ec906f431510a7e394af3
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2022-11-16 12:43:52 +0100

    Fix crash in covsearch CLI

--- src/pharmpy/cli.py
+++ src/pharmpy/cli.py
@@ -183,7 +183,7 @@ def run_covsearch(args):
         p_forward=args.p_forward,
         max_steps=args.max_steps,
         algorithm=args.algorithm,
-        results=args.model.modelfit_restuls,
+        results=args.model.modelfit_results,
         model=args.model,
         path=args.path,
     )
