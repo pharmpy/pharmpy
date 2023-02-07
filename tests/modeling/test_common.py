97e4fed5eacd43da29eaaf3b2faa9d6602adb1ed
Stella <stella.belin@farmaci.uu.se>
2023-02-07 09:11:47 +0100

    Test fix failing unit test by using different model

--- tests/modeling/test_common.py
+++ tests/modeling/test_common.py
@@ -4,7 +4,6 @@ from pathlib import Path
 import pytest
 import sympy
 
-from pharmpy.internals.fs.cwd import chdir
 from pharmpy.modeling import (
     convert_model,
     copy_model,
@@ -69,11 +68,10 @@ $ESTIMATION METHOD=1 INTER MAXEVALS=9990 PRINT=2 POSTHOC
     assert model.parameters['THETA_1'].init == 0.1
 
 
-def test_write_model(testdata, load_model_for_test, tmp_path):
-    model = load_model_for_test(testdata / 'nonmem' / 'minimal.mod')
-    with chdir(tmp_path):
-        write_model(model, tmp_path / 'run1.mod')
-        assert Path(tmp_path / 'run1.mod').is_file()
+def test_write_model(pheno_path, load_model_for_test, tmp_path):
+    model = load_model_for_test(pheno_path)
+    write_model(model, tmp_path / 'run1.mod')
+    assert Path(tmp_path / 'run1.mod').is_file()
 
 
 def test_generate_model_code(testdata, load_model_for_test):
