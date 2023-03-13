ed4d7eeb3920dc3bc7d155c5e99a96fed727cef0
Stella <stella.belin@farmaci.uu.se>
2023-03-13 15:55:26 +0100

    Handle BLQ/LLOQ column in dataset for BLQ transformations

--- src/pharmpy/modeling/blq.py
+++ src/pharmpy/modeling/blq.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pharmpy.deps import sympy
 from pharmpy.internals.expr.funcs import PHI
 from pharmpy.model import Assignment, EstimationSteps, Model
@@ -11,34 +13,54 @@ from .error import get_weighted_error_model_weight
 from .expressions import create_symbol
 
 
-def transform_blq(model: Model, lloq: float):
+def transform_blq(model: Model, lloq: Optional[float] = None):
     if not has_weighted_error_model(model):
         model = set_weighted_error_model(model)
     model = use_thetas_for_error_stdev(model)
 
     sset = model.statements
-    est_steps = model.estimation_steps
 
+    est_steps = model.estimation_steps
     est_steps_new = EstimationSteps([est_step.replace(laplace=True) for est_step in est_steps])
     model = model.replace(estimation_steps=est_steps_new)
 
     # FIXME: handle other DVs?
     y_symb = list(model.dependent_variables.keys())[0]
     y = sset.find_assignment(y_symb)
-
     ipred = y.expression.subs({rv: 0 for rv in model.random_variables.epsilons.names})
     w = get_weighted_error_model_weight(model)
     assert w is not None
 
+    if isinstance(lloq, float):
+        symb_lloq = create_symbol(model, 'LLOQ')
+        lloq_type = 'lloq'
+    else:
+        try:
+            lloq_datainfo = model.datainfo.typeix['blq']
+            lloq_type = 'blq'
+        except IndexError:
+            lloq_datainfo = model.datainfo.typeix['lloq']
+            lloq_type = 'lloq'
+        if len(lloq_datainfo.names) > 1:
+            raise ValueError(f'Can only have one of column type: {lloq_datainfo}')
+        symb_lloq = sympy.Symbol(lloq_datainfo[0].name)
+
     symb_dv = sympy.Symbol(model.datainfo.dv_column.name)
-    symb_lloq = create_symbol(model, 'LLOQ')
     symb_fflag = create_symbol(model, 'F_FLAG')
     symb_cumd = create_symbol(model, 'CUMD')
     symb_cumdz = create_symbol(model, 'CUMDZ')
 
-    is_above_lloq = sympy.GreaterThan(symb_dv, symb_lloq)
+    if lloq_type == 'lloq':
+        is_above_lloq = sympy.GreaterThan(symb_dv, symb_lloq)
+    else:
+        is_above_lloq = sympy.Equality(symb_lloq, 1)
 
-    lloq = Assignment(symb_lloq, sympy.Float(lloq))
+    assignments = []
+
+    if isinstance(lloq, float):
+        lloq = Assignment(symb_lloq, sympy.Float(lloq))
+        assignments.append(lloq)
+
     cumd = Assignment(symb_cumd, PHI((symb_lloq - ipred) / w))
     cumdz = Assignment(symb_cumdz, PHI(-ipred / w))
     fflag = Assignment(symb_fflag, sympy.Piecewise((0, is_above_lloq), (1, True)))
@@ -47,8 +69,10 @@ def transform_blq(model: Model, lloq: float):
         y.symbol, sympy.Piecewise((y.expression, is_above_lloq), (y_below_lloq, True))
     )
 
+    assignments.extend([cumd, cumdz, fflag, y_new])
+
     y_idx = sset.find_assignment_index(y.symbol)
-    sset_new = sset[:y_idx] + [lloq, cumd, cumdz, fflag, y_new] + sset[y_idx + 1 :]
+    sset_new = sset[:y_idx] + assignments + sset[y_idx + 1 :]
     model = model.replace(statements=sset_new)
 
     return model.update_source()
--- tests/modeling/test_blq.py
+++ tests/modeling/test_blq.py
@@ -1,3 +1,4 @@
+from pharmpy.deps import numpy as np
 from pharmpy.modeling.blq import transform_blq
 
 
@@ -12,3 +13,36 @@ def test_transform_blq(load_model_for_test, testdata):
     assert y_below_lloq in model.model_code
     assert 'LAPLACE' in model.model_code
     assert 'DV.GE.LLOQ' in model.model_code
+
+    model = load_model_for_test(testdata / 'nonmem' / 'pheno.mod')
+
+    df_blq = model.dataset
+    df_blq['BLQ'] = np.random.randint(0, 2, df_blq.shape[0])
+    model = model.replace(dataset=df_blq)
+    blq_col = model.datainfo['BLQ'].replace(type='blq')
+    di_blq = model.datainfo.set_column(blq_col)
+    model = model.replace(datainfo=di_blq)
+
+    model_blq = transform_blq(model)
+
+    assert y_above_lloq in model_blq.model_code
+    assert y_below_lloq in model_blq.model_code
+    assert 'LAPLACE' in model_blq.model_code
+    assert 'BLQ.EQ.1' in model_blq.model_code
+
+    model = load_model_for_test(testdata / 'nonmem' / 'pheno.mod')
+
+    df_lloq = model.dataset
+    df_lloq['LLOQ'] = np.random.random(df_lloq.shape[0])
+    model = model.replace(dataset=df_lloq)
+    lloq_col = model.datainfo['LLOQ'].replace(type='lloq')
+    di_lloq = model.datainfo.set_column(lloq_col)
+    model = model.replace(datainfo=di_lloq)
+
+    model_lloq = transform_blq(model)
+
+    assert y_above_lloq in model_lloq.model_code
+    assert y_below_lloq in model_lloq.model_code
+    assert 'LAPLACE' in model_lloq.model_code
+    assert 'DV.GE.LLOQ' in model_lloq.model_code
+    assert 'LLOQ = ' not in model_lloq.model_code
