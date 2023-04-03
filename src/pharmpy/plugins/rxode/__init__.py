437ad877fb0b14c551e7e2bda01721d03d9b2110
Johan Borg <johanwilhelmborg@gmail.com>
2023-04-03 09:25:02 +0200

    Outline structure of conversion

---
+++ src/pharmpy/plugins/rxode/__init__.py
@@ -0,0 +1,34 @@
+import pathlib
+
+import pharmpy.config as config
+
+#from .model import Model, convert_model, parse_modelfit_results
+
+r"""
+.. list-table:: Options for the nlmixr plugin
+   :widths: 25 25 50 150
+   :header-rows: 1
+
+   * - Option name
+     - Default value
+     - Type
+     - Description
+   * - ``rpath``
+     - ````
+     - str
+     - Path to R installation directory
+"""
+
+
+class RxODEConfiguration(config.Configuration):
+    module = 'pharmpy.plugins.nlmixr'
+    rpath = config.ConfigItem(
+        pathlib.Path(''),
+        'Path to R installation directory',
+        cls=pathlib.Path,
+    )
+
+
+conf = RxODEConfiguration()
+
+#__all__ = ('Model', 'convert_model', 'parse_modelfit_results')
