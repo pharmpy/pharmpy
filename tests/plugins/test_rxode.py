10df5658ab64beb27057270bd9500a732d1e1d63
Johan Borg <johanwilhelmborg@gmail.com>
2023-05-05 15:32:54 +0200

    fix init files, add test for rxode

--- tests/plugins/test_rxode.py
+++ tests/plugins/test_rxode.py
@@ -1,11 +1,13 @@
-from pharmpy.plugins.nlmixr import convert_model
+from pharmpy.plugins.rxode import convert_model
 
 
 def test_model(testdata, load_model_for_test):
     nmmodel = load_model_for_test(testdata / 'nonmem' / 'pheno.mod')
     model = convert_model(nmmodel)
-    assert 'ini' in model.model_code
-    assert 'model' in model.model_code
+    assert 'rxode2' in model.model_code
+    assert 'thetas' in model.model_code
+    assert 'omegas' in model.model_code
+    assert 'sigmas' in model.model_code
 
 
 def test_pheno_real(testdata, load_model_for_test):
@@ -17,26 +19,12 @@ def test_pheno_real(testdata, load_model_for_test):
 def test_pheno_error_model(testdata, load_model_for_test):
     mmodel = load_model_for_test(testdata / 'nonmem' / 'pheno_real.mod')
     model = convert_model(mmodel)
-    assert "add_error <- 0" in model.model_code
-    assert "prop_error <- SIGMA_1_1" in model.model_code
-    assert "Y ~ add(add_error) + prop(prop_error)" in model.model_code
-
-def test_prop_alias(testdata, load_model_for_test):
-    nmmodel = load_model_for_test(testdata / 'nonmem' / 'models' / 'mox2.mod')
-    model = convert_model(nmmodel)
-    assert "prop_error <- RUV_PROP" in model.model_code
-
-def test_remove_sigma(testdata, load_model_for_test):
-    nmmodel = load_model_for_test(testdata / 'nonmem' / 'models' / 'fviii6.mod')
-    model = convert_model(nmmodel)
-    assert "SIGMA_1_1" not in model.model_code
-
+    assert "Y <- F + SIGMA_1_1*W" in model.model_code
 
 def test_sigma(testdata, load_model_for_test):
     nmmodel = load_model_for_test(testdata / 'nonmem' / 'models' / 'mox2.mod')
     model = convert_model(nmmodel)
-    assert "RUV_PROP <- c(0.0, 0.1, Inf)" in model.model_code
-
+    assert "RUV_PROP ~ 0.1" in model.model_code
 
 def test_dataset_modifications(testdata, load_model_for_test):
     nmmodel = load_model_for_test(testdata / 'nonmem' / 'pheno.mod')
@@ -47,9 +35,7 @@ def test_dataset_modifications(testdata, load_model_for_test):
 def test_w_error_model(testdata, load_model_for_test):
     nmmodel = load_model_for_test(testdata / 'nonmem' / 'models' / 'fviii6.mod')
     model = convert_model(nmmodel)
-    assert 'add_error <- THETA_3' in model.model_code
-    assert 'prop_error <- THETA_4' in model.model_code
-    assert "Y ~ add(add_error) + prop(prop_error)" in model.model_code
+    assert 'W <- sqrt(IPRED**2*THETA_4**2 + THETA_3**2)' in model.model_code
 
 
 def test_omega_same(testdata, load_model_for_test):
