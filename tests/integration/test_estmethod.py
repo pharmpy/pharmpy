f02aed814f34ed779dabf05244e4251f5dff7fc5
Stella <stella.belin@farmaci.uu.se>
2023-05-25 14:04:30 +0200

    Use moxonidine instead of pheno in estmethod test

--- tests/integration/test_estmethod.py
+++ tests/integration/test_estmethod.py
@@ -1,34 +1,21 @@
-import shutil
-
 import pytest
 
 from pharmpy.internals.fs.cwd import chdir
-from pharmpy.model import Model
-from pharmpy.modeling import set_zero_order_elimination
 from pharmpy.tools import run_estmethod
 
 
 @pytest.mark.parametrize(
     'algorithm, methods, solvers, no_of_candidates, advan_ref',
     [
-        ('exhaustive', ['foce', 'laplace'], None, 2, 'ADVAN1'),
-        ('exhaustive_only_eval', ['foce', 'laplace'], None, 2, 'ADVAN1'),
+        ('exhaustive', ['foce', 'imp'], None, 2, 'ADVAN2'),
+        ('exhaustive_only_eval', ['foce', 'imp'], None, 2, 'ADVAN2'),
     ],
 )
 def test_estmethod(
-    tmp_path, model_count, testdata, algorithm, methods, solvers, no_of_candidates, advan_ref
+    tmp_path, start_model, model_count, testdata, algorithm, methods, solvers, no_of_candidates, advan_ref
 ):
     with chdir(tmp_path):
-        for path in (testdata / 'nonmem').glob('pheno_real.*'):
-            shutil.copy2(path, tmp_path)
-        shutil.copy2(testdata / 'nonmem' / 'pheno.dta', tmp_path)
-        model_start = Model.parse_model('pheno_real.mod')
-        model_start = model_start.replace(
-            datainfo=model_start.datainfo.replace(path=tmp_path / 'pheno.dta')
-        )
-        model_start = set_zero_order_elimination(model_start)
-
-        res = run_estmethod(algorithm, methods=methods, solvers=solvers, model=model_start)
+        res = run_estmethod(algorithm, methods=methods, solvers=solvers, model=start_model)
 
         assert len(res.summary_tool) == no_of_candidates
         assert len(res.models) == no_of_candidates
