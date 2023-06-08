b33f830b96244cb5937f7d4ab462d54b6689b777
Stella <stella.belin@farmaci.uu.se>
2023-06-08 10:08:38 +0200

    Move final test from test_modeling.py to test_parameter_variability.py, rename test_nested_update_source to test_nested_iiv_transformations and remove explicit update source

--- tests/modeling/test_parameter_variability.py
+++ tests/modeling/test_parameter_variability.py
@@ -1668,3 +1668,26 @@ def test_update_initial_individual_estimates(
 
         assert ('$ETAS FILE=run1_input.phi' in model.model_code) is file_exists
         assert (os.path.isfile('run1_input.phi')) is file_exists
+
+
+def test_nested_iiv_transformations(load_model_for_test, pheno_path):
+    model = load_model_for_test(pheno_path)
+    res = read_modelfit_results(pheno_path)
+
+    model = create_joint_distribution(model, individual_estimates=res.individual_estimates)
+
+    assert 'IIV_CL_IIV_V' in model.model_code
+
+    model = load_model_for_test(pheno_path)
+
+    model = remove_iiv(model, 'CL')
+
+    assert '0.031128' in model.model_code
+    assert '0.0309626' not in model.model_code
+
+    model = load_model_for_test(pheno_path)
+
+    model = remove_iiv(model, 'V')
+
+    assert '0.0309626' in model.model_code
+    assert '0.031128' not in model.model_code
