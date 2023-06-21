83d1d901f2eb1688e33d178fa5fd2eccb3778606
Antonia Hufnagl <antonia.hufnagl@farmaci.uu.se>
2023-06-21 09:46:50 +0200

    Add effect compartment model

--- src/pharmpy/modeling/pd.py
+++ src/pharmpy/modeling/pd.py
@@ -2,10 +2,74 @@
 :meta private:
 """
 from pharmpy.deps import sympy
-from pharmpy.model import Assignment, Model, output
+from pharmpy.model import (
+    Assignment,
+    Compartment,
+    CompartmentalSystem,
+    CompartmentalSystemBuilder,
+    Model,
+    Statements,
+    output,
+)
 
 from .error import set_proportional_error_model
 from .odes import add_individual_parameter, set_initial_estimates
+
+
+def add_effect_compartment(model: Model, expr: str):
+    """Add an effect compartment
+
+    Parameters
+    ----------
+    model : Model
+        Pharmpy model
+    expr : str
+        Name of PD effect function. The function can either be
+        * baseline: E = E0
+        * step effect: Emax = theta if C > 0 else 0, E = E0 + Emax
+        * linear: E = E0 + S * C
+        * Emax: E = E0 + Emax * C / (EC50 + C)
+        * sigmoidal: E = Emax * C^n / (EC50^n + C^n)
+        * log-lin: E = m * log(C + C0)
+        Valid strings are: baseline, linear, Emax, sigmoid, step, loglin
+
+    Return
+    ------
+    Model
+        Pharmpy model object
+
+    Examples
+    --------
+    >>> from pharmpy.modeling import load_example_model
+    >>> model = load_example_model("pheno")
+    >>> model = add_effect_compartment(model, "linear")
+    >>> model.statements.ode_system.find_compartment("EFFECT")
+    Compartment(EFFECT, amount=A_EFFECT, input=KE0*A_CENTRAL(t))
+    """
+    vc, cl = _get_central_volume_and_cl(model)
+
+    odes = model.statements.ode_system
+    central = odes.central_compartment
+    central_amount = sympy.Function(central.amount.name)(sympy.Symbol('t'))
+    cb = CompartmentalSystemBuilder(odes)
+
+    ke0 = sympy.Symbol("KE0")
+    model = add_individual_parameter(model, ke0.name)
+
+    effect = Compartment.create("EFFECT", input=ke0 * central_amount)
+    cb.add_compartment(effect)
+    cb.add_flow(effect, output, ke0)
+
+    model = model.replace(
+        statements=Statements(
+            model.statements.before_odes + CompartmentalSystem(cb) + model.statements.after_odes
+        )
+    )
+
+    conc_e = model.statements.ode_system.find_compartment("EFFECT").amount / vc
+
+    model = _add_effect(model, expr, conc_e)
+    return model
 
 
 def set_direct_effect(model: Model, expr: str):
@@ -34,6 +98,29 @@ def set_direct_effect(model: Model, expr: str):
     --------
 
     """
+    # vc = sympy.Symbol("VC") # must be changed later
+    vc, cl = _get_central_volume_and_cl(model)
+    conc = model.statements.ode_system.central_compartment.amount / vc
+
+    model = _add_effect(model, expr, conc)
+
+    return model
+
+
+def _get_central_volume_and_cl(model):
+    odes = model.statements.ode_system
+    central_comp = odes.central_compartment
+    elimination_rate = odes.get_flow(central_comp, output)
+    numer, denom = elimination_rate.as_numer_denom()
+    if denom != 1:
+        vc = denom
+        cl = numer
+    else:
+        raise ValueError('Model is not suitable')
+    return vc, cl
+
+
+def _add_effect(model: Model, expr: str, conc):
     e0 = sympy.Symbol("E0")
     model = add_individual_parameter(model, e0.name)
     if expr in ["Emax", "sigmoid", "step"]:
@@ -42,10 +129,6 @@ def set_direct_effect(model: Model, expr: str):
     if expr in ["Emax", "sigmoid"]:
         ec50 = sympy.Symbol("EC_50")
         model = add_individual_parameter(model, ec50.name)
-
-    # vc = sympy.Symbol("VC") # must be changed later
-    vc, cl = _get_central_volume_and_cl(model)
-    conc = model.statements.ode_system.central_compartment.amount / vc
 
     # Add effect E
     if expr == "baseline":
@@ -59,7 +142,7 @@ def set_direct_effect(model: Model, expr: str):
     elif expr == "step":
         E = Assignment(sympy.Symbol("E"), sympy.Piecewise((0, conc < 0), (emax, True)))
     elif expr == "sigmoid":
-        n = sympy.Symbol("n")
+        n = sympy.Symbol("n")  # Hill coefficient
         model = add_individual_parameter(model, n.name)
         model = set_initial_estimates(model, {"POP_n": 1})
         E = Assignment(sympy.Symbol("E"), emax * conc**n / (ec50**n + conc**n))
@@ -79,16 +162,3 @@ def set_direct_effect(model: Model, expr: str):
     # Add error model
     model = set_proportional_error_model(model, dv=2, zero_protection=False)
     return model
-
-
-def _get_central_volume_and_cl(model):
-    odes = model.statements.ode_system
-    central_comp = odes.central_compartment
-    elimination_rate = odes.get_flow(central_comp, output)
-    numer, denom = elimination_rate.as_numer_denom()
-    if denom != 1:
-        vc = denom
-        cl = numer
-    else:
-        raise ValueError('Model is not suitable')
-    return vc, cl
--- tests/modeling/test_pd.py
+++ tests/modeling/test_pd.py
@@ -1,6 +1,12 @@
 from pharmpy.deps import sympy
-from pharmpy.model import Assignment
-from pharmpy.modeling import set_direct_effect
+from pharmpy.model import (
+    Assignment,
+    Compartment,
+    CompartmentalSystem,
+    CompartmentalSystemBuilder,
+    output,
+)
+from pharmpy.modeling import add_effect_compartment, set_direct_effect
 
 
 def S(x):
@@ -10,53 +16,81 @@ def S(x):
 def test_set_direct_effect(load_model_for_test, testdata):
     model = load_model_for_test(testdata / "nonmem" / "pheno_pd.mod")
     conc = model.statements.ode_system.central_compartment.amount / S("V")
+    _test_effect_models(set_direct_effect(model, "baseline"), "baseline", conc)
+    _test_effect_models(set_direct_effect(model, "linear"), "linear", conc)
+    _test_effect_models(set_direct_effect(model, "Emax"), "Emax", conc)
+    _test_effect_models(set_direct_effect(model, "sigmoid"), "sigmoid", conc)
+    _test_effect_models(set_direct_effect(model, "step"), "step", conc)
+    _test_effect_models(set_direct_effect(model, "loglin"), "loglin", conc)
 
+
+def test_add_effect_compartment(load_model_for_test, testdata):
+    model = load_model_for_test(testdata / "nonmem" / "pheno_pd.mod")
+    conc_e = S("A_EFFECT") / S("V")
+    ke0 = S("KE0")
+    central_amount = sympy.Function("A_CENTRAL")(S('t'))
+    comp_e = Compartment.create("EFFECT", input=ke0 * central_amount)
+
+    model1 = add_effect_compartment(model, "baseline")
+    compartments = CompartmentalSystemBuilder(model1.statements.ode_system)
+    odes = model1.statements.ode_system
+    assert odes.find_compartment("EFFECT") == comp_e
+    assert odes.zero_order_inputs[0] == 0
+    assert odes.zero_order_inputs[1] == ke0 * central_amount
+    assert odes.get_compartment_outflows("EFFECT")[0][0] == output
+    assert odes.get_compartment_outflows("EFFECT")[0][1] == ke0
+    assert CompartmentalSystem(compartments).compartment_names == ['CENTRAL', 'EFFECT']
+
+    _test_effect_models(add_effect_compartment(model, "baseline"), "baseline", conc_e)
+    _test_effect_models(add_effect_compartment(model, "linear"), "linear", conc_e)
+    _test_effect_models(add_effect_compartment(model, "Emax"), "Emax", conc_e)
+    _test_effect_models(add_effect_compartment(model, "sigmoid"), "sigmoid", conc_e)
+    _test_effect_models(add_effect_compartment(model, "step"), "step", conc_e)
+    _test_effect_models(add_effect_compartment(model, "loglin"), "loglin", conc_e)
+
+
+def _test_effect_models(model, expr, conc):
     e = S("E")
     e0 = S("E0")
     emax = S("E_max")
     ec50 = S("EC_50")
 
-    model1 = set_direct_effect(model, 'baseline')
-    assert model1.statements[0] == Assignment(e0, S("POP_E0"))
-    assert model1.statements.after_odes[-2] == Assignment(e, e0)
-    assert model1.statements.after_odes[-1] == Assignment(S("Y_2"), e + e * S("epsilon_p"))
-
-    model2 = set_direct_effect(model, 'linear')
-    assert model2.statements[1] == Assignment(e0, S("POP_E0"))
-    assert model2.statements[0] == Assignment(S("S"), S("POP_S"))
-    assert model2.statements.after_odes[-2] == Assignment(e, e0 + S("S") * conc)
-    assert model2.statements.after_odes[-1] == Assignment(S("Y_2"), e + e * S("epsilon_p"))
-
-    model3 = set_direct_effect(model, "Emax")
-    assert model3.statements[0] == Assignment(ec50, S("POP_EC_50"))
-    assert model3.statements[2] == Assignment(e0, S("POP_E0"))
-    assert model3.statements[1] == Assignment(emax, S("POP_E_max"))
-    assert model3.statements.after_odes[-2] == Assignment(e, e0 + (emax * conc) / (ec50 + conc))
-    assert model3.statements.after_odes[-1] == Assignment(S("Y_2"), e + e * S("epsilon_p"))
-
-    model4 = set_direct_effect(model, "sigmoid")
-    assert model4.statements[0] == Assignment(S("n"), S("POP_n"))
-    assert model4.statements[1] == Assignment(ec50, S("POP_EC_50"))
-    assert model4.statements[3] == Assignment(e0, S("POP_E0"))
-    assert model4.statements[2] == Assignment(emax, S("POP_E_max"))
-    assert model4.statements.after_odes[-2] == Assignment(
-        e, (emax * conc ** S("n")) / (ec50 ** S("n") + conc ** S("n"))
-    )
-    assert model4.statements.after_odes[-1] == Assignment(S("Y_2"), e + e * S("epsilon_p"))
-    assert model4.parameters["POP_n"].init == 1
-
-    model5 = set_direct_effect(model, "step")
-    assert model5.statements[1] == Assignment(e0, S("POP_E0"))
-    assert model5.statements[0] == Assignment(emax, S("POP_E_max"))
-    assert model5.statements.after_odes[-2] == Assignment(
-        e, sympy.Piecewise((0, conc < 0), (emax, True))
-    )
-    assert model5.statements.after_odes[-1] == Assignment(S("Y_2"), e + e * S("epsilon_p"))
-
-    model6 = set_direct_effect(model, "loglin")
-    assert model6.statements[1] == Assignment(e0, S("POP_E0"))
-    assert model6.statements[0] == Assignment(S("m"), S("POP_m"))
-    assert model6.statements.after_odes[-2] == Assignment(
-        e, S("m") * sympy.log(conc + sympy.exp(e0 / S("m")))
-    )
-    assert model6.statements.after_odes[-1] == Assignment(S("Y_2"), e + e * S("epsilon_p"))
+    if expr == 'baseline':
+        assert model.statements[0] == Assignment(e0, S("POP_E0"))
+        assert model.statements.after_odes[-2] == Assignment(e, e0)
+        assert model.statements.after_odes[-1] == Assignment(S("Y_2"), e + e * S("epsilon_p"))
+    elif expr == 'linear':
+        assert model.statements[1] == Assignment(e0, S("POP_E0"))
+        assert model.statements[0] == Assignment(S("S"), S("POP_S"))
+        assert model.statements.after_odes[-2] == Assignment(e, e0 + S("S") * conc)
+        assert model.statements.after_odes[-1] == Assignment(S("Y_2"), e + e * S("epsilon_p"))
+    elif expr == "Emax":
+        assert model.statements[0] == Assignment(ec50, S("POP_EC_50"))
+        assert model.statements[2] == Assignment(e0, S("POP_E0"))
+        assert model.statements[1] == Assignment(emax, S("POP_E_max"))
+        assert model.statements.after_odes[-2] == Assignment(e, e0 + (emax * conc) / (ec50 + conc))
+        assert model.statements.after_odes[-1] == Assignment(S("Y_2"), e + e * S("epsilon_p"))
+    elif expr == "sigmoid":
+        assert model.statements[0] == Assignment(S("n"), S("POP_n"))
+        assert model.statements[1] == Assignment(ec50, S("POP_EC_50"))
+        assert model.statements[3] == Assignment(e0, S("POP_E0"))
+        assert model.statements[2] == Assignment(emax, S("POP_E_max"))
+        assert model.statements.after_odes[-2] == Assignment(
+            e, (emax * conc ** S("n")) / (ec50 ** S("n") + conc ** S("n"))
+        )
+        assert model.statements.after_odes[-1] == Assignment(S("Y_2"), e + e * S("epsilon_p"))
+        assert model.parameters["POP_n"].init == 1
+    elif expr == "step":
+        assert model.statements[1] == Assignment(e0, S("POP_E0"))
+        assert model.statements[0] == Assignment(emax, S("POP_E_max"))
+        assert model.statements.after_odes[-2] == Assignment(
+            e, sympy.Piecewise((0, conc < 0), (emax, True))
+        )
+        assert model.statements.after_odes[-1] == Assignment(S("Y_2"), e + e * S("epsilon_p"))
+    elif expr == "loglin":
+        assert model.statements[1] == Assignment(e0, S("POP_E0"))
+        assert model.statements[0] == Assignment(S("m"), S("POP_m"))
+        assert model.statements.after_odes[-2] == Assignment(
+            e, S("m") * sympy.log(conc + sympy.exp(e0 / S("m")))
+        )
+        assert model.statements.after_odes[-1] == Assignment(S("Y_2"), e + e * S("epsilon_p"))
