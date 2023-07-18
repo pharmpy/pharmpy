d357b29e61cb603ac63798511e133e412e2d49dd
Stella <stella.belin@farmaci.uu.se>
2023-07-18 15:12:26 +0200

    Move some integration tests for estmethod to unit tests

--- tests/integration/test_estmethod.py
+++ tests/integration/test_estmethod.py
@@ -5,12 +5,11 @@ from pharmpy.tools import run_estmethod
 
 
 @pytest.mark.parametrize(
-    'algorithm, methods, solvers, covs, no_of_candidates, advan_ref',
+    'algorithm, methods, covs, no_of_candidates, advan_ref',
     [
-        ('exhaustive', ['foce', 'imp'], None, None, 2, 'ADVAN2'),
-        ('exhaustive_only_eval', ['foce', 'imp'], None, None, 2, 'ADVAN2'),
-        ('exhaustive', ['foce'], None, ['sandwich', 'cpg'], 2, 'ADVAN2'),
-        ('exhaustive', ['foce', 'imp'], None, ['sandwich', 'cpg'], 4, 'ADVAN2'),
+        ('exhaustive', ['foce', 'imp'], None, 2, 'ADVAN2'),
+        ('exhaustive_only_eval', ['foce', 'imp'], None, 2, 'ADVAN2'),
+        ('exhaustive', ['foce'], ['sandwich', 'cpg'], 2, 'ADVAN2'),
     ],
 )
 def test_estmethod(
@@ -20,15 +19,12 @@ def test_estmethod(
     testdata,
     algorithm,
     methods,
-    solvers,
     covs,
     no_of_candidates,
     advan_ref,
 ):
     with chdir(tmp_path):
-        res = run_estmethod(
-            algorithm, methods=methods, solvers=solvers, covs=covs, model=start_model
-        )
+        res = run_estmethod(algorithm, methods=methods, covs=covs, model=start_model)
 
         assert len(res.summary_tool) == no_of_candidates
         assert len(res.models) == no_of_candidates
--- tests/tools/test_estmethod.py
+++ tests/tools/test_estmethod.py
@@ -5,21 +5,23 @@ from pharmpy.tools.estmethod.tool import SOLVERS, create_workflow, validate_inpu
 
 
 @pytest.mark.parametrize(
-    'algorithm, methods, solvers, no_of_models',
+    'algorithm, methods, solvers, covs, no_of_models',
     [
-        ('exhaustive', ['foce'], None, 1),
-        ('exhaustive', ['foce', 'laplace'], None, 2),
-        ('exhaustive', ['foce', 'imp'], ['lsoda'], 2),
-        ('exhaustive', ['foce'], 'all', len(SOLVERS)),
-        ('exhaustive_with_update', ['foce'], None, 2),
-        ('exhaustive_with_update', ['foce', 'laplace'], None, 4),
-        ('exhaustive_with_update', ['laplace'], None, 3),
-        ('exhaustive_with_update', ['foce'], ['lsoda'], 3),
-        ('exhaustive_with_update', ['foce'], 'all', len(SOLVERS) * 2 + 1),
+        ('exhaustive', ['foce'], None, None, 1),
+        ('exhaustive', ['foce', 'laplace'], None, None, 2),
+        ('exhaustive', ['foce', 'imp'], ['lsoda'], None, 2),
+        ('exhaustive', ['foce'], 'all', None, len(SOLVERS)),
+        ('exhaustive_with_update', ['foce'], None, None, 2),
+        ('exhaustive_with_update', ['foce', 'laplace'], None, None, 4),
+        ('exhaustive_with_update', ['laplace'], None, None, 3),
+        ('exhaustive_with_update', ['foce'], ['lsoda'], None, 3),
+        ('exhaustive_with_update', ['foce'], 'all', None, len(SOLVERS) * 2 + 1),
+        ('exhaustive', ['foce'], None, ['sandwich', 'cpg'], 2),
+        ('exhaustive', ['foce', 'imp'], None, ['sandwich', 'cpg'], 4),
     ],
 )
-def test_algorithm(algorithm, methods, solvers, no_of_models):
-    wf = create_workflow(algorithm, methods=methods, solvers=solvers)
+def test_algorithm(algorithm, methods, solvers, covs, no_of_models):
+    wf = create_workflow(algorithm, methods=methods, solvers=solvers, covs=covs)
     fit_tasks = [task.name for task in wf.tasks if task.name.startswith('run')]
 
     assert len(fit_tasks) == no_of_models
