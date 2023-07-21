75cfb86cf5454dc469e3647a8136a36c8511b42c
Antonia Hufnagl <antonia.hufnagl@farmaci.uu.se>
2023-07-21 15:17:10 +0200

    Fix issue #1879 create_basic_pk_model

--- tests/modeling/test_basic_models.py
+++ tests/modeling/test_basic_models.py
@@ -1,3 +1,4 @@
+from pharmpy.internals.immutable import frozenmapping
 from pharmpy.modeling import create_basic_pk_model
 
 
@@ -8,3 +9,5 @@ def test_create_basic_pk_model():
     model = create_basic_pk_model('oral')
     print(model.parameters)
     assert len(model.parameters) == 8
+
+    assert type(model.dependent_variables) == frozenmapping
