c3bcdb746a574dcd7a35d6da91a7ab9385665eb7
Antonia Hufnagl <antonia.hufnagl@farmaci.uu.se>
2023-08-08 15:52:03 +0200

    Allow spaces as seperator in create_basic_pk_model

--- tests/modeling/test_basic_models.py
+++ tests/modeling/test_basic_models.py
@@ -7,7 +7,6 @@ def test_create_basic_pk_model(testdata):
     assert len(model.parameters) == 6
 
     model = create_basic_pk_model('oral')
-    # print(model.parameters)
     assert len(model.parameters) == 8
 
     dataset_path = testdata / 'nonmem/pheno.dta'
@@ -18,10 +17,11 @@ def test_create_basic_pk_model(testdata):
     run1 = set_zero_order_absorption(model)
     assert not model.dataset.empty
     assert not run1.dataset.empty
-    assert type(model.dependent_variables) == frozenmapping
+    assert isinstance(model.dependent_variables, frozenmapping)
 
     dataset_path = testdata / 'nonmem/pheno_pd.csv'
     model = create_basic_pk_model(
         modeltype='oral', dataset_path=dataset_path, cl_init=0.01, vc_init=1.0, mat_init=0.1
     )
     assert not model.dataset.empty
+    assert model.datainfo._separator == r'\s+'
