8cb3967089cfc2bdc8db52c7afe71abe686dcdf1
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2023-09-29 12:58:34 +0200

    Remove unused attributes in log entries

--- src/pharmpy/workflows/log.py
+++ src/pharmpy/workflows/log.py
@@ -1,6 +1,5 @@
 import datetime
 from dataclasses import dataclass
-from pathlib import Path
 from typing import Optional
 
 from pharmpy.deps import pandas as pd
@@ -10,10 +9,6 @@ from pharmpy.deps import pandas as pd
 class LogEntry:
     category: Optional[str] = None
     time: datetime.datetime = datetime.datetime.now()
-    model: str = ""
-    tool: str = ""
-    task: str = ""
-    path: Optional[Path] = None
     message: str = ""
 
     def to_dict(self):
@@ -80,19 +75,11 @@ class Log:
         """
         categories = [entry.category for entry in self._log]
         times = [entry.time for entry in self._log]
-        models = [entry.model for entry in self._log]
-        tools = [entry.tool for entry in self._log]
-        tasks = [entry.task for entry in self._log]
-        paths = [entry.path for entry in self._log]
         messages = [entry.message for entry in self._log]
         df = pd.DataFrame(
             {
                 'category': categories,
                 'time': times,
-                'model': models,
-                'tool': tools,
-                'task': tasks,
-                'path': paths,
                 'message': messages,
             }
         )
