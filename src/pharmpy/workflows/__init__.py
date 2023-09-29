d52e1623948c28286d64546e7906c330ca6d4508
Stella <stella.belin@farmaci.uu.se>
2023-09-29 13:29:49 +0200

    Add ModelfitResults to __all__ in workflows/__init__.py

--- src/pharmpy/workflows/__init__.py
+++ src/pharmpy/workflows/__init__.py
@@ -37,7 +37,7 @@ from .model_database import (
     ModelDatabase,
     NullModelDatabase,
 )
-from .results import Results
+from .results import ModelfitResults, Results
 from .task import Task
 from .tool_database import LocalDirectoryToolDatabase, NullToolDatabase, ToolDatabase
 from .workflow import Workflow, WorkflowBuilder
@@ -89,6 +89,7 @@ __all__ = [
     'NullModelDatabase',
     'NullToolDatabase',
     'ModelDatabase',
+    'ModelfitResults',
     'Results',
     'Task',
     'ToolDatabase',
