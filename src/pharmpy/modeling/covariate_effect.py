8d7055ae266acfec7bf7cf0fa425216830260eb6
Johan Borg <johanwilhelmborg@gmail.com>
2023-11-03 10:41:09 +0100

    Avoid raising error when trying to remove covariate effect if non-existent

--- src/pharmpy/modeling/covariate_effect.py
+++ src/pharmpy/modeling/covariate_effect.py
@@ -183,6 +183,8 @@ def remove_covariate_effect(model: Model, parameter: str, covariate: str):
     False
 
     """
+    if not has_covariate_effect(model, parameter, covariate):
+        return model
     kept_thetas, before_odes = simplify_model(
         model,
         model.statements.before_odes,
