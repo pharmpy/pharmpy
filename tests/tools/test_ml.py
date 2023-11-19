e42759ba88c4b6b1f3e3689a40dee019dea16e66
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2023-11-19 21:40:44 +0100

    Support tflite for Python 3.10 on Windows

--- tests/tools/test_ml.py
+++ tests/tools/test_ml.py
@@ -10,7 +10,7 @@ from pharmpy.tools import (
 )
 
 tflite_condition = (
-    sys.version_info >= (3, 10)
+    sys.version_info >= (3, 11)
     and sys.platform == 'win32'
     or sys.version_info >= (3, 12)
     and sys.platform == 'darwin'
