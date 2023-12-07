d31f0f0c9aab6a8a1756079202a89dd9bb059a44
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2023-12-07 17:01:27 +0100

    Bump version: 0.105.2 → 0.105.3

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def longdesc(text_str):
 
 setup(
     name='pharmpy-core',
-    version='0.105.2',
+    version='0.105.3',
     license='GNU Lesser General Public License v3 (LGPLv3)',
     description='Pharmacometric modeling',
     long_description='%s\n\n%s'
--- src/pharmpy/__init__.py
+++ src/pharmpy/__init__.py
@@ -9,7 +9,7 @@ Definitions
 ===========
 """
 
-__version__ = '0.105.2'
+__version__ = '0.105.3'
 
 import logging
 
