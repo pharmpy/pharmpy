1a64f2c68eeac7adf9ef47ccfc754a1862a929dd
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2024-01-18 10:24:58 +0100

    Use symengine internally in Expr

--- src/pharmpy/basic/expr.py
+++ src/pharmpy/basic/expr.py
@@ -1,5 +1,4 @@
-from pharmpy.deps import sympy
-from pharmpy.internals.expr.parse import parse
+from pharmpy.deps import symengine, sympy
 
 
 class Expr:
@@ -8,16 +7,12 @@ class Expr:
     def __init__(self, source):
         if isinstance(source, Expr):
             self._expr = source._expr
-        elif isinstance(source, sympy.Expr):
-            self._expr = source
-        elif isinstance(source, str):
-            self._expr = parse(source)
         else:
-            raise ValueError(f"Cannot create Expr from {type(source)}")
+            self._expr = symengine.sympify(source)
 
     @property
     def name(self):
-        if isinstance(self._expr, sympy.Symbol):
+        if isinstance(self._expr, symengine.Symbol):
             return self._expr.name
         else:
             raise ValueError("Expression has no name")
@@ -56,28 +51,23 @@ class Expr:
         return Expr(-self._expr)
 
     def __pos__(self):
-        return Expr(+self._expr)
+        return Expr(self._expr)
 
     def _sympy_(self):
-        return self._expr
+        return sympy.sympify(self._expr)
 
     def __eq__(self, other):
-        if isinstance(other, sympy.Basic):
-            return self._expr == other
-        elif isinstance(other, Expr):
-            return self._expr == other._expr
-        else:
-            return False
+        return self._expr == other
 
     def __hash__(self):
         return hash(self._expr)
 
     @classmethod
     def symbol(cls, name):
-        symb = sympy.Symbol(name)
+        symb = symengine.Symbol(name)
         return cls(symb)
 
     @classmethod
     def integer(cls, value):
-        n = sympy.Integer(value)
+        n = symengine.Integer(value)
         return cls(n)
