307dd48fbc6494fc19411ac274eeab9528b5b317
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2024-02-23 14:33:47 +0100

    Add classes for hashing model and dataset in workflows

---
+++ src/pharmpy/workflows/hashing.py
@@ -0,0 +1,61 @@
+import base64
+import hashlib
+import json
+
+from pharmpy.deps import numpy as np
+from pharmpy.modeling import load_dataset
+
+
+def _encode(obj):
+    # Encode a model subobject into a bytes string
+    d = obj.to_dict()
+    js = json.dumps(d)
+    enc = js.encode('utf-8')
+    return enc
+
+
+def _update_hash_with_dataset(df, h):
+    values = df.values
+    if not values.flags['C_CONTIGUOUS']:
+        values = np.ascontiguousarray(values)
+    columns = repr(df.columns).encode('utf-8')
+    index = repr(df.index).encode('utf-8')
+    dtypes = repr(list(df.dtypes)).encode('utf-8')
+    h.update(values)
+    h.update(columns)
+    h.update(index)
+    h.update(dtypes)
+
+
+def _hash_to_string(h):
+    digest = h.digest()
+    b64 = base64.urlsafe_b64encode(digest).decode()
+    return b64.replace('=', '')  # Remove padding characters
+
+
+class Hash:
+    def __str__(self):
+        return self._hash
+
+
+class DatasetHash(Hash):
+    def __init__(self, df):
+        h = hashlib.sha256()
+        _update_hash_with_dataset(df, h)
+        self._hash = _hash_to_string(h)
+
+
+class ModelHash(Hash):
+    def __init__(self, model):
+        di = model.datainfo
+        if di is not None:
+            if model.dataset is None:
+                model = load_dataset(model)
+            di = di.replace(path=None)
+            model.replace(datainfo=di)
+        model_bytes = _encode(model)
+        h = hashlib.sha256()
+        _update_hash_with_dataset(model.dataset, h)
+        self.dataset_hash = _hash_to_string(h)
+        h.update(model_bytes)
+        self._hash = _hash_to_string(h)
