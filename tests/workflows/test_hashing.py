c648cef1f725f3a7e24fb8c87c78073645b5c5c3
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2024-04-25 09:11:22 +0200

    Add tests for changing name, description and inits when hashing

--- tests/workflows/test_hashing.py
+++ tests/workflows/test_hashing.py
@@ -1,3 +1,4 @@
+from pharmpy.modeling import set_initial_estimates
 from pharmpy.workflows.hashing import DatasetHash, ModelHash
 
 
@@ -7,3 +8,15 @@ def test_hash(load_example_model_for_test):
     assert str(h) == "Q1IeIf2PVAiya2ghqLshv7JSRZD-CG_Q2T_FMPvD6Zk"
     d = DatasetHash(model.dataset)
     assert str(d) == h.dataset_hash
+
+    # name should not change hash
+    m2 = model.replace(name="SomeOtherName")
+    assert str(h) == str(ModelHash(m2))
+
+    # description should not change hash
+    m3 = model.replace(description="MyNewDescr")
+    assert str(h) == str(ModelHash(m3))
+
+    # changing init of a parameter should change hash
+    m4 = set_initial_estimates(model, {'IVV': 0.99})
+    assert str(h) != str(ModelHash(m4))
