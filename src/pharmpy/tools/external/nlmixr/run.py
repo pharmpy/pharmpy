a768a997a16892a68d73488641b59a63498f8c70
Johan Borg <johanwilhelmborg@gmail.com>
2024-05-21 13:15:15 +0200

    Remove comment

--- src/pharmpy/tools/external/nlmixr/run.py
+++ src/pharmpy/tools/external/nlmixr/run.py
@@ -268,12 +268,9 @@ def verification(
     meta = path / '.pharmpy'
     meta.mkdir(parents=True, exist_ok=True)
     write_fix_eta(nonmem_res, path=path)
-    # try:
     # FIXME : use fit() instead and incorporate write_fix_eta
     nlmixr_model_entry = ModelEntry.create(nlmixr_model, modelfit_results=None)
     nlmixr_model_entry = execute_model(nlmixr_model_entry, db, path=path)
-    # except Exception:
-    #     raise Exception("nlmixr2 model could not be fitted")
 
     # Combine the two based on ID and time
     if not ignore_print:
