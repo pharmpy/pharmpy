fd72f4b4cf24862a927744024445ae72ffd5c1d5
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2024-07-17 15:09:26 +0200

    Add type checking of modeling.compartments

--- src/pharmpy/modeling/compartments.py
+++ src/pharmpy/modeling/compartments.py
@@ -14,12 +14,13 @@ def get_lag_times(model: Model):
     dict
         Dictionary from compartment name to lag time expression
     """
-    odes = model.statements.ode_system
-    assert odes is not None
-    names = odes.compartment_names
     d = {}
+    odes = model.statements.ode_system
+    if odes is None:
+        return d
+    names = odes.compartment_names
     for name in names:
-        cmt = odes.find_compartment(name)
+        cmt = odes.find_compartment_or_raise(name)
         if cmt.lag_time != 0:
             d[name] = cmt.lag_time
     return d
@@ -38,12 +39,13 @@ def get_bioavailability(model: Model):
     dict
         Dictionary from compartment name to bioavailability expression
     """
-    odes = model.statements.ode_system
-    assert odes is not None
-    names = odes.compartment_names
     d = {}
+    odes = model.statements.ode_system
+    if odes is None:
+        return d
+    names = odes.compartment_names
     for name in names:
-        cmt = odes.find_compartment(name)
+        cmt = odes.find_compartment_or_raise(name)
         if cmt.bioavailability != 1:
             d[name] = cmt.bioavailability
     return d
