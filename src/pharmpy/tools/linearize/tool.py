4fefb74ecc5f90f4d49392d634b378b3199343f7
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2024-08-26 11:11:16 +0200

    Abort linearization if derivatives model failed completely

--- src/pharmpy/tools/linearize/tool.py
+++ src/pharmpy/tools/linearize/tool.py
@@ -12,7 +12,7 @@ from pharmpy.modeling import (
 )
 from pharmpy.modeling.estimation_steps import add_derivative
 from pharmpy.tools.modelfit import create_fit_workflow
-from pharmpy.workflows import ModelEntry, Task, Workflow, WorkflowBuilder
+from pharmpy.workflows import ModelEntry, Task, Workflow, WorkflowBuilder, abort_workflow
 from pharmpy.workflows.results import ModelfitResults
 
 from .results import calculate_results
@@ -126,6 +126,8 @@ def create_derivative_model(context, modelentry):
 
 
 def _create_linearized_model(context, model_name, description, model, derivative_model_entry):
+    if derivative_model_entry.modelfit_results is None:
+        abort_workflow(context, "Error while running the derivative model")
     new_input_file = cleanup_columns(derivative_model_entry)
 
     linbase = Model.create(
