cebfe2ec2049b20b5112725cc9f0bf33b56ae9f0
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2024-09-05 11:22:06 +0200

    Do not scatter dicts
    Doing this can result in a change in arguments to the
    function on the server side.

--- src/pharmpy/workflows/optimize.py
+++ src/pharmpy/workflows/optimize.py
@@ -7,7 +7,8 @@ def optimize_task_graph_for_dask_distributed(client, graph):
     optimized = {key: _scatter_computation(Future, client, value) for key, value in graph.items()}
     from dask.optimization import fuse
 
-    return fuse(optimized)[0]
+    fused = fuse(optimized)[0]
+    return fused
 
 
 def _scatter_computation(Future, client, computation):
@@ -35,7 +36,7 @@ def _scatter_computation(Future, client, computation):
 def _scatter_value(Future, client, value):
     # TODO: We could automatically compute whether object size is above
     # threshold with a slight twist on https://stackoverflow.com/a/30316760
-    if isinstance(value, (int, str, float, bool, range, Future)) or callable(value):
+    if isinstance(value, (dict, int, str, float, bool, range, Future)) or callable(value):
         return value
     else:
         dask_version = tuple(int(i) for i in dask.__version__.split('.'))
