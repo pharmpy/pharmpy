57d361f2577c8e7ed9a28b46dc25c9650e9cb735
Rikard Nordgren <rikard.nordgren@uu.se>
2025-02-19 09:26:08 +0100

    Update mixture plot with ranking based uncertainty

--- src/pharmpy/tools/vpc/mixture_plot.py
+++ src/pharmpy/tools/vpc/mixture_plot.py
@@ -1,5 +1,4 @@
 from pharmpy.deps import altair as alt
-from pharmpy.deps import numpy as np
 from pharmpy.deps import pandas as pd
 from pharmpy.model.external.nonmem.table import NONMEMTableFile
 
@@ -11,8 +10,12 @@ def plot_mixture_ids(orig_phm_path, sim_phm_path):
     tab = NONMEMTableFile(orig_phm_path)
 
     df = tab.tables[0].data_frame
-    df = df.set_index(['ID', 'SUBPOP'])
-    pmix = df[['PMIX']]
+    pmix = df[['ID', 'SUBPOP', 'PMIX']]
+    pmix = pmix.sort_values(["SUBPOP", "PMIX"])
+    subpops = pmix['SUBPOP'].unique()
+    ids = pmix['ID'].unique()
+    pmix = pmix.assign(rank=list(range(1, len(ids) + 1)) * len(subpops))
+    pmix = pmix.set_index(["SUBPOP", "rank"])
 
     simtab = NONMEMTableFile(sim_phm_path)
 
@@ -20,9 +23,13 @@ def plot_mixture_ids(orig_phm_path, sim_phm_path):
     for i, tab in enumerate(simtab.tables):
         df = simtab.tables[i].data_frame
         df = df[['ID', 'SUBPOP', 'PMIX']]
+        df = df.assign(sim=i)
         full = pd.concat((full, df))
 
-    minmax = full.groupby(['ID', 'SUBPOP']).agg(
+    full = full.sort_values(["sim", "SUBPOP", "PMIX"])
+    full = full.assign(rank=list(range(1, len(ids) + 1)) * len(subpops) * len(simtab.tables))
+
+    minmax = full.groupby(['SUBPOP', 'rank']).agg(
         q5=("PMIX", lambda x: x.quantile(0.05)), q95=("PMIX", lambda x: x.quantile(0.95))
     )
 
@@ -30,15 +37,14 @@ def plot_mixture_ids(orig_phm_path, sim_phm_path):
 
     facet = alt.hconcat(title="IPmix with uncertainty 5-95% CI")
 
-    for i in data.index.unique(level="SUBPOP"):
-        idvals = data.loc[:, i, :].sort_values(by="PMIX").reset_index()
-        idvals["SCALE"] = np.linspace(0, 100, len(idvals))
+    for i in subpops:
+        df = data.loc[i].reset_index()
 
         chart = (
-            alt.Chart(idvals)
+            alt.Chart(df)
             .mark_text()
             .encode(
-                x=alt.X("SCALE:Q", axis=alt.Axis(labels=False, ticks=False), title="ID"),
+                x=alt.X("rank:Q", axis=alt.Axis(labels=False, ticks=False), title="ID"),
                 y=alt.Y("PMIX:Q", title=f"IPmix({i})").scale(domain=[0.0, 1.0]),
                 text="ID:N",
             )
@@ -46,10 +52,10 @@ def plot_mixture_ids(orig_phm_path, sim_phm_path):
         )
 
         area = (
-            alt.Chart(idvals)
-            .mark_area(opacity=0.4)
+            alt.Chart(df)
+            .mark_area(opacity=0.7)
             .encode(
-                alt.X("SCALE:Q"),
+                alt.X("rank:Q"),
                 alt.Y("q95:Q"),
                 alt.Y2("q5:Q"),
             )
