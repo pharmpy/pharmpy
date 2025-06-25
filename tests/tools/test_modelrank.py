f8ab61a138b6f941f8697c82597a8f2796df8506
Stella <stella.belin@farmaci.uu.se>
2025-06-25 15:02:35 +0200

    Add strictness module to ModelRank

--- tests/tools/test_modelrank.py
+++ tests/tools/test_modelrank.py
@@ -2,6 +2,11 @@ import pytest
 
 from pharmpy.deps import pandas as pd
 from pharmpy.modeling import remove_parameter_uncertainty_step, set_initial_estimates, set_name
+from pharmpy.tools.modelrank.strictness import (
+    evaluate_strictness,
+    get_strictness_expr,
+    get_strictness_predicates,
+)
 from pharmpy.tools.modelrank.tool import (
     get_rank_type_kwargs,
     prepare_model_entries,
@@ -67,6 +72,134 @@ def test_rank_models(
 )
 def test_get_rank_type_kwargs(rank_type, kwargs):
     assert get_rank_type_kwargs(rank_type) == kwargs
+
+
+@pytest.mark.parametrize(
+    'strictness, model_path, ref_predicates',
+    [
+        ('minimization_successful', 'models/mox2.mod', {'minimization_successful': True}),
+        (
+            'minimization_successful or rounding_errors',
+            'models/mox2.mod',
+            {'minimization_successful': True, 'rounding_errors': False},
+        ),
+        (
+            'minimization_successful or (rounding_errors and sigdigs >= 0.1)',
+            'models/mox2.mod',
+            {
+                'minimization_successful': True,
+                'rounding_errors': False,
+                'sigdigs': 3.8,
+                'sigdigs >= 0.1': True,
+            },
+        ),
+        (
+            'minimization_successful or (rse <= 0.1)',
+            'models/mox2.mod',
+            {'minimization_successful': True, 'rse': None, 'rse <= 0.1': None},
+        ),
+        (
+            'not minimization_successful',
+            'models/mox2.mod',
+            {'~minimization_successful': False, 'minimization_successful': True},
+        ),
+        (
+            'not minimization_successful or not rounding_errors',
+            'models/mox2.mod',
+            {
+                '~minimization_successful': False,
+                'minimization_successful': True,
+                '~rounding_errors': True,
+                'rounding_errors': False,
+            },
+        ),
+        (
+            'not estimate_near_boundary',
+            'models/mox2.mod',
+            {'~estimate_near_boundary': False, 'estimate_near_boundary': True},
+        ),
+        (
+            'not estimate_near_boundary_theta and not estimate_near_boundary_omega',
+            'models/mox2.mod',
+            {
+                '~estimate_near_boundary_theta': True,
+                'estimate_near_boundary_theta': False,
+                '~estimate_near_boundary_omega': False,
+                'estimate_near_boundary_omega': True,
+            },
+        ),
+        (
+            'condition_number <= 100',
+            'models/mox2.mod',
+            {'condition_number': None, 'condition_number <= 100': None},
+        ),
+        (
+            'minimization_successful and (rse <= 1.0)',
+            'models/pheno5.mod',
+            {'minimization_successful': True, 'rse': 0.5814089116181571, 'rse <= 1.0': True},
+        ),
+        (
+            'minimization_successful and (rse <= 0.5)',
+            'models/pheno5.mod',
+            {'minimization_successful': True, 'rse': 0.5814089116181571, 'rse <= 0.5': False},
+        ),
+        (
+            'condition_number <= 100',
+            'models/pheno5.mod',
+            {'condition_number': 6828363899484635.0, 'condition_number <= 100': False},
+        ),
+    ],
+)
+def test_get_strictness_predicates(
+    testdata, load_model_for_test, strictness, model_path, ref_predicates
+):
+    model_start = load_model_for_test(testdata / 'nonmem' / model_path)
+    res_start = read_modelfit_results(testdata / 'nonmem' / model_path)
+    me_start = ModelEntry.create(model_start, modelfit_results=res_start)
+
+    expr = get_strictness_expr(strictness)
+    predicates = get_strictness_predicates(me_start, expr)
+    assert predicates.keys() == ref_predicates.keys()
+    for key, value in predicates.items():
+        assert ref_predicates[key] == value
+
+
+@pytest.mark.parametrize(
+    'strictness, model_path, strictness_fulfilled',
+    [
+        ('minimization_successful', 'models/mox2.mod', True),
+        ('minimization_successful or rounding_errors', 'models/mox2.mod', True),
+        (
+            'minimization_successful or (rounding_errors and sigdigs >= 0.1)',
+            'models/mox2.mod',
+            True,
+        ),
+        ('minimization_successful or (rse <= 0.1)', 'models/mox2.mod', True),
+        ('not minimization_successful', 'models/mox2.mod', False),
+        ('not minimization_successful or not rounding_errors', 'models/mox2.mod', True),
+        ('minimization_successful and (rse <= 0.1)', 'models/mox2.mod', None),
+        ('minimization_successful and not estimate_near_boundary', 'models/mox2.mod', False),
+        ('minimization_successful and not estimate_near_boundary_theta', 'models/mox2.mod', True),
+        ('minimization_successful and not estimate_near_boundary_omega', 'models/mox2.mod', False),
+        ('maxevals_exceeded', 'models/mox2.mod', False),
+        ('condition_number <= 100', 'models/mox2.mod', None),
+        ('final_zero_gradient', 'models/mox2.mod', None),
+        ('final_zero_gradient', 'pheno.mod', False),
+        ('minimization_successful and (rse <= 1.0)', 'models/pheno5.mod', True),
+        ('minimization_successful and (rse <= 0.5)', 'models/pheno5.mod', False),
+        ('condition_number <= 100', 'models/pheno5.mod', False),
+    ],
+)
+def test_evaluate_strictness(
+    testdata, load_model_for_test, strictness, model_path, strictness_fulfilled
+):
+    model_start = load_model_for_test(testdata / 'nonmem' / model_path)
+    res_start = read_modelfit_results(testdata / 'nonmem' / model_path)
+    me_start = ModelEntry.create(model_start, modelfit_results=res_start)
+
+    expr = get_strictness_expr(strictness)
+    predicates = get_strictness_predicates(me_start, expr)
+    assert evaluate_strictness(expr, predicates) == strictness_fulfilled
 
 
 @pytest.mark.parametrize(
