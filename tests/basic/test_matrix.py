adfc07e075e3a7af2f3904e19d9e355e0a97b191
Rikard Nordgren <rikard.nordgren@uu.se>
2025-09-17 16:45:30 +0200

    Add unit test for Matrix.inv

--- tests/basic/test_matrix.py
+++ tests/basic/test_matrix.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pharmpy.basic import Matrix
+from pharmpy.basic import Expr, Matrix
 
 
 def test_init():
@@ -39,6 +39,12 @@ def test_cholesky():
     # Example taken from: https://docs.sympy.org/latest/modules/matrices/matrices.html
     m = Matrix(((25, 15, -5), (15, 18, 0), (-5, 0, 11)))
     assert m.cholesky() == Matrix(((5, 0, 0), (3, 3, 0), (-1, 1, 3)))
+
+
+def test_inv():
+    A = Matrix(((2, 3), (1, 4)))
+    Ainv = A.inv()
+    assert Ainv == Matrix(((Expr("4/5"), Expr("-3/5")), (Expr("-1/5"), Expr("2/5"))))
 
 
 def test_matmul():
