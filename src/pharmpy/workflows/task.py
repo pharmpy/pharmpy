9d860691d0d01464bbf918344bbdef2ce0980748
Rikard Nordgren <rikard.nordgren@uu.se>
2025-10-02 09:30:51 +0200

    Add type checking of pharmpy.workflows.task

--- src/pharmpy/workflows/task.py
+++ src/pharmpy/workflows/task.py
@@ -1,11 +1,11 @@
-from typing import Callable, Generic, TypeVar
+from __future__ import annotations
+
+from typing import Callable, Self
 
 from pharmpy.internals.immutable import Immutable
 
-T = TypeVar('T')
 
-
-class Task(Generic[T], Immutable):
+class Task(Immutable):
     """One task in a workflow
 
     Parameters
@@ -18,16 +18,16 @@ class Task(Generic[T], Immutable):
         Input arguments to func
     """
 
-    def __init__(self, name: str, function: Callable[..., T], *task_input):
+    def __init__(self, name: str, function: Callable, *task_input):
         self._name = name
         self._function = function
         self._task_input: tuple = task_input
 
     @classmethod
-    def create(cls, name: str, function: Callable[..., T], *task_input):
+    def create(cls, name: str, function: Callable, *task_input) -> Self:
         return cls(name, function, *task_input)
 
-    def replace(self, **kwargs):
+    def replace(self, **kwargs) -> Task:
         name = kwargs.get("name", self._name)
         function = kwargs.get("function", self._function)
         task_input = kwargs.get("task_input", self._task_input)
@@ -38,19 +38,19 @@ class Task(Generic[T], Immutable):
         )
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Task name"""
         return self._name
 
     @property
-    def function(self):
+    def function(self) -> Callable:
         """Task function"""
         return self._function
 
     @property
-    def task_input(self):
+    def task_input(self) -> tuple:
         """Tuple of static input to function"""
         return self._task_input
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self._name
