64f88094a9ed18d5fc7cd18ecc9a4cf8a85f95e4
king-of-poppk <119671899+king-of-poppk@users.noreply.github.com>
2025-10-27 11:38:45 +0100

    Add alternative utility to `sympy.posify`

--- src/pharmpy/internals/expr/assumptions.py
+++ src/pharmpy/internals/expr/assumptions.py
@@ -6,7 +6,23 @@ from typing import Iterable
 
 from pharmpy.deps import sympy
 
+from .leaves import free_images_and_symbols
+from .subs import subs
+
 
 def assume_all(predicate: sympy.assumptions.Predicate, expressions: Iterable[sympy.Expr]):
     tautology = sympy.Q.is_true(True)
     return reduce(__and__, map(predicate, expressions), tautology)
+
+
+def posify(expr: sympy.Expr):
+    if expr.is_positive is not None:
+        return expr
+
+    if isinstance(expr, sympy.Symbol):
+        return sympy.Symbol(expr.name, positive=True, **expr.assumptions0)
+
+    if isinstance(expr.func, sympy.core.function.UndefinedFunction):
+        return sympy.core.function.UndefinedFunction(expr.func.name, positive=True)(*expr.args)
+
+    return subs(expr, {x: posify(x) for x in free_images_and_symbols(expr)}, simultaneous=True)
