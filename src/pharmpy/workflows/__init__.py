f7af430ed104a65f46f0b2be8ae2101a54f4d728
Rikard Nordgren <rikard.nordgren@uu.se>
2025-12-04 16:18:59 +0100

    Remove unused default_model_database configuration item

--- src/pharmpy/workflows/__init__.py
+++ src/pharmpy/workflows/__init__.py
@@ -1,23 +1,3 @@
-"""
-.. list-table:: Options for the data module
-   :widths: 25 25 50 150
-   :header-rows: 1
-
-   * - Option name
-     - Default value
-     - Type
-     - Description
-   * - ``default_model_database``
-     - ``pharmpy.workflows.LocalDirectoryDatabase``
-     - str
-     - Name of default model database class
-
-"""
-
-import importlib
-
-import pharmpy.config as config
-
 from .args import split_common_options
 from .contexts import Context, LocalDirectoryContext
 from .execute import execute_subtool, execute_workflow
@@ -34,35 +14,11 @@ from .task import Task
 from .workflow import Workflow, WorkflowBuilder
 
 
-class WorkflowConfiguration(config.Configuration):
-    module = 'pharmpy.workflows'
-    default_model_database = config.ConfigItem(
-        'pharmpy.workflows.LocalDirectoryDatabase', 'Name of default model database class'
-    )
-
-
-conf = WorkflowConfiguration()
-
-
-def _importclass(name):
-    a = name.split('.')
-    module_name = '.'.join(a[:-1])
-    if module_name == __name__:
-        return globals()[a[-1]]
-    else:
-        module = importlib.import_module(module_name)
-        return module.a[-1]
-
-
-default_model_database = _importclass(conf.default_model_database)
-
-
 class DispatchingError(Exception):
     pass
 
 
 __all__ = (
-    'default_model_database',
     'execute_subtool',
     'execute_workflow',
     'split_common_options',
