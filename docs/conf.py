772dc722938793a4596368af819d63ad1736fcd7
Rikard Nordgren <rikard.nordgren@uu.se>
2026-01-27 14:52:53 +0100

    Block check selection_critera reference in docs build

--- docs/conf.py
+++ docs/conf.py
@@ -93,4 +93,5 @@ pd.set_option('display.max_columns', 1000)
 '''
 
 linkcheck_ignore = [r'https://doi.org/10.1002/psp4.12741', # Page is reachable from browser but gives 403 with GET
-    r'https://doi.org/10.1515/ijb-2019-0082']
+    r'https://doi.org/10.1515/ijb-2019-0082',
+    r'https://projecteuclid.org/journals/electronic-journal-of-statistics/volume-8/issue-1/A-note-on-BIC-in-mixed-effects-models/10.1214/14-EJS890.full']
