80b7c2d3f0fa27f18a4ec1d528aa7a8ffd9eb5b7
Rikard Nordgren <rikard.nordgren@uu.se>
2026-04-10 12:28:04 +0200

    Add Arm to trial_design

--- src/pharmpy/model/trial_design.py
+++ src/pharmpy/model/trial_design.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from collections.abc import Sequence
-from typing import Any, Optional
+from abc import abstractmethod
+from collections.abc import Iterable, Sequence
+from typing import Any, Optional, Union, overload
 
 from pharmpy.internals.immutable import Immutable
 from pharmpy.model.datainfo import DataVariable
@@ -9,7 +10,16 @@ from pharmpy.model.statements import Dose
 
 
 class Activity(Immutable):
-    pass
+    @abstractmethod
+    def to_dict(self) -> dict[str, Any]: ...
+
+    @classmethod
+    def from_dict(cls, d: dict[str, Any]) -> Activity:
+        if d['class'] == 'Observations':
+            act = Observations.from_dict(d)
+        else:
+            act = Administration.from_dict(d)
+        return act
 
 
 class Observations(Activity):
@@ -174,3 +184,97 @@ class Administration(Activity):
 
     def __repr__(self):
         return f"Administration({self._variable.name}, {self._dose}, {self._start_time}, {self._time_points})"
+
+
+class Arm:
+    """Arm definition"""
+
+    def __init__(self, size: int, activities: tuple[Activity, ...]):
+        self._size = size
+        self._activities = activities
+
+    @classmethod
+    def create(cls, size: int, activities: Sequence[Activity]) -> Arm:
+        for act in activities:
+            if not isinstance(act, Activity):
+                raise TypeError("Activities in Arm must be of type Activity")
+        return cls(size, tuple(activities))
+
+    def replace(
+        self,
+        size: Optional[int] = None,
+        activities: Optional[Sequence[Activity]] = None,
+    ) -> Arm:
+        if size is None:
+            size = self._size
+        if activities is None:
+            activities = self._activities
+        return Arm.create(size=size, activities=activities)
+
+    @property
+    def size(self) -> int:
+        """Size of arm"""
+        return self._size
+
+    @property
+    def activities(self) -> tuple[Activity, ...]:
+        """All activities in the arm"""
+        return self._activities
+
+    def to_dict(self) -> dict[str, Any]:
+        acts = tuple(a.to_dict() for a in self)
+        return {
+            'size': self._size,
+            'activities': acts,
+        }
+
+    @classmethod
+    def from_dict(cls, d: dict[str, Any]) -> Arm:
+        acts = []
+        for sdict in d['activities']:
+            acts.append(Activity.from_dict(sdict))
+        return cls.create(size=d['size'], activities=acts)
+
+    def __len__(self):
+        return len(self._activities)
+
+    @overload
+    def __getitem__(self, ind: int) -> Activity: ...
+
+    @overload
+    def __getitem__(self, ind: slice) -> Arm: ...
+
+    def __getitem__(self, ind: Union[int, slice]) -> Union[Activity, Arm]:
+        if isinstance(ind, slice):
+            return self.replace(activities=self._activities[ind])
+        else:
+            return self._activities[ind]
+
+    def __add__(self, other: Union[Activity, Iterable[Activity]]) -> Arm:
+        if isinstance(other, Activity):
+            return self.replace(activities=self._activities + (other,))
+        elif isinstance(other, Iterable):
+            return self.replace(activities=self._activities + tuple(other))
+        else:
+            return NotImplemented
+
+    def __radd__(self, other: Union[Activity, Iterable[Activity]]) -> Arm:
+        if isinstance(other, Activity):
+            return self.replace(activities=(other,) + self._activities)
+        elif isinstance(other, Iterable):
+            return self.replace(activities=tuple(other) + self._activities)
+        else:
+            return NotImplemented
+
+    def __eq__(self, other: Any):
+        if self is other:
+            return True
+        if not isinstance(other, Arm):
+            return NotImplemented
+        return self._size == other._size and self._activities == other._activities
+
+    def __hash__(self):
+        return hash((self._size, self._activities))
+
+    def __repr__(self):
+        return f"Arm(size={self._size}, {self._activities})"
--- tests/model/test_trial_design.py
+++ tests/model/test_trial_design.py
@@ -1,6 +1,8 @@
+import pytest
+
 from pharmpy.basic import Expr
 from pharmpy.model import Bolus, DataVariable
-from pharmpy.model.trial_design import Administration, Observations
+from pharmpy.model.trial_design import Administration, Arm, Observations
 
 
 def test_observations():
@@ -67,3 +69,87 @@ def test_administration():
     }
     assert adm1.to_dict() == d
     assert Administration.from_dict(d) == adm1
+
+
+def test_arm():
+    dv = DataVariable("CONC", "dv", "ratio")
+    obs1 = Observations(dv, 0.0, (0.0, 1.0, 2.0))
+    arm1 = Arm(size=50, activities=(obs1,))
+    arm2 = Arm.create(size=50, activities=[obs1])
+    assert arm1 == arm1
+    assert arm1 == arm2
+    assert hash(arm1) == hash(arm2)
+    assert arm1 != 23
+
+    obs2 = Observations(dv, 0.0, (1.0, 3.0))
+    assert arm1.replace(size=75).size == 75
+    assert arm1.replace(activities=(obs2,)).activities == (obs2,)
+
+    amt = DataVariable("AMT", "dose", "ratio")
+    dose = Bolus.create(100)
+    adm1 = Administration(amt, dose, 0.0, (0.0, 1.0, 2.0))
+    arm4 = Arm.create(size=50, activities=[obs1, adm1])
+
+    d = {
+        'size': 50,
+        'activities': (
+            {
+                'class': 'Observations',
+                'variable': {
+                    'count': False,
+                    'name': 'CONC',
+                    'properties': {},
+                    'scale': 'ratio',
+                    'type': 'dv',
+                },
+                'start_time': 0.0,
+                'time_points': (0.0, 1.0, 2.0),
+            },
+            {
+                'class': 'Administration',
+                'variable': {
+                    'count': False,
+                    'name': 'AMT',
+                    'properties': {},
+                    'scale': 'ratio',
+                    'type': 'dose',
+                },
+                'dose': {'class': 'Bolus', 'amount': 'Integer(100)', 'admid': 1},
+                'start_time': 0.0,
+                'time_points': (0.0, 1.0, 2.0),
+            },
+        ),
+    }
+
+    assert arm4.to_dict() == d
+    assert Arm.from_dict(d) == arm4
+
+    assert len(arm1) == 1
+
+    assert arm1[0] == obs1
+
+    amt = DataVariable("AMT", "dose", "ratio")
+    dose = Bolus.create(100)
+    adm1 = Administration(amt, dose, 0.0, (0.0, 1.0, 2.0))
+
+    arm3 = Arm.create(size=75, activities=[obs1, obs2, adm1])
+    assert len(arm3) == 3
+    assert arm3[2] == adm1
+    assert arm3[0:2] == Arm.create(size=75, activities=[obs1, obs2])
+    assert arm3[1:] == Arm.create(size=75, activities=[obs2, adm1])
+
+    assert obs1 + Arm.create(size=75, activities=[obs2, adm1]) == arm3
+    assert [obs1] + Arm.create(size=75, activities=[obs2, adm1]) == arm3
+    assert Arm.create(size=75, activities=[obs1, obs2]) + adm1 == arm3
+    assert Arm.create(size=75, activities=[obs1, obs2]) + [adm1] == arm3
+
+    with pytest.raises(TypeError):
+        23 + arm3
+
+    with pytest.raises(TypeError):
+        arm3 + "a"
+
+    with pytest.raises(TypeError):
+        arm3 + 1
+
+    assert repr(arm1) == "Arm(size=50, (Observations(CONC, 0.0, (0.0, 1.0, 2.0)),))"
