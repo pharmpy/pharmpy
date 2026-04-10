d24c004c5796a67c79c3f65104c3920da3b664bc
Rikard Nordgren <rikard.nordgren@uu.se>
2026-04-10 10:34:26 +0200

    Add Administration to trial_design

--- src/pharmpy/model/trial_design.py
+++ src/pharmpy/model/trial_design.py
@@ -5,6 +5,7 @@ from typing import Any, Optional
 
 from pharmpy.internals.immutable import Immutable
 from pharmpy.model.datainfo import DataVariable
+from pharmpy.model.statements import Dose
 
 
 class Activity(Immutable):
@@ -82,3 +83,94 @@ class Observations(Activity):
 
     def __repr__(self):
         return f"Observations({self._variable.name}, {self._start_time}, {self._time_points})"
+
+
+class Administration(Activity):
+    """Administration activity"""
+
+    def __init__(
+        self, variable: DataVariable, dose: Dose, start_time: float, time_points: tuple[float, ...]
+    ):
+        self._variable = variable
+        self._dose = dose
+        self._start_time = start_time
+        self._time_points = time_points
+
+    @classmethod
+    def create(
+        cls, variable: DataVariable, dose: Dose, start_time: float, time_points: Sequence[float]
+    ) -> Administration:
+        return cls(variable, dose, start_time, tuple(time_points))
+
+    def replace(
+        self,
+        variable: Optional[DataVariable] = None,
+        dose: Optional[Dose] = None,
+        start_time: Optional[float] = None,
+        time_points: Optional[Sequence[float]] = None,
+    ) -> Administration:
+        if variable is None:
+            variable = self._variable
+        if dose is None:
+            dose = self._dose
+        if start_time is None:
+            start_time = self._start_time
+        if time_points is None:
+            time_points = self._time_points
+        return Administration.create(variable, dose, start_time, time_points)
+
+    @property
+    def variable(self) -> DataVariable:
+        """The dose data variable"""
+        return self._variable
+
+    @property
+    def dose(self) -> Dose:
+        """The dose"""
+        return self._dose
+
+    @property
+    def start_time(self) -> float:
+        """Start time of observation sequence"""
+        return self._start_time
+
+    @property
+    def time_points(self) -> tuple[float, ...]:
+        """Observation times relative to start_time"""
+        return self._time_points
+
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            'class': 'Administration',
+            'variable': self._variable.to_dict(),
+            'dose': self._dose.to_dict(),
+            'start_time': self._start_time,
+            'time_points': self._time_points,
+        }
+
+    @classmethod
+    def from_dict(cls, d: dict[str, Any]) -> Administration:
+        return cls.create(
+            DataVariable.from_dict(d['variable']),
+            Dose.from_dict(d['dose']),
+            d['start_time'],
+            d['time_points'],
+        )
+
+    def __eq__(self, other: Any):
+        if self is other:
+            return True
+        if not isinstance(other, Administration):
+            return NotImplemented
+        return (
+            self._variable == other._variable
+            and self._dose == other._dose
+            and self._start_time == other._start_time
+            and self._time_points == other._time_points
+        )
+
+    def __hash__(self):
+        return hash((self._variable, self._dose, self._start_time, self._time_points))
+
+    def __repr__(self):
+        return f"Administration({self._variable.name}, {self._dose}, {self._start_time}, {self._time_points})"
--- tests/model/test_trial_design.py
+++ tests/model/test_trial_design.py
@@ -1,5 +1,6 @@
-from pharmpy.model import DataVariable
-from pharmpy.model.trial_design import Observations
+from pharmpy.basic import Expr
+from pharmpy.model import Bolus, DataVariable
+from pharmpy.model.trial_design import Administration, Observations
 
 
 def test_observations():
@@ -31,3 +32,38 @@ def test_observations():
     }
     assert obs1.to_dict() == d
     assert Observations.from_dict(d) == obs1
+
+
+def test_administration():
+    amt = DataVariable("AMT", "dose", "ratio")
+    dose = Bolus.create(100)
+    adm1 = Administration(amt, dose, 0.0, (0.0, 1.0, 2.0))
+    adm2 = Administration.create(amt, dose, 0.0, [0.0, 1.0, 2.0])
+    assert adm1 == adm2
+    assert adm1 == adm1
+    assert adm1 != 1.0
+    assert hash(adm1) == hash(adm2)
+    assert repr(adm1) == 'Administration(AMT, Bolus(100, admid=1), 0.0, (0.0, 1.0, 2.0))'
+
+    amt2 = DataVariable("DOSE", "dose", "ratio")
+    dose2 = Bolus.create(10 * Expr.symbol("WGT"))
+    assert adm1.replace(variable=amt2).variable == amt2
+    assert adm1.replace(start_time=1.0).start_time == 1.0
+    assert adm1.replace(time_points=[2.0]).time_points == (2.0,)
+    assert adm1.replace(dose=dose2).dose == dose2
+
+    d = {
+        'class': 'Administration',
+        'variable': {
+            'count': False,
+            'name': 'AMT',
+            'properties': {},
+            'scale': 'ratio',
+            'type': 'dose',
+        },
+        'dose': {'class': 'Bolus', 'amount': 'Integer(100)', 'admid': 1},
+        'start_time': 0.0,
+        'time_points': (0.0, 1.0, 2.0),
+    }
+    assert adm1.to_dict() == d
+    assert Administration.from_dict(d) == adm1
