f06f20a0bad514ffd06021eee0f37cd45a7d051e
Stella Belin <stella.belin@uu.se>
2026-05-29 11:06:03 +0200

    Support multiline observations in FDATA parsing

--- src/pharmpy/model/external/fcon/model.py
+++ src/pharmpy/model/external/fcon/model.py
@@ -6,11 +6,13 @@
 import re
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Optional
+from typing import Container, Optional
 
 from pharmpy.deps import pandas as pd
 from pharmpy.model import Model as BaseModel
 from pharmpy.model import ModelSyntaxError
+
+FORTRAN_FORMAT = re.compile(r'(\d+)\w(\d+)')
 
 
 def detect_model(src, *args, **kwargs):
@@ -42,7 +44,7 @@ def _parse_labels_and_formats(code):
             in_labl = True
         if in_labl:
             stripped = line[4:].replace(' ', '')
-            if stripped.startswith(','):
+            if stripped.startswith(',') or (not line.startswith('LABL') and line[0].isalpha()):
                 in_labl = False
             else:
                 a = stripped.split(',')
@@ -60,13 +62,103 @@ def _parse_labels_and_formats(code):
 def parse_dataset(code, path):
     labels, formats = _parse_labels_and_formats(code)
     widths = []
-    for fmt in formats:
-        m = re.match(r'(\d+)\w(\d+)', fmt)
-        if not m:
-            raise ModelSyntaxError(f"Unrecognized Data format in FORM: {fmt}")
-        widths += [int(m.group(2))] * int(m.group(1))
-    df = pd.read_fwf(path.parent / 'FDATA', widths=widths, header=None, names=labels)
+    has_linebreaks = any('/' in fmt for fmt in formats)
+    if has_linebreaks:
+        df = read_multiline_observations(path.parent / 'FDATA', labels, formats)
+    else:
+        for fmt in formats:
+            no_of_cols, col_width = parse_format(fmt)
+            widths += [col_width] * no_of_cols
+        df = pd.read_fwf(path.parent / 'FDATA', widths=widths, header=None, names=labels)
+    df.index = range(1, len(df) + 1)
+
+    # Copied conversion from pharmpy/model/external/nonmem/dataset/__init__.py
+    idcol = _idcol(labels)
+    if all((_ids := df[idcol].astype('int32')) == df[idcol]):
+        df[idcol] = _ids
+
     return df
+
+
+def parse_format(fmt):
+    m = FORTRAN_FORMAT.match(fmt)
+    if not m:
+        raise ModelSyntaxError(f"Unrecognized Data format in FORM: {fmt}")
+    no_of_cols, col_width = int(m.group(1)), int(m.group(2))
+    return no_of_cols, col_width
+
+
+def read_multiline_observations(path, labels, formats):
+    formats = group_formats(formats)
+    observation = []
+    parsed_rows = []
+    i = 0
+
+    with open(path, 'r') as f:
+        for line in f:
+            line = line.strip('\n')
+            parsed_line = parse_line(line, formats[i])
+            observation.extend(parsed_line)
+            i += 1
+
+            if len(observation) >= len(labels):
+                assert len(observation) == len(labels)
+                parsed_rows.append(observation)
+                observation = []
+                i = 0
+
+    df = pd.DataFrame(parsed_rows, columns=labels)
+    return df
+
+
+def group_formats(formats):
+    rows = []
+    final_row = False
+    for fmt in formats:
+        outer_match = re.match(r'(\d+)?\((.+)\)', fmt)
+        if outer_match:
+            no_of_rows = int(outer_match.group(1))
+            inner_fmt = outer_match.group(2).rstrip('/')
+        else:
+            no_of_rows = 1
+            inner_fmt = fmt
+
+        if no_of_rows > 1:
+            rows.extend([[inner_fmt]] * no_of_rows)
+        else:
+            if final_row:
+                rows[-1].append(inner_fmt)
+            else:
+                rows.append([inner_fmt])
+                final_row = True
+    return rows
+
+
+def parse_line(line, fmts):
+    offset = 0
+    values = []
+    for fmt in fmts:
+        no_of_cols, col_width = parse_format(fmt)
+        for i in range(no_of_cols):
+            start = i * col_width + offset
+            end = start + col_width
+            value = line[start:end].strip()
+            if '.' in value:
+                value = float(value)
+            else:  # NONMEM only supports numbers
+                value = int(value)
+            values.append(value)
+        offset = end
+    return values
+
+
+def _idcol(columns: Container[str | None]) -> str | None:
+    if 'ID' in columns:
+        return 'ID'
+    elif 'L1' in columns:
+        return 'L1'
+    else:
+        return None
 
 
 def parse_model(code: str, path: Path, missing_data_token: Optional[str] = None):
--- tests/nonmem/test_fcon.py
+++ tests/nonmem/test_fcon.py
@@ -1,9 +1,73 @@
-def test_dataset(load_model_for_test, testdata):
-    model = load_model_for_test(testdata / 'nonmem' / 'fcon' / 'FCON')
+import pytest
+
+from pharmpy.model.external.fcon.model import group_formats, read_multiline_observations
+
+
+@pytest.mark.parametrize(
+    'fcon, model_path, col',
+    [
+        (
+            'pheno',
+            'pheno_real.mod',
+            'WGT',
+        ),
+        (
+            'mox2',
+            'models/mox2.mod',
+            'TIME',
+        ),
+    ],
+)
+def test_dataset(load_model_for_test, testdata, fcon, model_path, col):
+    model = load_model_for_test(testdata / 'nonmem' / 'fcon' / fcon / 'FCON')
     assert model.internals.code.startswith('FILE')
 
     df = model.dataset
+    assert df.notna().all().all()
+    assert df['ID'].dtype == 'int32'
 
-    nmtran = load_model_for_test(testdata / 'nonmem' / 'pheno_real.mod')
+    nmtran = load_model_for_test(testdata / 'nonmem' / model_path)
+    assert df.index.equals(nmtran.dataset.index)
+    assert list(df[col]) == list(nmtran.dataset[col])
 
-    assert list(df['WGT']) == list(nmtran.dataset['WGT'])
+
+def test_read_multiline_observations(testdata):
+    path = testdata / 'nonmem' / 'fcon' / 'mox2' / 'FDATA'
+    labels = [
+        'ID',
+        'VISI',
+        'DGRP',
+        'DOSE',
+        'NEUY',
+        'SCR',
+        'AGE',
+        'SEX',
+        'WT',
+        'ACE',
+        'DIG',
+        'DIU',
+        'TAD',
+        'TIME',
+        'CLCR',
+        'AMT',
+        'SS',
+        'II',
+        'DV',
+        'EVID',
+        'MDV',
+    ]
+    formats = ['4(4E19.0/)', '3E19.0', '2F2.0']
+    df = read_multiline_observations(path, labels, formats)
+    assert len(df) == 1148
+    assert df.notna().all().all()
+
+
+def test_group_formats():
+    formats = ['4(4E19.0/)', '3E19.0', '2F2.0']
+    assert group_formats(formats) == [
+        ['4E19.0'],
+        ['4E19.0'],
+        ['4E19.0'],
+        ['4E19.0'],
+        ['3E19.0', '2F2.0'],
+    ]
