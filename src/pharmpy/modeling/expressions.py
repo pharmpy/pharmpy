4b32f1213d93aa2ee707604621c08939d24e10ab
Rikard Nordgren <rikard.nordgren@uu.se>
2026-08-11 09:17:21 +0200

    Fix return types in docstring in modeling.expression

--- src/pharmpy/modeling/expressions.py
+++ src/pharmpy/modeling/expressions.py
@@ -49,7 +49,7 @@ def get_observation_expression(model: Model) -> Expr:
 
     Returns
     -------
-    Expression
+    Expr
         Symbolic expression
 
     Examples
@@ -91,7 +91,7 @@ def get_individual_prediction_expression(model: Model) -> Expr:
 
     Returns
     -------
-    Expression
+    Expr
         Symbolic expression
 
     Examples
@@ -123,7 +123,7 @@ def get_population_prediction_expression(model: Model) -> Expr:
 
     Returns
     -------
-    Expression
+    Expr
         Symbolic expression
 
     Examples
@@ -224,7 +224,7 @@ def create_symbol(model: Model, stem: str, force_numbering: bool = False) -> Exp
 
     Returns
     -------
-    Symbol
+    Expr
         Created symbol with unique name
 
     Examples
