3a61b7d3c6553e3e191bbb4b0390f8d5b838b052
Rikard Nordgren <rikard.nordgren@uu.se>
2026-09-18 12:57:51 +0200

    Add parser support for UNIFORM and NORMAL in NONMEM $SIMULATION

--- tests/nonmem/records/test_simulation.py
+++ tests/nonmem/records/test_simulation.py
@@ -7,6 +7,8 @@ import pytest
         ('$SIMULATION (1212) NSUBPROBS=0', 1),
         ('$SIMULATION (1212) NSUBPROBLEMS=1', 1),
         ('$SIMULATION (1212) SUBPROB=60', 60),
+        ('$SIMULATION (1212 NORMAL) SUBPROB=60', 60),
+        ('$SIMULATION (1212) (898 UNIFORM) SUBPROB=60', 60),
     ],
 )
 def test_nsubs(parser, buf, value):
